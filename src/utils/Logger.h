ce058bcebcddd05ddf3cc0f5440dba73bdc5bc7c
Martin Bernreuther <bernreuther@hlrs.de>
2010-05-26 20:34:14 +0000

    Logger.h: changed timestamp format (YYMMDDThhmmss instead of seconds since 1.1.1970) (since log messages are not written that much, the additional CPU cycles should be not that critical)
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@247 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/utils/Logger.h
+++ src/utils/Logger.h
@@ -1,6 +1,8 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#define USE_GETTIMEOFDAY
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -9,6 +11,10 @@
 #include <ctime>
 #include <string>
 #include <sstream>
+
+#ifdef USE_GETTIMEOFDAY
+#include <sys/time.h>
+#endif
 
 /* some applications define PARALLEL to enable MPI */
 #ifdef PARALLEL
@@ -27,18 +33,18 @@ namespace Log {
 
 	class Logger;
 
-	/* Gobal logger variable for use in the entire program.
-	 * Must be initialized with constructor e.g. new Log::Logger().
-	 * Namespace visibility:
-	 *	 using Log::global_log;
-	 */
+	/** Gobal logger variable for use in the entire program.
+	  * Must be initialized with constructor e.g. new Log::Logger().
+	  * Namespace visibility:
+	  *	 using Log::global_log;
+	  */
 #ifndef LOGGER_SRC
 	extern Log::Logger *global_log;
 #endif
 
-	/* list of available log levels
-	 * For each level a name has to be specified in the constructor of the Logger() class.
-	 * This name will be prepended later on to the log message. */
+	/** list of available log levels
+	  * For each level a name has to be specified in the constructor of the Logger() class.
+	  * This name will be prepended later on to the log message. */
 	typedef enum {
 		None        = 0,    /* supress output */
 		Fatal       = 1,    /* program exit */
@@ -49,7 +55,7 @@ namespace Log {
 		All
 	} logLevel;
 
-	/* Logging class 
+	/** Logging class 
 	 * Provides easy interface to handle log messages. Initialize either with 
 	 * output level and stream or output level and filename or use default constructor
 	 * values (Error, &(std::cout)). With a given file basename and MPI Support each rank will
@@ -67,19 +73,24 @@ namespace Log {
 	 * MPI_Init call.
 	 */
 	class Logger{
-		private:        
+		private:
 			std::string _filename;
 			std::ostream *_log_stream;
 			logLevel _log_level;
 			logLevel _msg_log_level;
 			bool _do_output;
 			std::map<logLevel, std::string> logLevelNames;
+#ifdef USE_GETTIMEOFDAY
+			timeval _starttime;
+#else
+			time_t _starttime;
+#endif
 
 #ifdef MPI_SUPPORT
 			int _rank;
 #endif
 
-			// initilaize the list of log levels with the corresponding short names
+			/// initilaize the list of log levels with the corresponding short names
 			void init_log_levels() {
 				logLevelNames.insert(std::pair<logLevel, std::string>(Fatal,   "FATAL ERROR"  ));
 				logLevelNames.insert(std::pair<logLevel, std::string>(Error,   "ERROR"        ));
@@ -90,9 +101,10 @@ namespace Log {
 
 
 		public:
-			/* Initializes the log level, log stream and the list of log level names.
-			 * If MPI_SUPPORT is enabled by default all process perform logging output. */
-			Logger( logLevel level = Log::Error, std::ostream *os = &(std::cout) ): _log_level(level), _log_stream(os) { 
+			/** Initializes the log level, log stream and the list of log level names.
+			  * If MPI_SUPPORT is enabled by default all process perform logging output. */
+			Logger( logLevel level = Log::Error, std::ostream *os = &(std::cout) ): _log_level(level), _log_stream(os) {
+				init_starting_time();
 				_filename = "";
 				_do_output  = true;
 				this->init_log_levels();
@@ -102,6 +114,7 @@ namespace Log {
 			}
 
 			Logger( logLevel level, std::string prefix ): _log_level(level) {
+				init_starting_time();
 				std::stringstream filenamestream;
 				filenamestream << prefix;
 				_do_output  = true;
@@ -114,14 +127,14 @@ namespace Log {
 				_log_stream = new std::ofstream( _filename.c_str() );
 			}
 
-			/* Destructor flushes stream */
+			/// Destructor flushes stream
 			~Logger(){ 
 				*_log_stream << std::flush;
 				if ( _filename != "" )
 					(static_cast<std::ofstream*>(_log_stream))->close();
 			}
 
-			/* General output template for variables, strings, etc. */
+			/// General output template for variables, strings, etc.
 			template<typename T> 
 				Logger &operator<<(T const& t) { 
 					if ( _msg_log_level <= _log_level && _do_output )
@@ -149,23 +162,38 @@ namespace Log {
 					return *this;
 				}
 
-			/* Add log info in front of messages */
+			/// Add log info in front of messages
 			Logger& msg_level( logLevel level ){
 				_msg_log_level = level;
 				if ( _msg_log_level <= _log_level && _do_output ) {
 					// Include timestamp
 					time_t t;
 					t = time( NULL );
-					*_log_stream << t << " ";
+					tm* lt = localtime(&t);
+					//*_log_stream << ctime(&t) << " ";
+					std::stringstream timestampstream;
+					// maybe sprintf is easier here...
+					timestampstream << std::setfill ('0') << std::setw (2)
+					                << lt->tm_year << lt->tm_mon << lt->tm_mday
+					                << "T" << lt->tm_hour << lt->tm_min << lt->tm_sec;
+					*_log_stream << timestampstream.str() << " ";
+					//timestampstream.str(""); timestampstream.clear();
+#ifdef USE_GETTIMEOFDAY
+					timeval tod;
+					gettimeofday(&tod, 0);
+					*_log_stream << tod.tv_sec-_starttime.tv_sec+(tod.tv_usec-_starttime.tv_usec)/1.E6 << "\t";
+#else
+					*_log_stream << t-_starttime << "\t";
+#endif
 #ifdef MPI_SUPPORT
-					*_log_stream << "[rank " << _rank << "] ";
+					*_log_stream << "[" << _rank << "]\t";
 #endif
 					*_log_stream << logLevelNames[level] << ": ";
 				}
 				return *this;
 			}
 
-			/* shorthand versions for easy usage of the different log levels */
+			/// shorthand versions for easy usage of the different log levels
 			Logger& fatal()  { return msg_level(Fatal);   }
 			Logger& error()  { return msg_level(Error);   }
 			Logger& warning(){ return msg_level(Warning); }
@@ -173,38 +201,47 @@ namespace Log {
 			Logger& debug()  { return msg_level(Debug);   }
 
 
-			// set log level
+			/// set log level
 			logLevel set_log_level( logLevel l ){
 				_log_level = l;
 				return _log_level;
 			}
-			// return log level
+			/// return log level
 			logLevel get_log_level(){
 				return _log_level;
 			}
 
-			/* switch on / off output */
+			/// switch on / off output
 			bool set_do_output(bool val) {
 				return _do_output = val;
+			}
+
+			/// initialize starting time
+			void init_starting_time(){
+#ifdef USE_GETTIMEOFDAY
+				gettimeofday(&_starttime, 0);
+#else
+				_starttime = time( NULL );
+#endif
 			}
 
 #ifdef MPI_SUPPORT
 			/* methods for easy handling of output processes */
 
-			/* allow logging only for a single process */
+			/// allow logging only for a single process
 			bool set_mpi_output_root(int root = 0) {
 				if (_rank != root)
 					_do_output = false;
 				return _do_output;
 			}
 
-			/* all processes shall perform logging */
+			/// all processes shall perform logging
 			bool set_mpi_output_all() {
 				_do_output = true;
 				return _do_output;
 			}
 
-			/* allow a set of processes for logging */
+			/// allow a set of processes for logging
 			bool set_mpi_output_ranks(int num_nums, int* nums) {
 				int i;
 				for(i = 0; i < num_nums; i++)
