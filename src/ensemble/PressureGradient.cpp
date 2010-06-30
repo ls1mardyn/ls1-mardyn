7d01356fa8ae0fbc4fd53082882b35599ed94f7a
Martin Horsch <martin.horsch@upb.de>
2010-06-30 14:01:10 +0000

    Introduces the new class "PressureGradient", essentially containing the "Domain" code chunks dealing with the external acceleration.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@287 a63bd714-7e14-4b5e-94e7-302c8c8ff188

---
+++ src/ensemble/PressureGradient.cpp
@@ -0,0 +1,201 @@
+/*
+ * M. T. Horsch, LS1/Mardyn project moderated by Martin Bernreuther
+ * (C)2010 GNU General Public License
+ */
+#include "PressureGradient.h"
+#include "parallel/DomainDecompBase.h"
+#include "particleContainer/ParticleContainer.h"
+#include "molecules/Molecule.h"
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+void PressureGradient::specifyComponentSet(unsigned cosetid, double v[3], double tau, double ainit[3], double timestep)
+{
+	this->_localN[cosetid] = 0;
+	for(unsigned d = 0; d < 3; d++)
+	{
+		this->_universalAdditionalAcceleration[d][cosetid] = ainit[d];
+		this->_localVelocitySum[d][cosetid] = 0.0;
+	}
+	this->_universalTau[cosetid] = tau;
+	if(this->_localRank == 0)
+	{
+		this->_globalN[cosetid] = 0;
+		for(unsigned d = 0; d < 3; d++)
+		{
+			this->_globalTargetVelocity[d][cosetid] = v[d];
+			this->_globalVelocitySum[d][cosetid] = 0.0;
+		}
+		this->_globalVelocityQueuelength[cosetid] = (unsigned)ceil(
+				sqrt(this->_universalTau[cosetid] / (timestep*this->_universalConstantAccelerationTimesteps))
+				);
+		cout << "coset " << cosetid << " will receive "
+			<< _globalVelocityQueuelength[cosetid] << " velocity queue entries." << endl;
+	}
+}
+
+/*
+ * diese Version beschleunigt in alle Raumrichtungen
+ */
+void PressureGradient::determineAdditionalAcceleration
+(
+ DomainDecompBase* domainDecomp,
+ ParticleContainer* molCont, double dtConstantAcc )
+{
+	for( map<unsigned, double>::iterator uAAit = _universalAdditionalAcceleration[0].begin();
+			uAAit != _universalAdditionalAcceleration[0].end();
+			uAAit++ )
+	{
+		this->_localN[uAAit->first] = 0;
+		for(unsigned d = 0; d < 3; d++)
+			this->_localVelocitySum[d][uAAit->first] = 0.0;
+	}
+	for(Molecule* thismol = molCont->begin(); thismol != molCont->end(); thismol = molCont->next())
+	{
+		unsigned cid = thismol->componentid();
+		map<unsigned, unsigned>::iterator uCSIDit = this->_universalComponentSetID.find(cid);
+		if(uCSIDit == _universalComponentSetID.end()) continue;
+		unsigned cosetid = uCSIDit->second;
+		this->_localN[cosetid]++;
+		for(unsigned d = 0; d < 3; d++)
+			this->_localVelocitySum[d][cosetid] += thismol->v(d);
+	}
+
+	// domainDecomp->collectCosetVelocity(&_localN, _localVelocitySum, &_globalN, _globalVelocitySum);
+	//
+	domainDecomp->collCommInit( 4 * _localN.size() );
+	for( map<unsigned, unsigned long>::iterator lNit = _localN.begin(); lNit != _localN.end(); lNit++ )
+	{
+		domainDecomp->collCommAppendUnsLong(lNit->second);
+		for(int d = 0; d < 3; d++)
+			domainDecomp->collCommAppendDouble( this->_localVelocitySum[d][lNit->first]);
+	}
+	domainDecomp->collCommAllreduceSum();
+	for( map<unsigned, unsigned long>::iterator lNit = _localN.begin(); lNit != _localN.end(); lNit++ )
+	{
+		_globalN[lNit->first] = domainDecomp->collCommGetUnsLong();
+		for(int d = 0; d < 3; d++)
+			_globalVelocitySum[d][lNit->first] = domainDecomp->collCommGetDouble();
+	}
+	domainDecomp->collCommFinalize();
+
+	map<unsigned, long double>::iterator gVSit;
+	if(!this->_localRank)
+	{
+		for(gVSit = _globalVelocitySum[0].begin(); gVSit != _globalVelocitySum[0].end(); gVSit++)
+		{
+#ifndef NDEBUG
+			global_log->debug() << "required entries in velocity queue: " << _globalVelocityQueuelength[gVSit->first] << endl;
+			global_log->debug() << "entries in velocity queue: " << _globalPriorVelocitySums[0][gVSit->first].size() << endl;
+#endif
+			for(unsigned d = 0; d < 3; d++)
+			{
+				while(_globalPriorVelocitySums[d][gVSit->first].size() < _globalVelocityQueuelength[gVSit->first])
+					_globalPriorVelocitySums[d][gVSit->first].push_back(_globalVelocitySum[d][gVSit->first]);
+			}
+		}
+	}
+
+	if(!this->_localRank)
+	{
+		for(gVSit = _globalVelocitySum[0].begin(); gVSit != _globalVelocitySum[0].end(); gVSit++)
+		{
+			double invgN = 1.0 / this->_globalN[gVSit->first];
+			double invgtau = 1.0 / this->_universalTau[gVSit->first];
+			double invgtau2 = invgtau * invgtau;
+			double previousVelocity[3];
+			for(unsigned d = 0; d < 3; d++)
+			{
+				previousVelocity[d] = invgN * _globalPriorVelocitySums[d][gVSit->first].front();
+				this->_globalPriorVelocitySums[d][gVSit->first].pop_front();
+				this->_universalAdditionalAcceleration[d][gVSit->first]
+					+= dtConstantAcc * invgtau2 *
+					( this->_globalTargetVelocity[d][gVSit->first]
+					  - 2.0*this->_globalVelocitySum[d][gVSit->first]*invgN
+					  + previousVelocity[d] );
+			}
+#ifndef NDEBUG
+			global_log->debug() << "accelerator no. " << gVSit->first 
+				<< "previous vz: " << previousVelocity[2] 
+				<< "current vz: " << _globalVelocitySum[2][gVSit->first]*invgN << endl;
+#endif
+		}
+	}
+
+	domainDecomp->collCommInit(7*this->_localN.size());
+	for( map<unsigned, unsigned long>::iterator lNit = _localN.begin();
+			lNit != this->_localN.end();
+			lNit++ )
+	{
+		domainDecomp->collCommAppendUnsLong(_globalN[lNit->first]);
+		for(int d=0; d < 3; d++)
+		{
+			domainDecomp->collCommAppendDouble(
+					this->_universalAdditionalAcceleration[d][lNit->first]
+					);
+			domainDecomp->collCommAppendDouble(
+					this->_globalVelocitySum[d][lNit->first]
+					);
+		}
+	}
+	domainDecomp->collCommBroadcast();
+	for( map<unsigned, unsigned long>::iterator lNit = _localN.begin();
+			lNit != this->_localN.end();
+			lNit++ )
+	{
+		_globalN[lNit->first] = domainDecomp->collCommGetUnsLong();
+		for(int d=0; d < 3; d++)
+		{
+			this->_universalAdditionalAcceleration[d][lNit->first]
+				= domainDecomp->collCommGetDouble();
+			this->_globalVelocitySum[d][lNit->first]
+				= domainDecomp->collCommGetDouble();
+		}
+	}
+	domainDecomp->collCommFinalize();
+}
+
+double PressureGradient::getDirectedVelocity(unsigned cosetid, unsigned d)
+{
+	if(!this->_localRank) 
+		return this->_globalVelocitySum[d][cosetid] / this->_globalN[cosetid];
+	else return 0.0;
+}
+
+double PressureGradient::getUniformAcceleration(unsigned cosetid)
+{
+	double aa = 0.0;
+	for(unsigned d = 0; d < 3; d++)
+		aa += _universalAdditionalAcceleration[d][cosetid] * _universalAdditionalAcceleration[d][cosetid];
+	return sqrt(aa);
+}
+double PressureGradient::getUniformAcceleration(unsigned cosetid, unsigned d)
+{
+	return this->_universalAdditionalAcceleration[d][cosetid];
+}
+
+double PressureGradient::getMissingVelocity(unsigned cosetid, unsigned d)
+{
+	double vd = this->_globalVelocitySum[d][cosetid] / this->_globalN[cosetid];
+	return this->_globalTargetVelocity[d][cosetid] - vd;
+}
+
+double* PressureGradient::getTargetVelocity(unsigned set)
+{
+   double* retv = new double[3];
+   for(int d=0; d < 3; d++)
+      retv[d] = this->_globalTargetVelocity[d][set];
+   return retv;
+}
+
+double* PressureGradient::getAdditionalAcceleration(unsigned set)
+{
+   double* retv = new double[3];
+   for(int d=0; d < 3; d++)
+      retv[d] = this->_universalAdditionalAcceleration[d][set];
+   return retv;
+}
+
---
+++ src/ensemble/PressureGradient.h
@@ -0,0 +1,112 @@
+/*************************************************************************
+ * Copyright (C) 2010 by Martin Bernreuther <bernreuther@hlrs.de> et al. *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or (at *
+ * your option) any later version.                                       *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful, but   *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of            * 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ * General Public License for more details.                              *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the Free Software           *
+ * Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.   *
+ *************************************************************************/
+
+#ifndef PRESSUREGRADIENT_H_
+#define PRESSUREGRADIENT_H_
+
+#include <deque>
+#include <map>
+
+class DomainDecompBase;
+class ParticleContainer;
+
+class PressureGradient
+{
+ public:
+	PressureGradient(int rank) { this->_localRank = rank; };
+
+	//! @brief assigns a coset ID to a component (ID)
+	void assignCoset(unsigned cid, unsigned cosetid) { _universalComponentSetID[cid] = cosetid; }
+	//! @brief sets the information on the acceleration model for one coset
+	void specifyComponentSet(unsigned cosetid, double v[3], double tau, double ainit[3], double timestep);
+	//! @brief sets the number of timesteps between two updates of the uniform acceleration
+	void setUCAT(unsigned uCAT) { this->_universalConstantAccelerationTimesteps = uCAT; }
+	//! @brief returns the number of timesteps between two updates of the uniform acceleration
+	unsigned getUCAT() { return this->_universalConstantAccelerationTimesteps; }
+	//! @brief are there any cosets?
+	bool isAcceleratingUniformly() {
+		return ( (this->_universalTau.size() > 0)
+		   && (this->_universalConstantAccelerationTimesteps > 0) );
+	}
+	//! @brief updates the intensity and direction of the uniform acceleration
+	void determineAdditionalAcceleration(
+		DomainDecompBase* domainDecomp,
+		ParticleContainer* molCont, double dtConstantAcc
+	);
+	//! @brief returns the acceleration map (necessary for passing data to the integrator)
+	std::map<unsigned, double>* getUAA() { return this->_universalAdditionalAcceleration; }
+	//! @brief returns the cosetid of a component (0 for unaccelerated components)
+	unsigned getComponentSet(unsigned cid)
+	{
+		if(_universalComponentSetID.find(cid) == _universalComponentSetID.end()) return 0;
+		else return this->_universalComponentSetID[cid];
+	}
+	//! @brief returns the directed velocity for a component set
+	//! @param cosetid ID of the component set
+	//! @param d x direction (0), y direction (1), or z direction (2)
+	double getDirectedVelocity(unsigned cosetid, unsigned d);
+	//! @brief returns the absolute external acceleration for a component set
+	//! @param cosetid ID of the component set
+	double getUniformAcceleration(unsigned cosetid);
+	//! @brief returns the external acceleration for a component set
+	//! @param cosetid ID of the component set
+	//! @param d x direction (0), y direction (1), or z direction (2)
+	double getUniformAcceleration(unsigned cosetid, unsigned d);
+	//! @brief returns the difference between the desired velocity and the global average velocity
+	double getMissingVelocity(unsigned cosetid, unsigned d);
+	//! @brief total number of particles that belong to the specified component set
+	double getCosetN(unsigned cosetid) { return this->_globalN[cosetid]; }
+	unsigned maxCoset() { return this->_universalTau.size(); }
+
+	//! @brief returns the component -> set ID map
+	std::map<unsigned, unsigned> getComponentSets() { return this->_universalComponentSetID; }
+
+	std::map<unsigned, double> getTau() { return this->_universalTau; }
+	double getTau(unsigned set) { return this->_universalTau[set]; }
+
+	double* getTargetVelocity(unsigned set);
+	double* getAdditionalAcceleration(unsigned set);
+
+ private:
+	unsigned _localRank;
+
+	/// calculate new value of the uniform acceleration each # timesteps
+	unsigned _universalConstantAccelerationTimesteps;
+	/// assigns a component set ID to some of the components
+	std::map<unsigned, unsigned> _universalComponentSetID;
+	/// local number of molecules that belong to a given component set ID
+	std::map<unsigned, unsigned long> _localN;
+	/// global number of molecules that belong to a given component set ID
+	std::map<unsigned, unsigned long> _globalN;
+	/// local sum of the velocity vectors corresponding to a given component set ID
+	std::map<unsigned, long double> _localVelocitySum[3];
+	/// global sum of the velocity vectors corresponding to a given component set ID
+	std::map<unsigned, long double> _globalVelocitySum[3];
+	/// uniform acceleration
+	std::map<unsigned, double> _universalAdditionalAcceleration[3];
+	/// target average velocity for the molecules of a coset
+	std::map<unsigned, double> _globalTargetVelocity[3];
+	/// delay variable tau of a coset
+	std::map<unsigned, double> _universalTau;
+	/// queue of previously recorded velocity sums
+	std::map<unsigned, std::deque<long double> > _globalPriorVelocitySums[3];
+	/// number of items in the velocity queue
+	std::map<unsigned, unsigned> _globalVelocityQueuelength;
+};
+#endif
+
