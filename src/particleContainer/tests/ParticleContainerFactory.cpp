e951d29b84cba72cd30ec8cf1fe5a50357b5940d
Wolfgang Eckhardt <eckhardw@in.tum.de>
2011-02-19 15:09:44 +0000

    extended ParticleContainerFactory, so that the Domain and ParticleContainer can be set up from a phasespace file.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@471 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/particleContainer/tests/ParticleContainerFactory.cpp
+++ src/particleContainer/tests/ParticleContainerFactory.cpp
@@ -8,8 +8,16 @@
 #include "particleContainer/tests/ParticleContainerFactory.h"
 #include "particleContainer/ParticleContainer.h"
 #include "particleContainer/LinkedCells.h"
+#include "particleContainer/AdaptiveSubCells.h"
 
+#include "ensemble/GrandCanonical.h"
+#include "parallel/DomainDecompBase.h"
+#include "Domain.h"
+
+#include "io/InputOldstyle.h"
 #include "utils/Logger.h"
+
+#include <list>
 
 using namespace Log;
 
@@ -30,4 +38,40 @@ ParticleContainer* ParticleContainerFactory::createEmptyParticleContainer(type t
 		global_log->error() << "ParticleContainerFactory: Unsupported type requested! " << std::endl;
 		return NULL;
 	}
+}
+
+
+
+ParticleContainer* ParticleContainerFactory::createInitializedParticleContainer(
+		type type, Domain* domain, DomainDecompBase* domainDecomposition, double cutoff, const std::string& fileName) {
+
+	InputOldstyle inputReader;
+	inputReader.setPhaseSpaceHeaderFile(fileName.c_str());
+	inputReader.setPhaseSpaceFile(fileName.c_str());
+	inputReader.readPhaseSpaceHeader(domain, 1.0);
+
+	double bBoxMin[3];
+	double bBoxMax[3];
+	for (int i = 0; i < 3; i++) {
+		bBoxMin[i] = domainDecomposition->getBoundingBoxMin(i, domain);
+		bBoxMax[i] = domainDecomposition->getBoundingBoxMax(i, domain);
+	}
+
+	ParticleContainer* moleculeContainer;
+	if (type == LinkedCell) {
+		moleculeContainer = new LinkedCells(bBoxMin, bBoxMax, cutoff, cutoff, cutoff, 1.0, NULL);
+	} else if (type == AdaptiveSubCell) {
+		moleculeContainer = new AdaptiveSubCells(bBoxMin, bBoxMax, cutoff, cutoff, cutoff, NULL);
+	} else {
+		global_log->error() << "ParticleContainerFactory: Unsupported type requested! " << std::endl;
+		return NULL;
+	}
+
+	std::list<ChemicalPotential> chemPot;
+	inputReader.readPhaseSpace(moleculeContainer, &chemPot, domain, domainDecomposition);
+	moleculeContainer->update();
+	moleculeContainer->deleteOuterParticles();
+
+	domain->initParameterStreams(cutoff, cutoff);
+	return moleculeContainer;
 }
--- src/particleContainer/tests/ParticleContainerFactory.h
+++ src/particleContainer/tests/ParticleContainerFactory.h
@@ -8,7 +8,11 @@
 #ifndef PARTICLECONTAINERFACTORY_H_
 #define PARTICLECONTAINERFACTORY_H_
 
+#include <string>
+
 class ParticleContainer;
+class Domain;
+class DomainDecompBase;
 
 /**
  * This is a factory class to create different types of particleContainers,
@@ -18,13 +22,24 @@ class ParticleContainerFactory {
 
 public:
 
-	enum type {LinkedCell};
+	enum type {LinkedCell, AdaptiveSubCell};
 
 	/**
 	 * creates a small, empty particleContainer. For the moment, only LinkedCell is supported.
 	 * The caller is responsible for deleting the pointer!
 	 */
 	static ParticleContainer* createEmptyParticleContainer(type type);
+
+	/**
+	 * return a (more or less - rather less ;) initialized Particlecontainer. Actually, only the
+	 * given file is read, and the domain is set up correspondingly to the file, and after that the
+	 * container is initialized from the file. For the domain, the parameter streams are initialized,
+	 * but no further initialization methods are called on any class!
+	 *
+	 * The caller is responsible for deleting the pointer!
+	 */
+	static ParticleContainer* createInitializedParticleContainer(
+			type type, Domain* domain, DomainDecompBase* domainDecomposition, double cutoff, const std::string& fileName);
 
 };
 
