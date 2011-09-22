612f602bf66b6e5e57042b33bb1550fae1ec2f58
Wolfgang Eckhardt <eckhardw@in.tum.de>
2011-09-22 13:12:47 +0000

    replaced ifs by asserts again.
    The reason which I believed to have for this seems to not exist any more... ;)
    
    
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@763 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/molecules/Molecule.h
+++ src/molecules/Molecule.h
@@ -54,15 +54,12 @@ public:
 	Molecule(const Molecule& m);
 
 	~Molecule() {
-		if (_sites_d) {
-			delete[] _sites_d;
-		}
-		if (_osites_e) {
-			delete[] _osites_e;
-		}
-		if (_sites_F) {
-			delete[] _sites_F;
-		}
+		assert(_sites_d);
+		delete[] _sites_d;
+		assert(_osites_e);
+		delete[] _osites_e;
+		assert(_sites_F);
+		delete[] _sites_F;
 	}
 
 	/** get molecule ID */
