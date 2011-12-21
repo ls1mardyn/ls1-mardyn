3b02b711b4f8915e424c02b9239bca40b277fbec
Christoph Niethammer <niethammer@hlrs.de>
2011-12-21 13:45:48 +0000

    Fixing compilation problem with vtk.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@884 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- makefile/vtkwriter.mk
+++ makefile/vtkwriter.mk
@@ -4,6 +4,6 @@ $(info included vtkwriter.mk!)
 VTK_SOURCES = $(shell find ./ -name "*.cpp" | grep -v "/tests/" | grep "/vtk/")
 
 SOURCES += $(VTK_SOURCES)
-INCLUDES += -I../dependencies-external/libxsd
-LDFLAGS += -lxerces-c
 CXXFLAGS += -DVTK
+INCLUDES += -I../dependencies-external/libxsd -I$(VTK_INCDIR)
+LDFLAGS += -lxerces-c -L$(VTK_LIBDIR)
