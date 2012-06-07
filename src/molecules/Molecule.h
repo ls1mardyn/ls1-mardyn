1e7bb7c2f463898c87da745bd45a0cf4cf0f016f
Wolfgang Eckhardt <eckhardw@in.tum.de>
2012-06-07 12:57:52 +0000

    moved method minusSiteSiteDistance to Molecule.h, too, as it was not so unused...
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@1147 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/molecules/Molecule.h
+++ src/molecules/Molecule.h
@@ -323,4 +323,12 @@ inline void SiteSiteDistance(const double drm[3], const double ds1[3], const dou
 	dr2 = drs[0]*drs[0] + drs[1]*drs[1] + drs[2]*drs[2];
 }
 
+
+inline void minusSiteSiteDistance(const double drm[3], const double ds1[3], const double ds2[3], double drs[3], double& dr2)
+{
+	for (unsigned short d = 0; d < 3; ++d)
+		drs[d] = ds2[d] - drm[d] - ds1[d];
+	dr2 = drs[0]*drs[0] + drs[1]*drs[1] + drs[2]*drs[2];
+}
+
 #endif /*MOLECULE_H_*/
