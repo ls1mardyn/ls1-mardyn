a34c627e231623126131319419e32211a6849261
Kovacevic Tijana <tijana.kovachevic@gmail.com>
2012-06-26 15:04:31 +0000

    unit tests for particle insertion
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@1183 a63bd714-7e14-4b5e-94e7-302c8c8ff188

---
+++ src/tests/ParticleInsertionTest.h
@@ -0,0 +1,42 @@
+/*
+ * ParticleInsertionTest.h
+ *
+ *  Created on: Jun 18, 2012
+ *      Author: tijana
+ */
+
+#ifndef PARTICLEINSERTIONTEST_H_
+#define PARTICLEINSERTIONTEST_H_
+
+#include "utils/TestWithSimulationSetup.h"
+#include "Simulation.h"
+#include "molecules/Molecule.h"
+#include "particleContainer/LinkedCells.h"
+#include "MoleculeWrapper.h"
+#include "ParticleInsertion.h"
+#include <sstream>
+
+#include "particleContainer/adapter/ParticlePairs2PotForceAdapter.h"
+
+class ParticleInsertionTest: public utils::TestWithSimulationSetup {
+	TEST_SUITE( ParticleInsertionTest);
+	TEST_METHOD( testRotation);
+	TEST_METHOD( testTranslationAndRotation);
+	TEST_METHOD( testParameterSetup);
+	TEST_SUITE_END();
+public:
+	ParticleInsertionTest();
+	virtual ~ParticleInsertionTest();
+
+	void testRotation();
+
+	void testTranslationAndRotation();
+
+	void testParameterSetup();
+
+	void readParamFile(string file_name, int* maxIter, int* maxRestarts, int* maxRotations, double* tolerance,
+			double* maxAngle, double* maxAllowedAngle, double* minAngle, bool* largeStepsizeOnOverlap,
+			bool* restartIfIncreases);
+};
+
+#endif /* PARTICLEINSERTIONTEST_H_ */
