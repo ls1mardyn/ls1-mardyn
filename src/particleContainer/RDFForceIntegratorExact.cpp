f2a2bb8bdcd21d81b7102751cf6f7a26ef3bc334
Kovacevic Tijana <tijana.kovachevic@gmail.com>
2012-09-07 21:25:33 +0000

    optimizations of the exact integrator
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/tijana@1391 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/particleContainer/RDFForceIntegratorExact.cpp
+++ src/particleContainer/RDFForceIntegratorExact.cpp
@@ -19,6 +19,7 @@ int RDFForceIntegratorExact::_n_alpha = 0;
 double RDFForceIntegratorExact::_dx = 0;
 double RDFForceIntegratorExact::_dy = 0;
 double RDFForceIntegratorExact::_dz = 0;
+double RDFForceIntegratorExact::_rho = 0;
 
 double* RDFForceIntegratorExact::_scaling_factors = NULL;
 
@@ -101,7 +102,7 @@ void RDFForceIntegratorExact::precomputeScalingFactors() {
 	_d_level = _dn;
 	_n_levels = (int) ((_rc + _extension) / _d_level + 0.5) + 1;
 	_n_n = (int) (2 * _extension / _dn + 0.5) + 1;
-	_d_alpha = 20;
+	_d_alpha = 30;
 	_n_r = (int) ((_rc + _extension) / _dr + 0.5) + 1;
 	_n_alpha = (int) (360 / _d_alpha + 0.5);
 	_scaling_factors = new double[_n_levels * _n_n * _n_r];
@@ -179,30 +180,30 @@ void RDFForceIntegratorExact::getScalingFactor(double* mol_r, double* site_r,
 		double x, double y, double z, int site_i, double* scale) {
 
 	double above[2], below[2], curr_g[2], curr_x, curr_y, curr_z, curr_r,
-			alpha, phi, other_site_x, other_site_y, other_site_z, other_site_r;
+			alpha, phi, other_site_x, other_site_y, other_site_z, other_site_r,
+			ext_sin, ext_cos, sin_phi, cos_phi;
 	int curr_bin;
 
 	above[0] = above[1] = below[0] = below[1] = 0;
 
 	for (int idx_alpha = 0; idx_alpha < _n_alpha; idx_alpha++) {
 		alpha = idx_alpha * _d_alpha;
-
-		curr_x = x + _extension * sin(alpha * PI / 180);
-		other_site_x = x + 2 * _extension * sin(alpha * PI / 180);
+		ext_sin = _extension * sin(alpha * PI / 180);
+		ext_cos = _extension * sin(alpha * PI / 180);
+		curr_x = x + ext_sin;
+		other_site_x = x + 2 * ext_sin;
 
 		for (int idx_phi = 0; idx_phi < _n_alpha; idx_phi++) {
 			phi = idx_phi * _d_alpha;
-			curr_y = y + _extension * cos(alpha * PI / 180) * cos(phi * PI
-					/ 180);
+			cos_phi = cos(phi * PI / 180);
+			sin_phi = sin(phi * PI / 180);
+			curr_y = y + ext_cos * cos_phi;
 
-			curr_z = z + _extension * cos(alpha * PI / 180) * sin(phi * PI
-					/ 180);
+			curr_z = z + ext_cos * sin_phi;
 
-			other_site_y = y + 2 * _extension * cos(alpha * PI / 180) * cos(phi
-					* PI / 180);
+			other_site_y = y + 2 * ext_cos * cos_phi;
 
-			other_site_z = z + 2 * _extension * cos(alpha * PI / 180) * sin(phi
-					* PI / 180);
+			other_site_z = z + 2 * ext_cos * sin_phi;
 
 			curr_r = std::sqrt((curr_x - mol_r[0]) * (curr_x - mol_r[0])
 					+ (curr_y - mol_r[1]) * (curr_y - mol_r[1]) + (curr_z
@@ -267,6 +268,15 @@ void RDFForceIntegratorExact::traverseMolecules() {
 	_extension = currentMolecule->ljcenter_disp(0);
 	_dx = _dy = _dz = _dr = _dn = currentMolecule->getSigma() / 10;
 	precomputeScalingFactors();
+
+	// volume of the domain
+	double V = (_rmax[0] - _rmin[0]) * (_rmax[1] - _rmin[1]) * (_rmax[2]
+			- _rmin[2]);
+
+	// number density of the domain
+	//int numMolecules = _moleculeContainer->getNumberOfParticles();
+	_numMolecules = 9826;
+	_rho = _numMolecules / (V);
 
 	// iterate through molecules and add rdf influence
 	for (currentMolecule = _moleculeContainer->begin(); currentMolecule
@@ -451,15 +461,6 @@ void RDFForceIntegratorExact::integrateRDFSiteCartesian(double xlim[2],
 	std::vector<double> globalAcc = (*_globalADist)[0];
 	std::vector<std::vector<double> > globalSiteAcc = (*_globalSiteADist)[0];
 
-	// volume of the domain
-	double V = (_rmax[0] - _rmin[0]) * (_rmax[1] - _rmin[1]) * (_rmax[2]
-			- _rmin[2]);
-
-	// number density of the domain
-	//int numMolecules = _moleculeContainer->getNumberOfParticles();
-	_numMolecules = 9826;
-	double rho = _numMolecules / (V);
-
 	// molecule position
 
 	double molr[3] = { mol->r(0), mol->r(1), mol->r(2) };
@@ -467,7 +468,8 @@ void RDFForceIntegratorExact::integrateRDFSiteCartesian(double xlim[2],
 			+ mol->site_d(site)[1], mol->r(2) + mol->site_d(site)[2] };
 
 	int bin; // bin for the radius that rdf is read for
-	double currf[3], absmold, allowed_dist, small_y, small_z, abssited, g = 0;
+	double currf[3], absmold, allowed_dist, small_y, small_z, abssited;
+	double g[2] = { 0, 0 };
 	double scale[2] = { 1, 1 };
 	// dividing part of the sphere outside the bounding box into cells of size
 	// dx, dy, dz
@@ -493,7 +495,7 @@ void RDFForceIntegratorExact::integrateRDFSiteCartesian(double xlim[2],
 					continue;
 				}
 
-				double sited[3] = { siter[0] - x, siter[1] - y, siter[2] - z};
+				double sited[3] = { siter[0] - x, siter[1] - y, siter[2] - z };
 
 				abssited = std::sqrt(sited[0] * sited[0] + sited[1] * sited[1]
 						+ sited[2] * sited[2]);
@@ -513,65 +515,72 @@ void RDFForceIntegratorExact::integrateRDFSiteCartesian(double xlim[2],
 
 				}
 
-				if (mol->numSites() == 1) {
-					bin = (int) (abssited * globalAcc.size() / (_rc + 2
-							* _extension) - 0.5);
+				// if multiple LJ centers, use site-site rdf
+				// iterate through sites, treat cell center as a site
 
-					g = globalAcc[bin];
-					double currPot = 0;
-					PotForceLJ(sited, abssited * abssited, 24 * mol->getEps(),
-							mol->getSigma() * mol->getSigma(), currf, currPot);
 
-					double f[3] = { 0, 0, 0 };
+				// rdf (probability) value
+				bin = (int) (abssited * globalSiteAcc[site
+						* mol->numLJcenters() + site].size() / (_rc + 2
+						* _extension) - 0.5);
 
-					for (int d = 0; d < 3; d++) {
-						f[d] = rho * g * currf[d] * _dx * _dy * _dz;
-					}
+				g[0] = globalSiteAcc[site * mol->numLJcenters() + 0][bin];
+				g[1] = globalSiteAcc[site * mol->numLJcenters() + 1][bin];
+				if (site != 0)
+					g[0] /= 2;
 
-					//mol->Fljcenteradd(site, f);
-					if (boundary[0] == -1 && plane == 0) {
-						mol->addLeftxRdfInfluence(site, f);
-					}
+				if (site != 1)
+					g[1] /= 2;
 
-				} else {
-					// if multiple LJ centers, use site-site rdf
-					// iterate through sites, treat cell center as a site
+				g[0] *= scale[0];
+				g[1] *= scale[1];
 
-					for (unsigned int j = 0; j < mol->numLJcenters(); j++) {
+				double currPot = 0;
+				PotForceLJ(sited, abssited * abssited, 24 * mol->getEps(),
+						mol->getSigma() * mol->getSigma(), currf, currPot);
 
-						// rdf (probability) value
-						bin = (int) (abssited * globalSiteAcc[site
-								* mol->numLJcenters() + site].size() / (_rc + 2
-								* _extension) - 0.5);
+				//currPot *= 2 * PI * rho * g * radial * dx * dy * dz / 6;
+				double f0[3] = { 0, 0, 0 };
+				double f1[3] = { 0, 0, 0 };
 
-						g = globalSiteAcc[site * mol->numLJcenters() + j][bin];
-						if (site != j)
-							g /= 2;
-
-						g *= scale[j];
-						double currPot = 0;
-						PotForceLJ(sited, abssited * abssited, 24
-								* mol->getEps(), mol->getSigma()
-								* mol->getSigma(), currf, currPot);
-
-						//currPot *= 2 * PI * rho * g * radial * dx * dy * dz / 6;
-						double f[3] = { 0, 0, 0 };
-
-						for (int d = 0; d < 3; d++) {
-							f[d] = rho * g * currf[d] * _dx * _dy * _dz;
-						}
-						//if (f[0] > 0 && mol->id() == 18) cout<<"site: "<<site<<" r: "<<absr<<" value: "<<f[0]<<endl;
-
-						//mol->Fljcenteradd(site, f);
-						if (boundary[0] == -1 && plane == 0) {
-							mol->addLeftxRdfInfluence(site, f);
-						}
-
-					}
+				for (int d = 0; d < 3; d++) {
+					f0[d] = _rho * g[0] * currf[d] * _dx * _dy * _dz;
+					f1[d] = _rho * g[1] * currf[d] * _dx * _dy * _dz;
 				}
+				//if (f[0] > 0 && mol->id() == 18) cout<<"site: "<<site<<" r: "<<absr<<" value: "<<f[0]<<endl;
+
+				//mol->Fljcenteradd(site, f);
+				//if (boundary[0] == -1 && plane == 0) {
+				mol->addLeftxRdfInfluence(site, f0);
+				mol->addLeftxRdfInfluence(site, f1);
+				//}
+
 			}
+
 		}
 
 	}
 
 }
+/*
+ * if (mol->numSites() == 1) {
+ bin = (int) (abssited * globalAcc.size() / (_rc + 2
+ * _extension) - 0.5);
+
+ g = globalAcc[bin];
+ double currPot = 0;
+ PotForceLJ(sited, abssited * abssited, 24 * mol->getEps(),
+ mol->getSigma() * mol->getSigma(), currf, currPot);
+
+ double f[3] = { 0, 0, 0 };
+
+ for (int d = 0; d < 3; d++) {
+ f[d] = rho * g * currf[d] * _dx * _dy * _dz;
+ }
+
+ //mol->Fljcenteradd(site, f);
+ if (boundary[0] == -1 && plane == 0) {
+ mol->addLeftxRdfInfluence(site, f);
+ }
+
+ } */
