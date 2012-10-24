01906482fd1ddb834119b780a7f7f06548023c2d
Kovacevic Tijana <tijana.kovachevic@gmail.com>
2012-10-24 17:17:41 +0000

    removed additional debugging output
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/tijana@1551 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/parallel/DomainDecompDummy.cpp
+++ src/parallel/DomainDecompDummy.cpp
@@ -68,10 +68,7 @@ double DomainDecompDummy::getAverageEnergy(LinkedCells* linkedCells,
 	double* force = new double[3]; // placeholder
 	int num = 0;
 	Molecule* currentMolecule;
-	std::cout << "calling for: " << low_limit[0] << " " << low_limit[1] << " "
-			<< low_limit[2] << std::endl;
-	std::cout << "high: " << high_limit[0] << " " << high_limit[1] << " "
-			<< high_limit[2] << std::endl;
+
 	for (currentMolecule = linkedCells->begin(); currentMolecule
 			!= linkedCells->end(); currentMolecule = linkedCells->next()) {
 		if (currentMolecule->r(0) < high_limit[0] && currentMolecule->r(0)
