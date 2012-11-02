b9181c682514ddf67c5e6f84b57dc24291ef41aa
Kovacevic Tijana <tijana.kovachevic@gmail.com>
2012-11-02 18:47:58 +0000

    changed to what I was running at my machine since I couldnt run unit tests on atsccs machines
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/tijana@1579 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/tests/ParticleInsertionTest.cpp
+++ src/tests/ParticleInsertionTest.cpp
@@ -17,202 +17,73 @@ ParticleInsertionTest::ParticleInsertionTest() {
 ParticleInsertionTest::~ParticleInsertionTest() {
 	// TODO Auto-generated destructor stub
 }
+
+void ParticleInsertionTest::haloUsherTest() {
+	double boundings_min[] = { 0, 0, 0 };
+	double boundings_max[] = { 33.4278, 33.4278, 33.4278 };
+
+	LinkedCells linkedCells(boundings_min, boundings_max, 10, 10, 10, 1);
+	std::vector<Component> components;
+
+	// component - 2 LJ centers
+	Component dummyComponent(0);
+	dummyComponent.addLJcenter(0, 0, 0, 0.015, 0.0004, 6.6, 0, false);
+	//		dummyComponent.addLJcenter(1, 1, 0, 0.015, 0.0004, 6.6, 0,
+	//				false);
+	components.push_back(dummyComponent);
+
+	// domain
+	int ownrank = 0;
+	PressureGradient* pressureGradient = new PressureGradient(ownrank);
+	_domain = new Domain(ownrank, pressureGradient);
+	_domain->addComponent(dummyComponent);
+	_domain->initParameterStreams(10, 10);
+
+	// particle handler for the linked cells
+	ParticlePairsHandler* _particlePairsHandler =
+			new ParticlePairs2PotForceAdapter(*_domain);
+	linkedCells.setPairsHandler(_particlePairsHandler);
+
+	// molecules 1 and 2
+	Molecule dummyMolecule1(0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+			&components);
+	dummyMolecule1.upd_cache();
+	linkedCells.addParticle(dummyMolecule1);
+	linkedCells.update();
+
+	Molecule dummyMolecule2(1, 0, 2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+			&components);
+	dummyMolecule2.upd_cache();
+	linkedCells.addParticle(dummyMolecule2);
+	linkedCells.update();
+
+	//  ParticleInsertion instance
+	moleculardynamics::coupling::ParticleInsertion<Molecule, LinkedCells, 3>
+			insertion;
+
+	// molecule to be inserted
+	Molecule newMolecule(2, 0, -1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+			&components);
+	double f[3] = { 0, 0, 0 };
+	double u = linkedCells.getForceAndEnergy(&newMolecule, f);
+	double u_exp1 = 4 * 0.0004 * (std::pow(6.6 / 2, 12) - std::pow(6.6 / 2, 6));
+	double u_exp2 = 4 * 0.0004 * (std::pow(6.6 / 3, 12) - std::pow(6.6 / 3, 6));
+	std::cout << "expected: " << u_exp1 << " + " << u_exp2 << " = " << u_exp1
+			+ u_exp2 << std::endl;
+	std::cout << "actual: " << u << std::endl;
+	std::cout << "force getFandE: " << f[0] << " " << f[1] << " " << f[2]
+			<< std::endl;
+	linkedCells.traversePairs(_particlePairsHandler);
+	newMolecule.calcFM();
+	std::cout << "after blockt: " << newMolecule.F(0) << " "
+			<< newMolecule.F(1) << " " << newMolecule.F(2) << std::endl;
+	ASSERT_DOUBLES_EQUAL(u_exp1 + u_exp2, u, 1e-5);
+}
+
 //FIXME: Correct cutoff for linkedCells
-void ParticleInsertionTest::testRotation() {
 
-	// domain bounding box
-	double boundings_min[] = { 0, 0, 0 };
-	double boundings_max[] = { 33.4278, 33.4278, 33.4278 };
 
-	LinkedCells linkedCells(boundings_min, boundings_max, 100, 100, 3, 1);
-	std::vector<Component> components;
-
-	// component - 2 LJ centers
-	Component dummyComponent(0);
-	dummyComponent.addLJcenter(-1, -1, 0, 0.015, 0.000429325357, 6.61404407, 0,
-			false);
-	dummyComponent.addLJcenter(1, 1, 0, 0.015, 0.000429325357, 6.61404407, 0,
-			false);
-	components.push_back(dummyComponent);
-
-	// domain
-	int ownrank = 0;
-	PressureGradient* pressureGradient = new PressureGradient(ownrank);
-	_domain = new Domain(ownrank, pressureGradient);
-	_domain->addComponent(dummyComponent);
-	_domain->initParameterStreams(100, 100);
-
-	// particle handler for the linked cells
-	ParticlePairsHandler* _particlePairsHandler =
-			new ParticlePairs2PotForceAdapter(*_domain);
-	linkedCells.setPairsHandler(_particlePairsHandler);
-
-	// molecules 1 and 2
-	Molecule dummyMolecule1(0, 0, 8, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-	dummyMolecule1.upd_cache();
-	linkedCells.addParticle(dummyMolecule1);
-	linkedCells.update();
-
-	Molecule dummyMolecule2(1, 0, 18, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-	dummyMolecule2.upd_cache();
-	linkedCells.addParticle(dummyMolecule2);
-	linkedCells.update();
-
-	//  ParticleInsertion instance
-	moleculardynamics::coupling::ParticleInsertion<Molecule, LinkedCells, 3>
-			insertion;
-
-	// molecule to be inserted
-	Molecule newMolecule(2, 0, 13, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-
-	int num_tests = 3;
-	vector<Quaternion> qs;
-	qs.push_back(Quaternion(9.453842e-01, 1.701856e-01, 1.283354e-02,
-			2.777065e-01));
-	double angle = -PI / 4;
-	double torque[3] = { 0, 0, 1 };
-	qs.push_back(Quaternion(cos(angle / 2), torque[0] * sin(angle / 2),
-			torque[1] * sin(angle / 2), torque[2] * sin(angle / 2)));
-	qs.push_back(Quaternion(6.711773e-01, -6.365373e-01, 3.561545e-01,
-			1.322697e-01));
-
-	double equals[3] = { 0.944337, 5.01764, 0.944164 };
-
-	double targets[3] = { 0.943416768341188, 5.015456e+00, 0.943416768341188 };
-
-	moleculardynamics::coupling::interface::MardynMoleculeWrapper<Molecule, 3>
-			wrapper(&newMolecule);
-	for (int i = 0; i < num_tests; i++) {
-		Quaternion q = qs[i];
-		newMolecule.setq(q);
-		newMolecule.upd_cache();
-
-		// calculate starting energy & force
-		double force[3] = { 0, 0, 0 };
-		newMolecule.clearFM();
-
-		double energy = linkedCells.getForceAndEnergy(&newMolecule, force);
-		cout << "starting energy " << energy << endl;
-		wrapper.setForce(force);
-
-		// allocate vectors for logging energy, angle, lj position and center position
-		vector<double> vec_energy;
-		vector<double> vec_angle;
-		vector<double*> vec_lj;
-		vector<double*> vec_center;
-		string name_energy = "energy.txt";
-		string name_angle = "angle.txt";
-		string name_lj = "lj.txt";
-		string name_center = "center.txt";
-
-		int timestep = 0;
-		double energy_old = energy;
-
-		// perform insertion
-		insertion.rotateMolecule(&newMolecule, wrapper, &linkedCells, 1000, 45
-				* 3.14 / 180, 3.14, 0.1 * PI / 180, targets[i], 10000
-				* newMolecule.getEps(), &timestep, 0.001, &energy, &energy_old,
-				force, force, &q, 0, 1, &vec_energy, &vec_angle, &vec_lj,
-				&vec_center, name_energy, name_angle, name_lj, name_center);
-
-		ASSERT_DOUBLES_EQUAL(equals[i], energy, 1e-5);
-	}
-
-}
-
-void ParticleInsertionTest::testTranslationAndRotation() {
-	// domain bounding box
-	double boundings_min[] = { 0, 0, 0 };
-	double boundings_max[] = { 33.4278, 33.4278, 33.4278 };
-
-	LinkedCells linkedCells(boundings_min, boundings_max, 100, 100, 3, 1);
-	std::vector<Component> components;
-
-	// component - 2 LJ centers
-	Component dummyComponent(0);
-	dummyComponent.addLJcenter(-1, -1, 0, 0.015, 0.000429325357, 6.61404407, 0,
-			false);
-	dummyComponent.addLJcenter(1, 1, 0, 0.015, 0.000429325357, 6.61404407, 0,
-			false);
-	components.push_back(dummyComponent);
-
-	// domain
-	int ownrank = 0;
-	PressureGradient* pressureGradient = new PressureGradient(ownrank);
-	_domain = new Domain(ownrank, pressureGradient);
-	_domain->addComponent(dummyComponent);
-	_domain->initParameterStreams(100, 100);
-
-	// particle handler for the linked cells
-	ParticlePairsHandler* _particlePairsHandler =
-			new ParticlePairs2PotForceAdapter(*_domain);
-	linkedCells.setPairsHandler(_particlePairsHandler);
-
-	// molecules 1 and 2
-	Molecule dummyMolecule1(0, 0, 8, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-	dummyMolecule1.upd_cache();
-	linkedCells.addParticle(dummyMolecule1);
-	linkedCells.update();
-
-	Molecule dummyMolecule2(1, 0, 18, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-	dummyMolecule2.upd_cache();
-	linkedCells.addParticle(dummyMolecule2);
-	linkedCells.update();
-
-	//  ParticleInsertion instance
-	moleculardynamics::coupling::ParticleInsertion<Molecule, LinkedCells, 3>
-			insertion;
-
-	// molecule to be inserted
-	Molecule newMolecule(2, 0, 13, 8, 8, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-			&components);
-
-	int seeds[2] = { 4, 5 };
-	double equals[2] = { 0.943703, 0.943541 };
-
-	int num_tests = 2;
-
-	ParticleCell cell = linkedCells.getCell(linkedCells.getCellIndexOfMolecule(
-			linkedCells.begin()));
-	moleculardynamics::coupling::interface::MardynMoleculeWrapper<Molecule, 3>
-			wrapper(&newMolecule);
-
-	for (int i = 0; i < num_tests; i++) {
-		// calculate starting energy & force
-		double force[3] = { 0, 0, 0 };
-		newMolecule.clearFM();
-
-		double energy = linkedCells.getForceAndEnergy(&newMolecule, force);
-		cout << "starting energy " << energy << endl;
-		wrapper.setForce(force);
-
-		// allocate for logging
-		vector<double> vec_energy;
-		vector<double> vec_angle;
-		vector<double*> vec_lj;
-		vector<double*> vec_center;
-		string name_energy = "energy.txt";
-		string name_angle = "angle.txt";
-		string name_lj = "lj.txt";
-		string name_center = "center.txt";
-		double target = 0.943416768341188;
-		double energy_old = energy;
-
-		insertion.findParticlePosition(&linkedCells, cell, &newMolecule,
-				target, &energy, &energy_old, 0, 1, seeds[i], 100, 100, 100, 45
-						* PI / 180, 3.14, 0.1 * PI / 180, 0.001, &vec_energy,
-				&vec_angle, &vec_lj, &vec_center, name_energy, name_angle,
-				name_lj, name_center);
-		ASSERT_DOUBLES_EQUAL(equals[i], energy, 1e-5);
-
-	}
-}
-
+//FIXME: where should I be inserting???
 void ParticleInsertionTest::testParametersFullStudy() {
 	LinkedCells* linkedCells = (LinkedCells*) initializeFromFile(
 			ParticleContainerFactory::LinkedCell, "Ethan_20k_equilibrated.inp",
@@ -225,9 +96,9 @@ void ParticleInsertionTest::testParametersFullStudy() {
 	// component - 2 LJ centers
 	Component dummyComponent(0);
 	dummyComponent.addLJcenter(0, 0, -2.2157048, 0.0150347, 0.00042932536,
-				6.6140441, 0, 0);
+			6.6140441, 0, 0);
 	dummyComponent.addLJcenter(0, 0, 2.2157048, 0.0150347, 0.00042932536,
-				6.6140441, 0, 0);
+			6.6140441, 0, 0);
 	components.push_back(dummyComponent);
 	_domain = new Domain(ownrank, pressureGradient);
 	_domain->addComponent(dummyComponent);
@@ -264,19 +135,24 @@ void ParticleInsertionTest::testParametersFullStudy() {
 	int num_seeds = 3;
 	FILE* fresult = fopen("result_parameter_study.txt", "w");
 	int numTranslations[30] = { 10, 20, 30, 40, 50, 75, 100, 150, 200, 250,
-			300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
-	int numRotations[30] = {10, 20, 30, 40, 50, 75, 100, 150, 200, 250,
-			300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+			300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 2000, 3000,
+			4000, 5000, 6000, 7000, 8000, 9000, 10000 };
+	int numRotations[30] = { 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300,
+			350, 400, 450, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000,
+			5000, 6000, 7000, 8000, 9000, 10000 };
 	int maximalAngleStay[8] = { 45, 90, 120, 150, 180, 240, 300, 360 };
-	int maximalAngle[9] = {20, 30, 40, 45, 50, 60, 70, 80, 90};
+	int maximalAngle[9] = { 20, 30, 40, 45, 50, 60, 70, 80, 90 };
 
-	for (int t = 0; t <30; t++) {
+	for (int t = 0; t < 30; t++) {
 		for (int r = 0; r < 30; r++) {
 			for (int ms = 0; ms < 8; ms++) {
 				for (int ma = 0; ma < 9; ma++) {
 					double sum = 0;
 					for (int seed = 1; seed <= num_seeds; seed++) {
-						cout<<"starting "<<t * 30 *  8 * 9 * num_seeds + r * 8 * 9 * num_seeds + ms * 9 * num_seeds + ma * num_seeds + seed <<" out of "<< 30 * 30 * 8 * 9 * num_seeds << endl;
+						cout << "starting " << t * 30 * 8 * 9 * num_seeds + r
+								* 8 * 9 * num_seeds + ms * 9 * num_seeds + ma
+								* num_seeds + seed << " out of " << 30 * 30 * 8
+								* 9 * num_seeds << endl;
 						double* force = new double[3];
 						newMolecule.clearFM();
 						newMolecule.setr(0, 1);
@@ -299,24 +175,32 @@ void ParticleInsertionTest::testParametersFullStudy() {
 
 						double energy_old = energy;
 
+						double allowed_low[3] = { 0, 0, 0 };
+						double allowed_high[3] = {
+								linkedCells->getBoundingBoxMax(0),
+								linkedCells->getBoundingBoxMax(1),
+								linkedCells->getBoundingBoxMax(2) };
+
 						int performedTimesteps =
 								insertion.findParticlePosition(linkedCells,
-										cell, &newMolecule, u_avg, &energy,
-										&energy_old, true,
-										false, seed, numTranslations[t],
-										1000, numRotations[r], maximalAngle[ma] * PI / 180,
-										maximalAngleStay[ms] * PI / 180, 1 * PI / 180,
+										&newMolecule, u_avg, &energy,
+										&energy_old, true, false, seed,
+										numTranslations[t], 1000,
+										numRotations[r], maximalAngle[ma] * PI
+												/ 180, maximalAngleStay[ms]
+												* PI / 180, 1 * PI / 180,
 										0.001, &vec_energy, &vec_angle,
 										&vec_lj, &vec_center, name_energy,
-										name_angle, name_lj, name_center);
+										name_angle, name_lj, name_center,
+										allowed_low, allowed_high);
 						sum += performedTimesteps;
 						delete force;
 
 					}
 					sum /= num_seeds;
 					fprintf(fresult, "%g %d %d %d %d \n", sum,
-													numTranslations[t], numRotations[r],
-															maximalAngleStay[ms], maximalAngle[ma]);
+							numTranslations[t], numRotations[r],
+							maximalAngleStay[ms], maximalAngle[ma]);
 				}
 			}
 		}
@@ -324,11 +208,12 @@ void ParticleInsertionTest::testParametersFullStudy() {
 	fclose(fresult);
 }
 
+//FIXME: where should I be inserting?
 void ParticleInsertionTest::testParameterSetup() {
 
 	LinkedCells* linkedCells = (LinkedCells*) initializeFromFile(
-			ParticleContainerFactory::LinkedCell, "Ethan_20k_equilibrated.inp",
-			13.22808814);
+			ParticleContainerFactory::LinkedCell, "Ethan_10k_eps_rc1.5.inp",
+			9.92106);
 
 	int ownrank = 0;
 	PressureGradient* pressureGradient = new PressureGradient(ownrank);
@@ -343,7 +228,7 @@ void ParticleInsertionTest::testParameterSetup() {
 	components.push_back(dummyComponent);
 	_domain = new Domain(ownrank, pressureGradient);
 	_domain->addComponent(dummyComponent);
-	_domain->initParameterStreams( 13.22808814,  13.22808814);
+	_domain->initParameterStreams(9.92106, 9.92106);
 	moleculardynamics::coupling::ParticleInsertion<Molecule, LinkedCells, 3>
 			insertion;
 
@@ -371,18 +256,23 @@ void ParticleInsertionTest::testParameterSetup() {
 	}
 
 	u_avg /= count;
-	cout << "target energy: " << u_avg << " for "<<count<<" molecules"<<endl;
+	cout << "target energy: " << u_avg << " for " << count << " molecules"
+			<< endl;
 	string file_name_base = "../test_input/param_config";
 	string result_filename_base = "param_result_seed_";
 	int num_seeds = 100;
 
-	FILE* fresult = fopen("result_param_config_smt.txt", "w");
+	FILE
+			* fresult =
+					fopen(
+							"result_param_config_ethan_10k_rc1.5_insert_anywhere_restarts1000_new.txt",
+							"w");
 	for (int seed = 1; seed <= num_seeds; seed++) {
 
 		cout << "starting for seed " << seed << endl;
 		for (int i = 0; i < num_tests; i++) {
 			cout << "starting for setup " << i + 1 << endl;
-			newMolecule.setr(0,13);
+			newMolecule.setr(0, 8);
 			newMolecule.setr(1, 8);
 			newMolecule.setr(2, 8);
 			stringstream file_stream;
@@ -391,9 +281,11 @@ void ParticleInsertionTest::testParameterSetup() {
 			file_stream >> file_name;
 			// calculate starting energy & force
 			double* force = new double[3];
-			newMolecule.clearFM();
-			double energy = linkedCells->getForceAndEnergy(&newMolecule, force);
-
+			for (int i = 0; i < 3; i++)
+				force[i] = 1;
+			//			newMolecule.clearFM();
+			//			double energy = linkedCells->getForceAndEnergy(&newMolecule, force);
+			double energy = 1;
 			wrapper.setForce(force);
 
 			// allocate for logging
@@ -420,13 +312,18 @@ void ParticleInsertionTest::testParameterSetup() {
 					tolerance, maxAngle, maxAllowedAngle, minAngle,
 					largeStepsizeOnOverlap, restartIfIncreases);
 
+			double allowed_low[3] = { 0, 0, 0 };
+			double allowed_high[3] = { linkedCells->getBoundingBoxMax(0),
+					linkedCells->getBoundingBoxMax(1),
+					linkedCells->getBoundingBoxMax(2) };
+			//double allowed_high[3] = {5 * linkedCells->getCutoff(), 5 *linkedCells->getCutoff(), 5 * linkedCells->getCutoff()};
 			int performedTimesteps = insertion.findParticlePosition(
-					linkedCells, cell, &newMolecule, u_avg, &energy,
-					&energy_old, *largeStepsizeOnOverlap, *restartIfIncreases,
-					seed, *maxIter, *maxRestarts, *maxRotations, *maxAngle,
+					linkedCells, &newMolecule, u_avg, &energy, &energy_old,
+					*largeStepsizeOnOverlap, *restartIfIncreases, seed,
+					*maxIter, *maxRestarts * 10, *maxRotations, *maxAngle,
 					*maxAllowedAngle, *minAngle, *tolerance, &vec_energy,
 					&vec_angle, &vec_lj, &vec_center, name_energy, name_angle,
-					name_lj, name_center);
+					name_lj, name_center, allowed_low, allowed_high);
 
 			delete maxIter;
 			delete maxRestarts;
