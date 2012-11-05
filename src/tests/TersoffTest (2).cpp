918593ee1cf4c3169249d802da4008624060d794
Florian <florian>
2012-11-05 20:02:57 +0000

    Test
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@1584 a63bd714-7e14-4b5e-94e7-302c8c8ff188

---
+++ src/tests/TersoffTest (2).cpp
@@ -0,0 +1,211 @@
+/*
+ * TersoffTest.h
+ *
+ * @Date: 10.08.2012
+ * @Author: Florian Groetzner
+ */
+
+#include "TersoffTest.h"
+#include "molecules/Molecule.h"
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+TEST_SUITE_REGISTRATION(TersoffTest);
+
+TersoffTest::TersoffTest() {
+
+}
+
+TersoffTest::~TersoffTest() {
+}
+
+
+void TersoffTest::testTersoffbij() {
+
+
+	int numTests = 11;
+	// Test results
+	double input[11] = {-2.0,0.0,2.0,3.0,3.5,3.6,3.7,3.8,3.9,4.0,5.0};
+	double expected[11] = {0.56922,0.56922,0.56922,0.56922,0.58024,0.61463,0.67617,0.76978,0.89365,1.0,1.0};
+	double test = 0.0;
+
+	// Components for the molecules to test
+	std::vector<Component> components;
+	Component dummyComponent(0);
+
+	// Add tersoff
+	// dummyComponent.addTersoff(0,0,0,0,51.212,12.742,0,0,1.95,0.15,38049,4.3484,-0.57058,0.72751,1.5724e-7);
+	dummyComponent.addTersoff(0,0,0,1,5.1212e1,1.274e1,6.5912,4.1799,3.4,4.0,3.8049e4,4.3484,-0.57058,0.72751,1.5724e-7);
+	components.push_back(dummyComponent);
+
+	Molecule a(0, 0, 1.0, 1.0, 1.0,0,0,0,0,0,0,0,0,0,0, &components);
+
+	// Initialize parameter list
+	double params[15] = {0};
+	a.tersoffParameters(params);
+	
+	// Distance vector
+
+	double distance[3] = {0.0,0.0,0.0};
+
+	double dis;	
+	
+
+	for (int i = 0; i < numTests; i++){	
+		dis = input[i];
+
+		// Distance vector
+		distance[0] = input[i];
+
+
+		Molecule b(1, 0, 0.0, 0.0, input[i],0,0,0,0,0,0,0,0,0,0, &components);
+
+		// Add neighbours
+		a.addTersoffNeighbour(&b, false);
+		b.addTersoffNeighbour(&a, false);
+		
+		double params2[15] = {0};
+		a.tersoffParameters(params2);
+
+		test = Tersoffbij(&a, &b, params2, distance, dis);
+
+	
+		CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], test, 1e-2);
+
+	}
+
+
+}
+
+
+void TersoffTest::tersoffUIJplusUJI() {
+	/*
+	int numTests = 5;
+	// Test results
+	//double expected[numTests] = {1.0};
+	double test = 0.0;
+
+	// Components for the molecules to test
+	std::vector<Component> components;
+	Component dummyComponent(0);
+
+	// Add tersoff
+	dummyComponent.addTersoff(0,0,0,0,51.212,12.742,0,0,1.95,0.15,38049,4.3484,-0.57058,0.72751,1.5724e-7);
+	components.push_back(dummyComponent);
+
+	Molecule a(0, 0, 1.0, 1.0, 1.0,0,0,0,0,0,0,0,0,0,0, &components);
+
+	// Initialize parameter list
+	double params[15] = {0};
+	a.tersoffParameters(params);
+
+	double upotTersoff = 1.0;
+
+	double distance[3] = {0.0,0.0,0.0};
+
+	double dis;	
+	for (int i = 0; i < numTests; i++){
+		dis = (0.1*i) - 10.0;
+
+		Molecule b(1, 0, 0.0, 0.0, dis,0,0,0,0,0,0,0,0,0,0, &components);
+		
+		// Add neighbours
+		a.addTersoffNeighbour(&b, true);
+		b.addTersoffNeighbour(&a, true);
+
+		// Distance vector
+		distance[0] = dis;
+
+		test = TersoffUIJplusUJI(&a, &b, params, distance, dis, upotTersoff);
+	
+		//ASSERT_DOUBLES_EQUAL(*expected[i], test, 1e-6);
+	}
+*/
+}
+
+void TersoffTest::tersoffUIJattr() {
+	/*
+	int numTests = 5;
+	// Test results
+	//double expected[numTests] = {1.0};
+	double test = 0.0;
+
+	// Components for the molecules to test
+	std::vector<Component> components;
+	Component dummyComponent(0);
+
+	// Add tersoff
+	dummyComponent.addTersoff(0,0,0,0,51.212,12.742,0,0,1.95,0.15,38049,4.3484,-0.57058,0.72751,1.5724e-7);
+	components.push_back(dummyComponent);
+
+	Molecule a(0, 0, 1.0, 1.0, 1.0,0,0,0,0,0,0,0,0,0,0, &components);
+
+	// Initialize parameter list
+	double params[15] = {0};
+	a.tersoffParameters(params);
+
+	double distance[3] = {0.0,0.0,0.0};
+	
+	double dis;	
+	for (int i = 0; i < numTests; i++){
+		dis = (0.1*i) - 10.0;
+
+		Molecule b(1, 1, 0.0, 0.0, dis,0,0,0,0,0,0,0,0,0,0, &components);
+		
+		// Add neighbours
+		a.addTersoffNeighbour(&b, true);
+		b.addTersoffNeighbour(&a, true);
+
+		// Distance vector
+		distance[0] = dis;
+
+		test = TersoffUIJattr(&a, &b, params, distance, dis);
+	
+		//ASSERT_DOUBLES_EQUAL(*expected[i], test, 1e-6);
+	}
+*/
+}
+
+void TersoffTest::tersoffPotential() {
+	/*
+	int numTests = 5;
+	// Test results
+	double expected[5] = {1.7674e31, 6.9530e29, 2.7354e28, 1.0761e27, 4.2335e25};
+	double test = 0.0;
+
+	// Components for the molecules to test
+	std::vector<Component> components;
+	Component dummyComponent(0);
+
+	// Add tersoff
+	dummyComponent.addTersoff(0,0,0,0,51.212,12.742,0,0,1.95,0.15,38049,4.3484,-0.57058,0.72751,1.5724e-7);
+	components.push_back(dummyComponent);
+
+	Molecule a(0, 0, 0.0, 0.0, 0.0,0,0,0,0,0,0,0,0,0,0, &components);
+	
+	// Initialize parameter list
+	double params[15] = {0};
+	a.tersoffParameters(params);
+	
+	double upotTersoff = 1.0;
+	
+	for (int i = 0; i < numTests; i++){
+		Molecule b(1, 1, 0.0, 0.0, (0.1*i) - 10.0,0,0,0,0,0,0,0,0,0,0, &components);
+		
+		// Add neighbours
+		a.addTersoffNeighbour(&b, true);
+		b.addTersoffNeighbour(&a, true);
+
+		test = TersoffPotential(&a, params, upotTersoff);
+		
+		double result = expected[i];
+		//ASSERT_DOUBLES_EQUAL(result, test, 1e-1);
+	}
+*/
+}
+
+
+
