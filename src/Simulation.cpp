8b3a991c3184997eac6389ebd1b03d5c5599a8a3
Wolfgang Eckhardt <eckhardw@in.tum.de>
2013-06-10 12:09:45 +0000

    disable vectorizedCellHandler by default
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@1865 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -954,7 +954,7 @@ void Simulation::initConfigOldstyle(const string& inputfilename) {
 	global_log->info() << "xx quadrupole present: " << quadrupole_present << endl;
 	global_log->info() << "xx tersoff present: " << tersoff_present << endl;
 
-#if 1
+#if 0
 	if (charge_present || dipole_present || quadrupole_present || tersoff_present) {
 		_cellProcessor = new LegacyCellProcessor( _cutoffRadius, _LJCutoffRadius, _tersoffCutoffRadius, _particlePairsHandler);
 	} else {
