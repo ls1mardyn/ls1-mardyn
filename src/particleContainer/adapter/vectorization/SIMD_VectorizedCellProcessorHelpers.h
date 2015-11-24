6a191710f15fc5a7119e118c61ef7fb1f5f57947
Steffen Seckler <seckler@in.tum.de>
2015-11-24 12:06:25 +0000

    preparing for merge of novec + vec II: added unpackEps24Sig2, unpackShift6 to SIMD_VectorizedCellProcessorHelpers.h for novec
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/SecklerSteffen@3058 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/particleContainer/adapter/vectorization/SIMD_VectorizedCellProcessorHelpers.h
+++ src/particleContainer/adapter/vectorization/SIMD_VectorizedCellProcessorHelpers.h
@@ -19,12 +19,15 @@ static inline __attribute__((always_inline))
 void unpackEps24Sig2(vcp_double_vec& eps_24, vcp_double_vec& sig2, const DoubleArray& eps_sigI,
 		const size_t* const id_j){
 #if VCP_VEC_TYPE==VCP_NOVEC //novec comes first. For NOVEC no specific types are specified -- use build in ones.
+	eps_24 = eps_sigI[2 * id_j[0]];
+	sig2 = eps_sigI[2 * id_j[0] + 1];
 
 #elif VCP_VEC_TYPE==VCP_VEC_SSE3 //sse3
 	const vcp_double_vec e0s0 = vcp_simd_load(eps_sigI + 2 * id_j[0]);
 	const vcp_double_vec e1s1 = vcp_simd_load(eps_sigI + 2 * id_j[1]);
 	eps_24 = vcp_simd_unpacklo(e0s0, e1s1);
 	sig2 = vcp_simd_unpackhi(e0s0, e1s1);
+
 #elif VCP_VEC_TYPE==VCP_VEC_AVX //avx
 	static const __m256i memoryMask_first_second = _mm256_set_epi32(0, 0, 0, 0, 1<<31, 0, 1<<31, 0);
 	const vcp_double_vec e0s0 = vcp_simd_maskload(eps_sigI + 2 * id_j[0], memoryMask_first_second);
@@ -46,11 +49,13 @@ static inline __attribute__((always_inline))
 void unpackShift6(vcp_double_vec& shift6, const DoubleArray& shift6I,
 		const size_t* const id_j){
 #if VCP_VEC_TYPE==VCP_NOVEC //novec comes first. For NOVEC no specific types are specified -- use build in ones.
+	shift6 = shift6I[id_j[0]];
 
 #elif VCP_VEC_TYPE==VCP_VEC_SSE3 //sse3
 	const vcp_double_vec sh1 = _mm_load_sd(shift6I + id_j[0]);
 	const vcp_double_vec sh2 = _mm_load_sd(shift6I + id_j[1]);
 	shift6 = vcp_simd_unpacklo(sh1, sh2);
+
 #elif VCP_VEC_TYPE==VCP_VEC_AVX //avx
 	static const __m256i memoryMask_first = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 1<<31, 0);
 	const vcp_double_vec sh0 = vcp_simd_maskload(shift6I + id_j[0], memoryMask_first);
