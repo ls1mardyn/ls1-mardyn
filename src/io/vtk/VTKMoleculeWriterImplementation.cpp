5dd558dfcbf6f45bd39c9ddc81d5d80da173040e
Nikola Plamenov Tchipev <tchipev@in.tum.de>
2016-05-05 18:54:04 +0000

    Fix in VTKMoleculeWriterImplementation.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@3420 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/io/vtk/VTKMoleculeWriterImplementation.cpp
+++ src/io/vtk/VTKMoleculeWriterImplementation.cpp
@@ -140,7 +140,7 @@ void VTKMoleculeWriterImplementation::plotCenter(Molecule& molecule, int centerI
 	data_iterator->push_back(_rank);
 	data_iterator++;
 
-	const double* center_force = molecule.site_F(centerID);
+	const std::array<double, 3> center_force = molecule.site_F(centerID);
 	data_iterator->push_back(center_force[0]);
 	data_iterator->push_back(center_force[1]);
 	data_iterator->push_back(center_force[2]);
@@ -153,10 +153,10 @@ void VTKMoleculeWriterImplementation::plotCenter(Molecule& molecule, int centerI
 	Points::DataArray_sequence& pointsArraySequence = (*_vtkFile).UnstructuredGrid()->Piece().Points().DataArray();
 	Points::DataArray_iterator coordinates_iterator = pointsArraySequence.begin();
 
-	const double* curr_center = molecule.ljcenter_d(centerID);
-	coordinates_iterator->push_back(molecule.r(0) + curr_center[0]);
-	coordinates_iterator->push_back(molecule.r(1) + curr_center[1]);
-	coordinates_iterator->push_back(molecule.r(2) + curr_center[2]);
+	const std::array<double, 3> curr_center = molecule.ljcenter_d_abs(centerID);
+	coordinates_iterator->push_back(curr_center[0]);
+	coordinates_iterator->push_back(curr_center[1]);
+	coordinates_iterator->push_back(curr_center[2]);
 	_numMoleculesPlotted++;
 }
 
