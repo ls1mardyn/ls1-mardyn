e433b34df3da52f0727cd21ced804a17860ca729
Nikola Plamenov Tchipev <tchipev@in.tum.de>
2017-01-20 17:26:55 +0000

    move _cellIndex, _boxMin and _boxMax from derived classes ParticleCell and ParticleCellPointers upwards in base class Cell.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@4129 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/bhfmm/containers/ParticleCellPointers.h
+++ src/bhfmm/containers/ParticleCellPointers.h
@@ -81,52 +81,7 @@ public:
 		return _cellDataSoA;
 	}
 
-	/**
-	 * Returns the cell index
-	 */
-	unsigned long getCellIndex() {
-		return _cellIndex;
-	}
-
-	/**
-	 * Sets the cell index. On one process, this index must be unique.
-	 * @param cellIndex
-	 */
-	void setCellIndex(unsigned long cellIndex){
-		_cellIndex = cellIndex;
-	}
-
-	double getBoxMin(int d) const {
-		return _boxMin[d];
-	}
-
-	void setBoxMin(const double b[3]) {
-		for(int d=0; d< 3; ++d) {
-			_boxMin[d] = b[d];
-		}
-	}
-
-	double getBoxMax(int d) const {
-		return _boxMax[d];
-	}
-
-	void setBoxMax(const double b[3]) {
-		for (int d = 0; d < 3; ++d) {
-			_boxMax[d] = b[d];
-		}
-	}
-
 private:
-	/**
-	 * \brief lower left front corner
-	 */
-	double _boxMin[3];
-
-	/**
-	 * \brief upper right back corner
-	 */
-	double _boxMax[3];
-
 	/**
 	 * \brief A vector of pointers to the Molecules in this cell.
 	 */
@@ -137,12 +92,6 @@ private:
 	 * \author Johannes Heckl
 	 */
 	CellDataSoA _cellDataSoA;
-
-	/**
-	 * \brief The index of the cell.
-	 * On one process every index must be unique.
-	 */
-	unsigned long _cellIndex;
 };
 
 } /* namespace bhfmm */
