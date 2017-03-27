3d891a399a1363c06f6d50d65727b4aa70ca40cd
Matthias Heinen <mheinen@mail.upb.de>
2017-03-27 08:03:15 +0000

    Merge from trunk: svn merge -c 4152 https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk /home/mheinen/workspace/mainbranch20170119
    since this revision showed up to be eligible for merge:
    svn mergeinfo --show-revs eligible ^/MarDyn/trunk ^/MarDyn/branches/mheinenNEMD/mainbranch20170119
    
    
    git-svn-id: https://scm.projects.hlrs.de/authscm/jenkinsls1/svn/ls1/MarDyn/branches/mheinenNEMD/mainbranch20170119@4326 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/molecules/Molecule.h
+++ src/molecules/Molecule.h
@@ -8,6 +8,12 @@
 #ifndef SRC_MOLECULES_MOLECULE_H_
 #define SRC_MOLECULES_MOLECULE_H_
 
+/**
+ * the old class Molecule is now called FullMolecule and it implements MoleculeInterface.
+ * Please bear with us and introduce the necessary changes in MoleculeInterface
+ * and provide a stub at least for compiling Molecule_WR
+ */
+
 #ifndef MARDYN_WR
 	#include "FullMolecule.h"
 	typedef FullMolecule Molecule;
