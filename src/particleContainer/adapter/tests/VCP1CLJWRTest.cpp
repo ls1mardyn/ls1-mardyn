2e7e93d9de9604b8ad496d6a91e5143a41d602f6
Nikola Plamenov Tchipev <tchipev@in.tum.de>
2017-04-13 12:03:27 +0000

    second force calculation test works
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/tchipev@4401 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/particleContainer/adapter/tests/VCP1CLJWRTest.cpp
+++ src/particleContainer/adapter/tests/VCP1CLJWRTest.cpp
@@ -79,6 +79,42 @@ void VCP1CLJWRTest::testForcePotentialCalculationU0() {
 }
 
 void VCP1CLJWRTest::testForcePotentialCalculationF0() {
+	if (_domainDecomposition->getNumProcs() != 1) {
+		test_log->info() << "DomainDecompositionTest::testExchangeMolecules1Proc()"
+				<< " not executed (rerun with only 1 Process!)" << std::endl;
+		std::cout << "numProcs:" << _domainDecomposition->getNumProcs() << std::endl;
+		return;
+	}
+
+
+	// U (r_ij) = 4 epsilon * ( (sigma / r_ij)^12 - (sigma / r_ij)^6 )
+	// F (r_ij) = 24 epsilon * 1/r ( (sigma / r_ij)^6 - 2 * (sigma / r_ij)^12 )
+	// -> potential U = -1 per particle pair.
+	ParticleContainer* container = initializeFromFile(ParticleContainerFactory::LinkedCell, "ForceCalculationTestF0.inp", 1.3);
+
+	ASSERT_DOUBLES_EQUAL(0.0, _domain->getLocalUpot(), 1e-8);
+	ASSERT_DOUBLES_EQUAL(0.0, _domain->getLocalVirial(), 1e-8);
+
+	VCP1CLJ_WR cellProcessor( *_domain,  1.3, 1.3);
+	cellProcessor.setDtInv2m(1.0);
+	container->traverseCells(cellProcessor);
+
+	for (ParticleIterator m = container->iteratorBegin(); m != container->iteratorEnd(); ++m) {
+		m->calcFM();
+	}
+
+	for (ParticleIterator m = container->iteratorBegin(); m != container->iteratorEnd(); ++m) {
+		for (int i = 0; i < 3; i++) {
+			std::stringstream str;
+			str << "Molecule id=" << m->id() << " index i="<< i << " F[i]=" << m->F(i) << std::endl;
+			ASSERT_DOUBLES_EQUAL_MSG(str.str(), 0.0, m->F(i), 1e-7);
+		}
+	}
+
+	ASSERT_DOUBLES_EQUAL(-4, _domain->getLocalUpot(), 1e-8);
+	ASSERT_DOUBLES_EQUAL(0.0, _domain->getLocalVirial(), 1e-6);
+
+	delete container;
 }
 
 void VCP1CLJWRTest::testLennardJonesVectorization() {
