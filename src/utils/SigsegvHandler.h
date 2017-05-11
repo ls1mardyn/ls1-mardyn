9a5860cbc77dbcd200d6335fcd2be414d454d6d6
Steffen Seckler <seckler@in.tum.de>
2017-05-11 08:57:18 +0000

    added sigsegvhandler for better debugging purposes. you can invoke it using --sigsegvhandler
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@4578 a63bd714-7e14-4b5e-94e7-302c8c8ff188

---
+++ src/utils/SigsegvHandler.h
@@ -0,0 +1,34 @@
+/*
+ * SigsegvHandler.h
+ *
+ *  Created on: May 11, 2017
+ *      Author: seckler
+ */
+#pragma once
+
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "mardyn_assert.h"
+
+
+void handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  mardyn_exit(1);
+}
+
+void registerSigsegvHandler() {
+  signal(SIGSEGV, handler);   // install our handler
+}
+
