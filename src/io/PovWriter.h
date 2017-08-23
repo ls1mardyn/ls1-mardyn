9a2d094b7abf0a406739dc54e95e86adabd35168
Christoph Niethammer <niethammer@hlrs.de>
2017-08-23 05:30:37 +0000

    Cleanup of PovWriter removing unused constructor.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@5205 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/io/PovWriter.h
+++ src/io/PovWriter.h
@@ -1,29 +1,14 @@
-#ifndef POVWRITER_H_
-#define POVWRITER_H_
+#ifndef SRC_IO_POVWRITER_H_
+#define SRC_IO_POVWRITER_H_
 
-#include <string>
 
 #include "io/OutputBase.h"
 
 
 class PovWriter : public OutputBase {
 public:
-    PovWriter(){}
-	/** @brief Writes a POVray file of the current state of the simluation.
-	 *
-	 * The file can be used to visualize with POVray software (www.povray.org).
-	 *
-	 * @param filename Name of the POV file (including path)
-	 * @param particleContainer The molecules that have to be written to the
-	 *                 file are stored here
-	 * @param domainDecomp In the parallel version, the file has to be written
-	 *                 by more than one process. Methods to achieve this are
-	 *                 available in domainDecomp
-	 * @param writeFrequency Controls the frequency of writing out the data.
-	 */
-	PovWriter(unsigned long writeFrequency, std::string filename, bool incremental);
-	~PovWriter();
-
+	PovWriter() {}
+	~PovWriter() {}
 
 	/** @brief Read in XML configuration for PovWriter and all its included objects.
 	 *
@@ -47,11 +32,12 @@ public:
 	);
 	void finishOutput(ParticleContainer* particleContainer,
 			DomainDecompBase* domainDecomp, Domain* domain);
-	
+
 	std::string getPluginName() {
 		return std::string("PovWriter");
 	}
 	static OutputBase* createInstance() { return new PovWriter(); }
+
 private:
 	std::string _outputPrefix;
 	unsigned long _writeFrequency;
@@ -59,4 +45,4 @@ private:
 	bool  _appendTimestamp;
 };
 
-#endif /* POVWRITER_H_ */
+#endif  // SRC_IO_POVWRITER_H_
