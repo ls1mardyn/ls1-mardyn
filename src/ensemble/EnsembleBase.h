7bba0eee7343ae3d6f4fcf273f357101d7988d64
Nikola Plamenov Tchipev <tchipev@in.tum.de>
2017-10-18 17:12:32 +0000

    make access to Ensemble::getComponent safer.
    .at() has an out of bounds check, which spares us a lot of seg-faults. 
    
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@5649 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/ensemble/EnsembleBase.h
+++ src/ensemble/EnsembleBase.h
@@ -57,8 +57,8 @@ public:
 	virtual void updateGlobalVariable(ParticleContainer *particleContainer, GlobalVariable variable) = 0;
 
 	DomainBase* &domain() { return _domain; }
-	Component* getComponent(int cid) { return &_components[cid]; }
-	Component* getComponent(std::string name) { return &_components[_componentnamesToIds[name]]; }
+	Component* getComponent(int cid) { return &_components.at(cid); }
+	Component* getComponent(std::string name) { return getComponent(_componentnamesToIds[name]); }
 	std::vector<Component>* getComponents() { return &_components; }
 	void addComponent(Component& component) { _components.push_back(component); }
 
