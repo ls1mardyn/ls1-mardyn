ff69492d72be1ef2a29eb6bd9a91af1a126ec0d4
Christoph Niethammer <niethammer@hlrs.de>
2017-11-15 16:59:26 +0000

    Let readPhaseSpace return number of read molecules. Update doxygen documentation.
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@5732 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/io/InputBase.h
+++ src/io/InputBase.h
@@ -19,17 +19,16 @@ public:
 
 	virtual ~InputBase(){}
 
-	//! @brief read the phase space components and header information
-	//! @param timestep timestep length
+	/** @brief read the phase space components and header information
+	 * @param domain  pointer to domain object
+	 * @param timestep timestep length
+	 */
 	virtual void readPhaseSpaceHeader(Domain* domain, double timestep) = 0;
 
 	virtual void readXML(XMLfileUnits& /*xmlconfig*/) {}
 
-	/**
-	 *  @brief read the actual phase space information
-	 *  Returns "the highest molecule ID found in the phase space file";
-	 *  // todo why? should it be some kind of upper bound for the number of molecules???
-	 *  // WE: good question. For the scenario generator however, useful for the visualization...
+	/** @brief read the actual phase space information
+	 * @return number of read in molecules added to the particle containers of all processes
 	 */
 	virtual unsigned long readPhaseSpace(ParticleContainer* particleContainer, std::list<ChemicalPotential>* lmu, Domain* domain, DomainDecompBase* domainDecomp) = 0;
 
