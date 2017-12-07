12f9c5bf1f743e9f658809b7f29d68f872a19b1d
Tim Bierth <bierth@gmx.net>
2017-12-07 11:51:50 +0000

    new
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/sauermann@5826 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/parallel/ParticleDataRMM.h
+++ src/parallel/ParticleDataRMM.h
@@ -1,9 +1,10 @@
-#ifndef ParticleDataFull_H_
-#define ParticleDataFull_H_
+#ifndef ParticleDataRMM_H_
+#define ParticleDataRMM_H_
 
 #include <mpi.h>
 
 #include "molecules/MoleculeForwardDeclaration.h"
+#include "particleContainer/adapter/vectorization/SIMD_TYPES.h"
 
 //! @brief class to represent that particle data that is necessary for the exchange between processes
 //! @author Martin Buchholz
@@ -16,23 +17,20 @@
 //! This class achieves both. It is a class without constructor and destructor and only static
 //! methods, which means that only the member variables use memory, so it can be used like a
 //! C stuct. The static method setMPIType sets the given type to represent all the member variables.
-class ParticleDataFull {
+class ParticleDataRMM {
 public:
 	//! @brief defines a MPI datatype which can be used to transfer a MacroscopicData object
 	static void getMPIType(MPI_Datatype &sendPartType);
 
-	//! @brief copy data from object of class Molecule to object of class ParticleDataFull
-	static void MoleculeToParticleData(ParticleDataFull &particleStruct, Molecule &molecule);
+	//! @brief copy data from object of class Molecule to object of class ParticleDataRMM
+	static void MoleculeToParticleData(ParticleDataRMM &particleStruct, Molecule &molecule);
 
-	//! @brief copy data from object of class ParticleDataFull to object of class Molecule
-	static void ParticleDataToMolecule(const ParticleDataFull &particleStruct, Molecule &molecule);
+	//! @brief copy data from object of class class ParticleDataRMM to object of class Molecule
+	static void ParticleDataToMolecule(const ParticleDataRMM &particleStruct, Molecule &molecule);
 
-	double r[3];  //! position
-	double v[3];  //! velocity
-	double q[4];  //! rotational quaternion
-	double D[3];  //! rotational velocity (angular momentum, L)
-	unsigned long id; //! ID of the particle
-	int cid;  //! ID of the component of the particle
+	unsigned long id;
+	vcp_real_calc r[3];
+	vcp_real_calc v[3];
 };
 
-#endif /* ParticleDataFull_H_ */
+#endif /* ParticleDataRMM_H_ */
