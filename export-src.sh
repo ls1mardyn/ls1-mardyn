f39fdea1ea5f8d2344a11661a293dcea04e59e79
Nikola Plamenov Tchipev <tchipev@in.tum.de>
2018-03-08 15:37:19 +0000

    add the symbolic link to the makefile to export-src for convenience
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@6155 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- export-src.sh
+++ export-src.sh
@@ -6,5 +6,5 @@ FILES+=" "
 FILES+=$(find ./src/ -name "*.hpp")
 echo "FILES=" $FILES
 
-echo "tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mkTcTS/ tools/mkesfera/  makefile/ \$FILES"
-tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mktcts/ tools/mkesfera/  makefile/ $FILES
+echo "tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mkTcTS/ tools/mkesfera/  makefile/ src/Makefile \$FILES"
+tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mktcts/ tools/mkesfera/  makefile/ src/Makefile $FILES 
