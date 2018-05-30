7391f29ec766a6b8b75174a3181e58293b482a17
Steffen Seckler <seckler@in.tum.de>
2018-05-30 04:52:18 +0000

    bugfix for MaxCheck.cpp (invalid exit from openmp block)
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@6277 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/plugins/MaxCheck.cpp
+++ src/plugins/MaxCheck.cpp
@@ -15,15 +15,14 @@
 using namespace std;
 using Log::global_log;
 
-MaxCheck::MaxCheck()
-{
+MaxCheck::MaxCheck() {
 }
 
-MaxCheck::~MaxCheck()
-{
+MaxCheck::~MaxCheck() {
 }
 
-void MaxCheck::init(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, Domain* domain) {
+void MaxCheck::init(ParticleContainer* particleContainer,
+		DomainDecompBase* domainDecomp, Domain* domain) {
 	global_log->debug() << "MaxCheck enabled" << std::endl;
 }
 
@@ -31,25 +30,30 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 
 	// Timestep control
 	_control.start = 0;
-	_control.freq  = 1;
-	_control.stop  = 10000;
-	xmlconfig.getNodeValue("control/start",     _control.start);
+	_control.freq = 1;
+	_control.stop = 10000;
+	xmlconfig.getNodeValue("control/start", _control.start);
 	xmlconfig.getNodeValue("control/frequency", _control.freq);
-	xmlconfig.getNodeValue("control/stop",      _control.stop);
-	global_log->info() << "MaxCheck is acting start:freq:stop = " << _control.start<<":"<<_control.freq<<":"<<_control.stop << endl;
+	xmlconfig.getNodeValue("control/stop", _control.stop);
+	global_log->info() << "MaxCheck is acting start:freq:stop = "
+			<< _control.start << ":" << _control.freq << ":" << _control.stop
+			<< endl;
 
 	// targets
 	uint32_t numTargets = 0;
 	XMLfile::Query query = xmlconfig.query("targets/target");
 	numTargets = query.card();
-	global_log->info() << "MaxCheck: Number of component targets: " << numTargets << endl;
-	if(numTargets < 1) {
-		global_log->warning() << "MaxCheck: No target parameters specified. Program exit ..." << endl;
+	global_log->info() << "MaxCheck: Number of component targets: "
+			<< numTargets << endl;
+	if (numTargets < 1) {
+		global_log->warning()
+				<< "MaxCheck: No target parameters specified. Program exit ..."
+				<< endl;
 		Simulation::exit(-1);
 	}
 	string oldpath = xmlconfig.getcurrentnodepath();
 	XMLfile::Query::const_iterator nodeIter;
-	for( nodeIter = query.begin(); nodeIter; nodeIter++ ) {
+	for (nodeIter = query.begin(); nodeIter; nodeIter++) {
 		xmlconfig.changecurrentnode(nodeIter);
 
 		uint32_t cid_ub;
@@ -61,14 +65,17 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 		mv.method = MCM_UNKNOWN;
 
 		xmlconfig.getNodeValue("@method", mv.method);
-		global_log->info() << "MaxCheck: Method(cid="<<cid_ub<<"): " << mv.method << endl;
+		global_log->info() << "MaxCheck: Method(cid=" << cid_ub << "): "
+				<< mv.method << endl;
 
 		xmlconfig.getNodeValue("Fmax", mv.F);
-		global_log->info() << "MaxCheck: Fmax(cid="<<cid_ub<<"): " << mv.F << endl;
+		global_log->info() << "MaxCheck: Fmax(cid=" << cid_ub << "): " << mv.F
+				<< endl;
 		mv.F2 = mv.F * mv.F;
 
 		xmlconfig.getNodeValue("vmax", mv.v);
-		global_log->info() << "MaxCheck: vmax(cid="<<cid_ub<<"): " << mv.v << endl;
+		global_log->info() << "MaxCheck: vmax(cid=" << cid_ub << "): " << mv.v
+				<< endl;
 		mv.v2 = mv.v * mv.v;
 
 		_maxVals[cid_ub] = mv;
@@ -76,34 +83,31 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 
 }
 
-void MaxCheck::afterForces(
-		ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
-		unsigned long simstep)
-{
-	global_log->info() << "MaxCheck::afterForces() CHECKING for maxvals" << endl;
+void MaxCheck::afterForces(ParticleContainer* particleContainer,
+		DomainDecompBase* domainDecomp, unsigned long simstep) {
+	global_log->info() << "MaxCheck::afterForces() CHECKING for maxvals"
+			<< endl;
 	this->checkMaxVals(particleContainer, domainDecomp, simstep);
 }
 
-void MaxCheck::endStep(
-		ParticleContainer *particleContainer,
-		DomainDecompBase *domainDecomp, Domain *domain,
-		unsigned long simstep, std::list<ChemicalPotential> *lmu,
-		std::map<unsigned, CavityEnsemble> *mcav)
-{
+void MaxCheck::endStep(ParticleContainer *particleContainer,
+		DomainDecompBase *domainDecomp, Domain *domain, unsigned long simstep,
+		std::list<ChemicalPotential> *lmu,
+		std::map<unsigned, CavityEnsemble> *mcav) {
 	global_log->info() << "MaxCheck::endStep() CHECKING for maxvals" << endl;
 	this->checkMaxVals(particleContainer, domainDecomp, simstep);
 }
 
-void MaxCheck::checkMaxVals(
-		ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
-		unsigned long simstep)
-{
-	#if defined(_OPENMP)
-	#pragma omp parallel
-	#endif
+void MaxCheck::checkMaxVals(ParticleContainer* particleContainer,
+		DomainDecompBase* domainDecomp, unsigned long simstep) {
+
+	if (simstep < _control.start || simstep > _control.stop
+			|| simstep % _control.freq != 0)
+		return;
+#if defined(_OPENMP)
+#pragma omp parallel
+#endif
 	{
-		if(simstep < _control.start || simstep > _control.stop || simstep % _control.freq != 0)
-			return;
 
 		const ParticleIterator begin = particleContainer->iterator();
 
@@ -114,14 +118,13 @@ void MaxCheck::checkMaxVals(
 		double v[3];
 		MaxVals absVals;
 
-		for (ParticleIterator it = begin; it.hasNext(); it.next())
-		{
-			id=it->id();
-			cid_ub=it->componentid()+1;
-			for(uint8_t d=0; d<3; ++d) {
-				r[d]=it->r(d);
-				F[d]=it->F(d);
-				v[d]=it->v(d);
+		for (ParticleIterator it = begin; it.hasNext(); it.next()) {
+			id = it->id();
+			cid_ub = it->componentid() + 1;
+			for (uint8_t d = 0; d < 3; ++d) {
+				r[d] = it->r(d);
+				F[d] = it->F(d);
+				v[d] = it->v(d);
 			}
 
 			// calc abs vals
@@ -130,37 +133,34 @@ void MaxCheck::checkMaxVals(
 
 			MaxVals &mv = _maxVals[cid_ub];
 
-			if(MCM_LIMIT_TO_MAX_VALUE == mv.method)
-			{
-				if(mv.F > 0. && absVals.F2 > mv.F2) {
+			if (MCM_LIMIT_TO_MAX_VALUE == mv.method) {
+				if (mv.F > 0. && absVals.F2 > mv.F2) {
 					double Fabs = sqrt(absVals.F2);
 					double scale = mv.F / Fabs;
 					it->scale_F(scale);
 				}
 
-				if(mv.v > 0. && absVals.v2 > mv.v2) {
+				if (mv.v > 0. && absVals.v2 > mv.v2) {
 					double vabs = sqrt(absVals.v2);
 					double scale = mv.v / vabs;
 					it->scale_v(scale);
 				}
-			}
-			else if(MCM_DELETE_PARTICLES == mv.method)
-			{
-				if(mv.F > 0. && absVals.F2 > mv.F2)
+			} else if (MCM_DELETE_PARTICLES == mv.method) {
+				if (mv.F > 0. && absVals.F2 > mv.F2)
 					it.deleteCurrentParticle();
-	//				_deletions.push_back(&(*it));
+				//				_deletions.push_back(&(*it));
 
-				if(mv.v > 0. && absVals.v2 > mv.v2)
+				if (mv.v > 0. && absVals.v2 > mv.v2)
 					it.deleteCurrentParticle();
-	//				_deletions.push_back(&(*it));
+				//				_deletions.push_back(&(*it));
 			}
 		}
 
-	//		// perform deletions
-	//		for(auto it:_deletions) {
-	//			particleContainer->deleteMolecule(*it, true);
-	//			_deletions.pop_back();
-	//		}
+		//		// perform deletions
+		//		for(auto it:_deletions) {
+		//			particleContainer->deleteMolecule(*it, true);
+		//			_deletions.pop_back();
+		//		}
 
 	} // end pragma omp parallel
 }
