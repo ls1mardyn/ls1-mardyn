0823cfe990b716af454a45e2cd69ddd88b77c651
Steffen Seckler <seckler@in.tum.de>
2018-06-14 04:19:58 +0000

    fix parsing for some ide's
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@6326 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/Simulation.h
+++ src/Simulation.h
@@ -17,7 +17,7 @@ class Wall;
 class Mirror;
 using optparse::Values;
 
-#ifndef SIMULATION_SRC or IN_IDE_PARSER
+#if !defined (SIMULATION_SRC) or defined (IN_IDE_PARSER)
 class Simulation;
 /** Global pointer to the simulation object. Do not use directly. Instead use the reference. */
 extern Simulation* global_simulation;
