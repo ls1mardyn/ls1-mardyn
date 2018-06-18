1ec8f73e343efaa4c2f957107c9a128a35b924b2
Steffen Seckler <seckler@in.tum.de>
2018-06-18 07:13:09 +0000

    fixes incorrect enlarging for kdd
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/trunk@6345 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/parallel/KDDecomposition.cpp
+++ src/parallel/KDDecomposition.cpp
@@ -1437,12 +1437,15 @@ std::vector<CommunicationPartner> KDDecomposition::getNeighboursFromHaloRegion(D
 			} else if (haloRegion.offset[d] > 0 && ownHi[d] == _globalCellsPerDim[d] - 1) {
 				low[d] += _globalCellsPerDim[d];
 				high[d] += _globalCellsPerDim[d];
-			} else if (haloRegion.offset[d] == 0 && ownLo[d] < low[d]){
-				low[d]--;
-				enlarged[d][0] = true;
-			} else if (haloRegion.offset[d] == 0 && ownHi[d] > high[d]){
-				high[d]++;
-				enlarged[d][1] = true;
+			} else if (haloRegion.offset[d] == 0) {
+				if (ownLo[d] < low[d]) {
+					low[d]--;
+					enlarged[d][0] = true;
+				}
+				if (ownHi[d] > high[d]) {
+					high[d]++;
+					enlarged[d][1] = true;
+				}
 			}
 		}
 
