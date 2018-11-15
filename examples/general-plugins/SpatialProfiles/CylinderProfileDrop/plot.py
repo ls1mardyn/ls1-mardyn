22de556888a1968a7747abf5c5de65fa70904b79
kruegener <moritz.kruegener@gmail.com>
2018-11-15 20:31:12 +0100

    Added some documentation to plotting script for cylinder sampling

--- examples/general-plugins/SpatialProfiles/CylinderProfileDrop/plot.py
+++ examples/general-plugins/SpatialProfiles/CylinderProfileDrop/plot.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+''' Plot.py
+
+Short script for Plotting the output from SpatialProfile if used in cylindrical mode.
+	
+Can be used to plot all SpatialProfile output files that are written in standard format.
+Numer of skip_header lines might need to be adjusted if other / changed headers are written.
+To use this plotting script:
+	- Change the fileName to the proper name
+	- set skip_header number for your specific header lines
+	- set H to the size of the domain in Y-axis
+	- set R to the smaller size of (X-axis,Z-axis)
+	- set max_res to the number of bins you used in R/H direction (currently only same number supported, could be easily implemented)
+	- set the view parameter y_min, y_max, R2_max to bin the bin numbers you want to plot (0 to max_res)
+	- set levels to the number of contours desired
+	- set datasets to the number of accumulated datasets, should be found in header or config.xml
+'''
+
+
+
 # FileName
-fileName = 'drop.cyl._210000_kartesian.NDpr'
+fileName = 'profileName.NDpr'
 
 # Read File
 my_data = np.genfromtxt(fileName, skip_header=7, usecols=range(1, 800))
