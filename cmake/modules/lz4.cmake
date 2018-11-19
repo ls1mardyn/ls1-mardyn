5c72c6e7bd187845a34403b27309be0861355e86
fernanor <oliver.fernandes@visus.uni-stuttgart.de>
2018-11-19 14:56:24 +0100

    Added fix for parallel compile.

--- cmake/modules/lz4.cmake
+++ cmake/modules/lz4.cmake
@@ -15,17 +15,17 @@ if(ENABLE_LZ4)
             GIT_TAG dev
             SOURCE_DIR ${LZ4_SOURCE_DIR}
             BINARY_DIR ${LZ4_BINARY_DIR}
-            CONFIGURE_COMMAND cmake ${LZ4_SOURCE_DIR}/contrib/cmake_unofficial -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            CONFIGURE_COMMAND cmake ${LZ4_SOURCE_DIR}/contrib/cmake_unofficial -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DBUILD_STATIC_LIBS=ON
             BUILD_COMMAND make
             INSTALL_COMMAND ""    
     )
     # Create a liblz4 target to be used as a dependency by the program
-    add_library(liblz4 IMPORTED SHARED)
+    add_library(liblz4 IMPORTED STATIC)
     add_dependencies(liblz4 lz4)
 
     # set include directory associated with this target
     include_directories(${LZ4_SOURCE_DIR}/lib)
-    set(LZ4_LIB ${LZ4_BINARY_DIR}/liblz4.so)
+    set(LZ4_LIB ${LZ4_BINARY_DIR}/liblz4.a)
 else()
     message(STATUS "Not using LZ4.")
     set(LZ4_LIB "")
