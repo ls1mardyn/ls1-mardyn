b4ec9ee704f983aedeffc4d040a9d38fd263b62c
tchipevn <tchipevn@atsccs72.informatik.tu-muenchen.de>
2019-03-18 15:38:37 +0100

    back to powers of ten, as per ISO standards.

--- src/io/FlopRateWriter.cpp
+++ src/io/FlopRateWriter.cpp
@@ -138,21 +138,15 @@ void FlopRateWriter::measureFLOPS(ParticleContainer* particleContainer, unsigned
 }
 
 void FlopRateWriter::setPrefix(double f_in, double& f_out, char& prefix) const {
-	// powers of two or powers of ten?
-	double kilo = 1024.0;
-	double mega = 1024.0 * 1024.0;
-	double giga = 1024.0 * 1024.0 * 1024.0;
-	double tera = 1024.0 * 1024.0 * 1024.0 * 1024.0;
-	double peta = 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0;
-	double exa  = 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0;
+//	// powers of two or powers of ten? Update: resolved.
 
-//	// use powers of ten for correct comparison to TrillionAtoms run.
-//	double kilo = 1e+3;
-//	double mega = 1e+6;
-//	double giga = 1e+9;
-//	double tera = 1e+12;
-//	double peta = 1e+15;
-//	double exa  = 1e+18;
+	// Powers of ten, as stated by IEC, NIST and ISO, cf. Wikipedia on Binary_prefix
+	double kilo = 1e+3;
+	double mega = 1e+6;
+	double giga = 1e+9;
+	double tera = 1e+12;
+	double peta = 1e+15;
+	double exa  = 1e+18;
 
 	if (f_in >= exa) {
 		f_out = f_in / exa;
