0a39b5dfec48a28d974d6ead9e11fa6cae612708
SteffenSeckler <steffen.seckler@tum.de>
2019-03-26 09:31:50 +0100

    fix export-src.sh script

--- export-src.sh
+++ export-src.sh
@@ -6,5 +6,5 @@ FILES+=" "
 FILES+=$(find ./src/ -name "*.hpp")
 echo "FILES=" $FILES
 
-echo "tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mktcts/ tools/mkesfera/ libs/armadillo/ libs/rapidxml/ makefile/ src/Makefile \$FILES"
-tar cfz Mardyn-src.tar.gz tools/animake/ tools/mkCP/ tools/mktcts/ tools/mkesfera/ libs/armadillo/ libs/rapidxml/ makefile/ src/Makefile $FILES
+set -x
+tar cfz Mardyn-src.tar.gz tools/standalone-generators/ libs/armadillo/ libs/rapidxml/ makefile/ src/Makefile $FILES
