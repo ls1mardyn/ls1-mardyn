0d792aa47340789249868cd220240bf65e86cb43
SteffenSeckler <steffen.seckler@tum.de>
2019-10-04 09:16:05 +0200

    some comments

--- src/parallel/LoadBalancer.h
+++ src/parallel/LoadBalancer.h
@@ -8,8 +8,20 @@
 #include <array>
 #include <tuple>
 
+/**
+ * LoadBalancer class for the usage of arbitrary load balancing classes that are handled by GeneralDomainDecomposition.
+ */
 class LoadBalancer {
 public:
+	/**
+	 * Virtual destructor.
+	 */
 	virtual ~LoadBalancer() = default;
+
+	/**
+	 * The rebalancing call.
+	 * @param work Arbitrary unit of work, e.g., time for the current process
+	 * @return New decomposition. First entry is the new boxMin, second the new boxMax.
+	 */
 	virtual std::tuple<std::array<double, 3>, std::array<double, 3>> rebalance(double work) = 0;
 };
