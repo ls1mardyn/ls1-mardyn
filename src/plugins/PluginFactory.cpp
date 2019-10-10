a0fce5613d1205c4cb43043dd63084108dba9308
SteffenSeckler <steffen.seckler@tum.de>
2019-10-10 14:23:01 +0200

    completely format plugin factory

--- src/plugins/PluginFactory.cpp
+++ src/plugins/PluginFactory.cpp
@@ -1,10 +1,11 @@
+
 //
 // Created by Moritz Kruegener on 4/17/2018.
 //
 
 #include "PluginFactory.h"
-#include "Simulation.h"
 #include "Domain.h"
+#include "Simulation.h"
 
 #include <map>
 #include <string>
@@ -51,8 +52,8 @@
 #include "plugins/WallPotential.h"
 
 #ifdef VTK
-#include "io/vtk/VTKMoleculeWriter.h"
 #include "io/vtk/VTKGridWriter.h"
+#include "io/vtk/VTKMoleculeWriter.h"
 #endif
 
 /** @brief Register all default plugins with base PluginBase
@@ -105,68 +106,68 @@ void PluginFactory<PluginBase>::registerDefaultPlugins() {
 }
 
 /** @brief Enable selected plugins */
-template<>
-long PluginFactory<PluginBase>::enablePlugins(std::list<PluginBase*>& _plugins, XMLfileUnits& xmlconfig, std::string category, Domain* _domain) {
-    string oldpath = xmlconfig.getcurrentnodepath();
+template <>
+long PluginFactory<PluginBase>::enablePlugins(std::list<PluginBase*>& _plugins, XMLfileUnits& xmlconfig,
+											  std::string category, Domain* _domain) {
+	string oldpath = xmlconfig.getcurrentnodepath();
 
-    // plugins
-    long numPlugins = 0;
-    XMLfile::Query query = xmlconfig.query(category);
-    numPlugins = query.card();
-    global_log->info() << "Number of plugins with tag " << category << ": " << numPlugins << endl;
-    if(numPlugins < 1) {
-        global_log->warning() << "No plugins specified for tag" << category << "." << endl;
-    }
+	// plugins
+	long numPlugins = 0;
+	XMLfile::Query query = xmlconfig.query(category);
+	numPlugins = query.card();
+	global_log->info() << "Number of plugins with tag " << category << ": " << numPlugins << endl;
+	if (numPlugins < 1) {
+		global_log->warning() << "No plugins specified for tag" << category << "." << endl;
+	}
 
-    for (auto pluginIter = query.begin(); pluginIter; ++pluginIter) {
-        xmlconfig.changecurrentnode( pluginIter );
-        string pluginname("");
-        xmlconfig.getNodeValue("@name", pluginname);
-        bool enabled = true;
-        xmlconfig.getNodeValue("@enabled", enabled);
-        if(not enabled) {
-            global_log->debug() << "skipping disabled plugin: " << pluginname << endl;
-            continue;
-        }
-        global_log->info() << "Enabling plugin: " << pluginname << endl;
+	for (auto pluginIter = query.begin(); pluginIter; ++pluginIter) {
+		xmlconfig.changecurrentnode(pluginIter);
+		string pluginname("");
+		xmlconfig.getNodeValue("@name", pluginname);
+		bool enabled = true;
+		xmlconfig.getNodeValue("@enabled", enabled);
+		if (not enabled) {
+			global_log->debug() << "skipping disabled plugin: " << pluginname << endl;
+			continue;
+		}
+		global_log->info() << "Enabling plugin: " << pluginname << endl;
 
+		PluginBase* plugin = this->create(pluginname);
+		if (plugin == nullptr) {
+			global_log->warning() << "Could not create plugin using factory: " << pluginname << endl;
+		}
 
-        PluginBase* plugin = this->create(pluginname);
-        if(plugin == nullptr) {
-            global_log->warning() << "Could not create plugin using factory: " << pluginname << endl;
-        }
+		//@TODO: add plugin specific functions
 
-        //@TODO: add plugin specific functions
+		if (pluginname == "MmpldWriter") {
+			// @todo this should be handled in the MMPLD Writer readXML()
+			std::string sphere_representation = "simple";
+			xmlconfig.getNodeValue("@type", sphere_representation);
+			delete plugin;
+			if ("simple" == sphere_representation) {
+				plugin = new MmpldWriterSimpleSphere();
+			} else if ("multi" == sphere_representation) {
+				plugin = new MmpldWriterMultiSphere();
+			} else {
+				global_log->error() << "[MMPLD Writer] Unknown sphere representation type: " << sphere_representation
+									<< endl;
+				Simulation::exit(-1);
+			}
+		} else if (pluginname == "DomainProfiles") {
+			plugin = this->create("DensityProfileWriter");
+			// TODO: add _domain access (via Simularion)
+			_domain->readXML(xmlconfig);
+		}
 
-        if(pluginname == "MmpldWriter") {
-            // @todo this should be handled in the MMPLD Writer readXML()
-            std::string sphere_representation = "simple";
-            xmlconfig.getNodeValue("@type", sphere_representation);
-            delete plugin;
-            if("simple" == sphere_representation) {
-                plugin = new MmpldWriterSimpleSphere();
-            } else if("multi" == sphere_representation) {
-                plugin = new MmpldWriterMultiSphere ();
-            } else {
-                global_log->error() << "[MMPLD Writer] Unknown sphere representation type: " << sphere_representation << endl;
-                Simulation::exit(-1);
-            }
-        }
-        else if(pluginname == "DomainProfiles") {
-            plugin = this->create("DensityProfileWriter");
-            // TODO: add _domain access (via Simularion)
-            _domain->readXML(xmlconfig);
-        }
+		if (nullptr != plugin) {
+			plugin->readXML(xmlconfig);
+			_plugins.push_back(plugin);
+		} else {
+			global_log->warning() << "Unknown plugin " << pluginname << endl;
+		}
+	}
 
-        if(nullptr != plugin) {
-            plugin->readXML(xmlconfig);
-            _plugins.push_back(plugin);
-        } else {
-            global_log->warning() << "Unknown plugin " << pluginname << endl;
-        }
-    }
+	xmlconfig.changecurrentnode(oldpath);
 
-    xmlconfig.changecurrentnode(oldpath);
-
-    return numPlugins;
+	return numPlugins;
 }
