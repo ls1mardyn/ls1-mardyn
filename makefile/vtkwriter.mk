e93d0f760dbdcb0698b763ea2c73694237fe0460
Jeremy Harisch <jeremy@harisch.de>
2020-04-17 15:12:38 +0200

    Include Xerces-Paths on mpp3

--- makefile/vtkwriter.mk
+++ makefile/vtkwriter.mk
@@ -2,11 +2,13 @@
 $(info included vtkwriter.mk!)
 
 VTK_SOURCES = $(shell find ./ -name "*.cpp" | grep -v "/tests/" | grep "/vtk/")
+VTK_INCDIR = /dss/dsshome1/lrz/sys/spack/release/19.2/opt/x86_avx2/xerces-c/3.2.1-gcc-jz3anmr/bin
+VTK_LIBDIR = /dss/dsshome1/lrz/sys/spack/release/19.2/opt/x86_avx2/xerces-c/3.2.1-gcc-jz3anmr/lib
 
 SOURCES += $(VTK_SOURCES)
 CXXFLAGS += -DVTK
 
-INCLUDES += -isystem ../dependencies-external/libxsd 
+INCLUDES += -isystem ../dependencies-external/libxsd
 ifneq ($(VTK_INCDIR),)
   INCLUDES+= -isystem $(VTK_INCDIR)
   $(info Appended VTK_INCDIR)
