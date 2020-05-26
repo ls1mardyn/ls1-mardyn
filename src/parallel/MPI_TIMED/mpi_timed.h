9dabc36e8e24fe10cb87399ac4cae333e61575d2
Jeremy Harisch <jeremy@harisch.de>
2020-05-26 16:29:30 +0200

    Overwrite of MPI-Functions

--- src/parallel/MPI_TIMED/mpi_timed.h
+++ src/parallel/MPI_TIMED/mpi_timed.h
@@ -16,19 +16,20 @@ extern ProcessTimer _processTimer;
 extern "C"{
 #endif
 
-int __attribute__((weak)) MPI_Pcontrol(const int level, ...) {
+int MPI_Pcontrol(const int level, ...) {
 	return _processTimer.switchProfiling(level);
 }
 
-int __attribute__((weak)) MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
+int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
 						int tag, MPI_Comm comm) {
+	std::cout << "##################SEND##########" <<std::endl;
 	_processTimer.startTimer();
 	int result = PMPI_Send(buf, count, datatype, dest, tag, comm);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest,
+int MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest,
 					 int tag, MPI_Comm comm, MPI_Request *request) {
 	_processTimer.startTimer();
 	int result = PMPI_Isend(buf, count, datatype, dest, tag, comm, request);
@@ -36,7 +37,7 @@ int __attribute__((weak)) MPI_Isend(const void *buf, int count, MPI_Datatype dat
 	return result;
 }
 
-int __attribute__((weak)) MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
+int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
 					MPI_Comm comm, MPI_Status *status) {
 	_processTimer.startTimer();
 	int result = PMPI_Recv(buf, count, datatype, source, tag, comm, status);
@@ -44,7 +45,7 @@ int __attribute__((weak)) MPI_Recv(void *buf, int count, MPI_Datatype datatype, 
 	return result;
 }
 
-int __attribute__((weak)) MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
+int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
 					 MPI_Comm comm, MPI_Request *request) {
 	_processTimer.startTimer();
 	int result = PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
@@ -52,7 +53,7 @@ int __attribute__((weak)) MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
 	return result;
 }
 
-int __attribute__((weak)) MPI_File_open(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh) {
+int MPI_File_open(MPI_Comm comm, const char *filename, int amode, MPI_Info info, MPI_File *fh) {
 	_processTimer.startTimer();
 	int result = PMPI_File_open(comm, filename, amode, info, fh);
 	_processTimer.stopTimer();
@@ -60,7 +61,7 @@ int __attribute__((weak)) MPI_File_open(MPI_Comm comm, const char *filename, int
 }
 
 
-int __attribute__((weak)) MPI_File_close(MPI_File * fh){
+int MPI_File_close(MPI_File * fh){
 	_processTimer.startTimer();
 	int result = PMPI_File_close(fh);
 	_processTimer.stopTimer();
@@ -68,14 +69,14 @@ int __attribute__((weak)) MPI_File_close(MPI_File * fh){
 }
 
 
-int __attribute__((weak)) MPI_File_write(MPI_File mpi_fh, const void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+int MPI_File_write(MPI_File mpi_fh, const void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
 	_processTimer.startTimer();
 	int result = PMPI_File_write(mpi_fh, buf, count, datatype, status);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_File_write_at(MPI_File fh, MPI_Offset offset, const void * buf, int count,
+int MPI_File_write_at(MPI_File fh, MPI_Offset offset, const void * buf, int count,
 							 MPI_Datatype datatype, MPI_Status * status) {
 	_processTimer.startTimer();
 	int result = PMPI_File_write_at(fh, offset, buf, count, datatype, status);
@@ -83,21 +84,21 @@ int __attribute__((weak)) MPI_File_write_at(MPI_File fh, MPI_Offset offset, cons
 	return result;
 }
 
-int __attribute__((weak)) MPI_File_get_position(MPI_File fh, MPI_Offset * offset) {
+int MPI_File_get_position(MPI_File fh, MPI_Offset * offset) {
 	_processTimer.startTimer();
 	int result = PMPI_File_get_position(fh, offset);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Exscan(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+int MPI_Exscan(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
 	_processTimer.startTimer();
 	int result = PMPI_Exscan(sendbuf, recvbuf, count, datatype, op, comm);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Allreduce(const void* send_data, void* recv_data, int count, MPI_Datatype datatype, MPI_Op op,
+int MPI_Allreduce(const void* send_data, void* recv_data, int count, MPI_Datatype datatype, MPI_Op op,
 				  MPI_Comm communicator) {
 	_processTimer.startTimer();
 	int result = PMPI_Allreduce(send_data, recv_data, count, datatype, op, communicator);
@@ -105,7 +106,7 @@ int __attribute__((weak)) MPI_Allreduce(const void* send_data, void* recv_data, 
 	return result;
 }
 
-int __attribute__((weak)) MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
+int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
 					 int root, MPI_Comm comm) {
 	_processTimer.startTimer();
 	int result = PMPI_Bcast(buffer, count, datatype, root, comm);
@@ -113,14 +114,14 @@ int __attribute__((weak)) MPI_Bcast(void *buffer, int count, MPI_Datatype dataty
 	return result;
 }
 
-int __attribute__((weak)) MPI_Probe(int source, int tag, MPI_Comm comm, MPI_Status * status) {
+int MPI_Probe(int source, int tag, MPI_Comm comm, MPI_Status * status) {
 	_processTimer.startTimer();
 	int result = PMPI_Probe(source, tag, comm, status);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status * status) {
+int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status * status) {
 	_processTimer.startTimer();
 	int result = PMPI_Iprobe(source, tag, comm, flag, status);
 	_processTimer.stopTimer();
@@ -128,14 +129,14 @@ int __attribute__((weak)) MPI_Iprobe(int source, int tag, MPI_Comm comm, int *fl
 }
 
 
-int __attribute__((weak)) MPI_Test(MPI_Request * request, int *flag, MPI_Status * status) {
+int MPI_Test(MPI_Request * request, int *flag, MPI_Status * status) {
 	_processTimer.startTimer();
 	int result = PMPI_Test(request, flag, status);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Type_create_struct(int count, const int array_of_blocklengths[], const MPI_Aint array_of_displacements[],
+int MPI_Type_create_struct(int count, const int array_of_blocklengths[], const MPI_Aint array_of_displacements[],
 								  const MPI_Datatype array_of_types[], MPI_Datatype * newtype) {
 	_processTimer.startTimer();
 	int result = PMPI_Type_create_struct(count, array_of_blocklengths, array_of_displacements, array_of_types, newtype);
@@ -143,21 +144,21 @@ int __attribute__((weak)) MPI_Type_create_struct(int count, const int array_of_b
 	return result;
 }
 
-int __attribute__((weak)) MPI_Type_commit(MPI_Datatype * datatype) {
+int MPI_Type_commit(MPI_Datatype * datatype) {
 	_processTimer.startTimer();
 	int result = PMPI_Type_commit(datatype);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Type_free(MPI_Datatype *datatype) {
+int MPI_Type_free(MPI_Datatype *datatype) {
 	_processTimer.startTimer();
 	int result = PMPI_Type_free(datatype);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Cart_create(MPI_Comm comm_old, int ndims, const int dims[], const int periods[], int reorder,
+int MPI_Cart_create(MPI_Comm comm_old, int ndims, const int dims[], const int periods[], int reorder,
 						   MPI_Comm * comm_cart){
 	_processTimer.startTimer();
 	int result = PMPI_Cart_create(comm_old, ndims, dims, periods, reorder, comm_cart);
@@ -165,42 +166,42 @@ int __attribute__((weak)) MPI_Cart_create(MPI_Comm comm_old, int ndims, const in
 	return result;
 }
 
-int __attribute__((weak)) MPI_Cart_coords(MPI_Comm comm, int rank, int maxdims, int coords[]) {
+int MPI_Cart_coords(MPI_Comm comm, int rank, int maxdims, int coords[]) {
 	_processTimer.startTimer();
 	int result = PMPI_Cart_coords(comm, rank, maxdims, coords);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Cart_rank(MPI_Comm comm, const int coords[], int *rank) {
+int MPI_Cart_rank(MPI_Comm comm, const int coords[], int *rank) {
 	_processTimer.startTimer();
 	int result = PMPI_Cart_rank(comm, coords, rank);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Op_free(MPI_Op * op) {
+int MPI_Op_free(MPI_Op * op) {
 	_processTimer.startTimer();
 	int result = PMPI_Op_free(op);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Op_create(MPI_User_function *user_fn, int commute, MPI_Op *op) {
+int MPI_Op_create(MPI_User_function *user_fn, int commute, MPI_Op *op) {
 	_processTimer.startTimer();
 	int result = PMPI_Op_create(user_fn, commute, op);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Get_count(const MPI_Status * status, MPI_Datatype datatype, int *count) {
+int MPI_Get_count(const MPI_Status * status, MPI_Datatype datatype, int *count) {
 	_processTimer.startTimer();
 	int result = PMPI_Get_count(status, datatype, count);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Comm_free(MPI_Comm * comm) {
+int MPI_Comm_free(MPI_Comm * comm) {
 	_processTimer.startTimer();
 	int result = PMPI_Comm_free(comm);
 	_processTimer.stopTimer();
@@ -209,7 +210,7 @@ int __attribute__((weak)) MPI_Comm_free(MPI_Comm * comm) {
 
 /*
 // FIXME: Produces Seg-Fault
-int __attribute__((weak)) MPI_Comm_rank(MPI_Comm comm, int *rank) {
+int MPI_Comm_rank(MPI_Comm comm, int *rank) {
 	_processTimer.startTimer();
 	int result = PMPI_Comm_rank(comm, rank);
 	_processTimer.stopTimer();
@@ -217,21 +218,21 @@ int __attribute__((weak)) MPI_Comm_rank(MPI_Comm comm, int *rank) {
 }
 */
 
-int __attribute__((weak)) MPI_Get_address(const void *location, MPI_Aint * address) {
+int MPI_Get_address(const void *location, MPI_Aint * address) {
 	_processTimer.startTimer();
 	int result = PMPI_Get_address(location, address);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Dims_create(int nnodes, int ndims, int dims[]) {
+int MPI_Dims_create(int nnodes, int ndims, int dims[]) {
 	_processTimer.startTimer();
 	int result = PMPI_Dims_create(nnodes, ndims, dims);
 	_processTimer.stopTimer();
 	return result;
 }
 
-int __attribute__((weak)) MPI_Finalize(void) {
+int MPI_Finalize(void) {
 	int result = PMPI_Finalize();
 	// _processTimer.writeProcessTimeLog();
 	return result;
