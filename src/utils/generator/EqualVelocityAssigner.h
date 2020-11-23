d3591916466740dec63a8375d1d5bd9a6972c4ff
HomesGH <homes@tu-berlin.de>
2020-11-23 13:52:35 +0100

    Fixes bug and typos in velocity assigners

--- src/utils/generator/EqualVelocityAssigner.h
+++ src/utils/generator/EqualVelocityAssigner.h
@@ -5,19 +5,19 @@
 
 #include "VelocityAssignerBase.h"
 
-/** The VelocityAssigner can be used to assign normally distributed velocity vectors with absolute value mathching to a given temperature.
+/** The VelocityAssigner can be used to assign normally distributed velocity vectors with absolute value matching a given temperature.
  */
 class EqualVelocityAssigner : public VelocityAssignerBase {
 public:
-	EqualVelocityAssigner(double T = 0, int seed = 0) : VelocityAssignerBase(T), _mt(seed), _uniformDistrubtion(0, 1) {}
+	EqualVelocityAssigner(double T = 0, int seed = 0) : VelocityAssignerBase(T), _mt(seed), _uniformDistribution(0, 1) {}
 	~EqualVelocityAssigner(){}
 
 	void assignVelocity(Molecule *molecule) {
-		double v_abs = sqrt(/*kB=1*/ T() / molecule->component()->m());
+		double v_abs = sqrt(/*kB=1*/ (3+molecule->component()->getRotationalDegreesOfFreedom())*T() / molecule->component()->m());
 		/* pick angels for uniform distributino on S^2. */
 		double phi, theta;
-		phi   = 2*M_PI * _uniformDistrubtion(_mt);
-		theta = acos(2 * _uniformDistrubtion(_mt) - 1);
+		phi   = 2*M_PI * _uniformDistribution(_mt);
+		theta = acos(2 * _uniformDistribution(_mt) - 1);
 		double v[3];
 		v[0] = v_abs * sin(phi);
 		v[1] = v_abs * cos(phi) * sin(theta);
@@ -28,7 +28,7 @@ public:
 	}
 private:
 	std::mt19937 _mt; //!< Mersenne twister used as input for the uniform distribution
-	std::uniform_real_distribution<double> _uniformDistrubtion;
+	std::uniform_real_distribution<double> _uniformDistribution;
 };
 
 #endif  // SRC_UTILS_GENERATOR_EQUALVELOCITYASSIGNER_H_
