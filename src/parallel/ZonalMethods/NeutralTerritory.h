535f5b6a60e54a72f433f1526404139523bc3d88
Steffen Seckler <seckler@in.tum.de>
2020-11-25 16:40:01 +0100

    formats NeutralTerritory

--- src/parallel/ZonalMethods/NeutralTerritory.h
+++ src/parallel/ZonalMethods/NeutralTerritory.h
@@ -14,43 +14,44 @@
  * Shaw et. al. (A Fast, Scalable Method ..., 2005)
  *
  */
-class NeutralTerritory: public ZonalMethod {
+class NeutralTerritory : public ZonalMethod {
 public:
 	NeutralTerritory() = default;
 	~NeutralTerritory() override = default;
 
 	// TODO: This is untested
-	std::vector<HaloRegion> getHaloImportForceExportRegions(HaloRegion& initialRegion, double cutoffRadius, double /*skin*/,
-			bool coversWholeDomain[3], double cellLength[3]) override {
-		auto condition = [](const int d[3])->bool {
+	std::vector<HaloRegion> getHaloImportForceExportRegions(HaloRegion& initialRegion, double cutoffRadius,
+															double /*skin*/, bool coversWholeDomain[3],
+															double cellLength[3]) override {
+		auto condition = [](const int d[3]) -> bool {
 			int pseudoCellIndex = ((d[2] * 2) + d[1]) * 2 + d[2];
 			// determines, whether the region is in the disk
-				bool inDisk = (d[2] == 0) && pseudoCellIndex > 0;
+			bool inDisk = (d[2] == 0) && pseudoCellIndex > 0;
 
-				// determines, whether the region is in the tower
-				bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
+			// determines, whether the region is in the tower
+			bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
 
-				// return true, if region is in the tower or in the disk
-				return inDisk || inTower;
-			};
+			// return true, if region is in the tower or in the disk
+			return inDisk || inTower;
+		};
 		return getHaloRegionsConditional(initialRegion, cutoffRadius, 0., coversWholeDomain, condition);
 	}
 
 	// TODO: This is untested
-	std::vector<HaloRegion> getHaloExportForceImportRegions(HaloRegion& initialRegion, double cutoffRadius, double /*skin*/,
-			bool coversWholeDomain[3], double cellLength[3]) override {
-		auto condition = [](const int d[3])->bool {
+	std::vector<HaloRegion> getHaloExportForceImportRegions(HaloRegion& initialRegion, double cutoffRadius,
+															double /*skin*/, bool coversWholeDomain[3],
+															double cellLength[3]) override {
+		auto condition = [](const int d[3]) -> bool {
 			int pseudoCellIndex = ((d[2] * 2) + d[1]) * 2 + d[2];
 			// determines, whether the region is in the disk
-				bool inDisk = (d[2] == 0) && pseudoCellIndex < 0;
-				//difference to haloimportforceexport: here we have a "<"
-				// determines, whether the region is in the tower
-				bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
+			bool inDisk = (d[2] == 0) && pseudoCellIndex < 0;
+			// difference to haloimportforceexport: here we have a "<"
+			// determines, whether the region is in the tower
+			bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
 
-				// return true, if region is in the tower or in the disk
-				return inDisk || inTower;
-			};
+			// return true, if region is in the tower or in the disk
+			return inDisk || inTower;
+		};
 		return getHaloRegionsConditional(initialRegion, cutoffRadius, 0., coversWholeDomain, condition);
 	}
 };
-
