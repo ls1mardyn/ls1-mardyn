0a50d78e3031e910ad0be3279e76a4701556449c
Steffen Seckler <seckler@in.tum.de>
2020-11-25 17:59:47 +0100

    fixes NeutralTerritory zonal method.

--- src/parallel/ZonalMethods/NeutralTerritory.h
+++ src/parallel/ZonalMethods/NeutralTerritory.h
@@ -19,39 +19,39 @@ public:
 	NeutralTerritory() = default;
 	~NeutralTerritory() override = default;
 
-	// TODO: This is untested
 	std::vector<HaloRegion> getHaloImportForceExportRegions(HaloRegion& initialRegion, double cutoffRadius,
 															double /*skin*/, bool coversWholeDomain[3],
 															double cellLength[3]) override {
 		auto condition = [](const int d[3]) -> bool {
-			int pseudoCellIndex = ((d[2] * 2) + d[1]) * 2 + d[2];
-			// determines, whether the region is in the disk
-			bool inDisk = (d[2] == 0) && pseudoCellIndex > 0;
+			// Determines, whether the region is in the disk.
+			// Here, we cannot directly apply the stencil from the NT traversal, as for multiple cells also the
+			// direction x=0 and y=-1 needs to be taken into account.
+			bool inDisk = (d[2] == 0) && d[0] >= 0;
 
-			// determines, whether the region is in the tower
+			// Determines, whether the region is in the tower.
 			bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
 
-			// return true, if region is in the tower or in the disk
+			// Return true, if region is in the tower or in the disk.
 			return inDisk || inTower;
 		};
 		return getHaloRegionsConditional(initialRegion, cutoffRadius, 0., coversWholeDomain, condition);
 	}
 
-	// TODO: This is untested
 	std::vector<HaloRegion> getHaloExportForceImportRegions(HaloRegion& initialRegion, double cutoffRadius,
 															double /*skin*/, bool coversWholeDomain[3],
 															double cellLength[3]) override {
 		auto condition = [](const int d[3]) -> bool {
-			int pseudoCellIndex = ((d[2] * 2) + d[1]) * 2 + d[2];
-			// determines, whether the region is in the disk
-			bool inDisk = (d[2] == 0) && pseudoCellIndex < 0;
-			// difference to haloimportforceexport: here we have a "<"
-			// determines, whether the region is in the tower
+			// Determines, whether the region is in the disk.
+			// Here, we cannot directly apply the stencil from the NT traversal, as for multiple cells also the
+			// direction x=0 and y=-1 needs to be taken into account.
+			bool inDisk = (d[2] == 0) && d[0] <= 0;
+			// Difference to haloimportforceexport: here we have a "<="
+			// Determines, whether the region is in the tower.
 			bool inTower = (d[0] == 0) && (d[1] == 0) && (d[2] != 0);
 
-			// return true, if region is in the tower or in the disk
+			// Return true, if region is in the tower or in the disk.
 			return inDisk || inTower;
 		};
-		return getHaloRegionsConditional(initialRegion, cutoffRadius, 0., coversWholeDomain, condition);
+		return getHaloRegionsConditionalInside(initialRegion, cutoffRadius, 0., coversWholeDomain, condition);
 	}
 };
