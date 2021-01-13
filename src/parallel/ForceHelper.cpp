1b22e9fbd500a582078f26b24cb8df0226fcf931
Steffen Seckler <seckler@in.tum.de>
2021-01-13 15:26:34 +0100

    doc and renames

--- src/parallel/ForceHelper.cpp
+++ src/parallel/ForceHelper.cpp
@@ -7,35 +7,40 @@ std::variant<ParticleIterator, SingleCellIterator<ParticleCell>> addValuesAndGet
 	std::variant<ParticleIterator, SingleCellIterator<ParticleCell>> previousIterator,
 	Molecule& haloMolecule) {
 	// Reuse the previous iterator in case the particles match.
-	bool found = false;
+	bool usePreviousIterator = false;
 	std::visit(
 		[&](auto& previousIter) {
 			if (previousIter.isValid()) {
+				// Increment pointer!
 				++previousIter;
+
 				if (previousIter.isValid()) {
 					const double epsi = moleculeContainer->getCutoff() * 1e-6;
+					// If the particle pointed to by previousIter is close to the given position, use it.
 					if (fabs(previousIter->r(0) - position[0]) <= epsi and
 						fabs(previousIter->r(1) - position[1]) <= epsi and
 						fabs(previousIter->r(2) - position[2]) <= epsi) {
-						found = true;
+						usePreviousIterator = true;
 					}
 				}
 			}
 		},
 		previousIterator);
+
 	// If the previousIterator matches, use it!
-	auto originalIter = found ? previousIterator : moleculeContainer->getMoleculeAtPosition(position);
+	auto originalIter = usePreviousIterator ? previousIterator : moleculeContainer->getMoleculeAtPosition(position);
 
 	std::visit(
 		[&](auto originalIter) {
 			if (not originalIter.isValid()) {
 				// This should not happen
-				std::cout << "Original molecule not found";
+				std::cout << "Original molecule not usePreviousIterator";
 				mardyn_exit(1);
 			}
 
 			mardyn_assert(originalIter->getID() == haloMolecule.getID());
 
+			// Add the force values.
 			originalIter->Fadd(haloMolecule.F_arr().data());
 			originalIter->Madd(haloMolecule.M_arr().data());
 			originalIter->Viadd(haloMolecule.Vi_arr().data());
--- src/parallel/ForceHelper.h
+++ src/parallel/ForceHelper.h
@@ -3,14 +3,15 @@
 #include <variant>
 
 /**
- *
- * @param moleculeContainer
- * @param position
- * @param previousIterator
- * @param haloMolecule
- * @return
+ * Adds the force values (force (F), torque (M) and virial (Vi)) of haloMolecule to the particle at the given position.
+ * The function will reuse the previousIterator if after an increment it points to a particle at the given position.
+ * @param moleculeContainer The molecule container.
+ * @param position The position of the particle to which the force values will be added.
+ * @param previousIterator Can be used to speed up the force exchange. The iterator returned from this function should
+ * be used for this.
+ * @param haloMolecule The molecule whose force values will be added to the particle at the given position.
+ * @return Iterator to the current particle. Can be used in the next iteration as previousIterator.
  */
 std::variant<ParticleIterator, SingleCellIterator<ParticleCell>> addValuesAndGetIterator(
 	ParticleContainer* moleculeContainer, const double* position,
-	std::variant<ParticleIterator, SingleCellIterator<ParticleCell>> previousIterator,
-	Molecule& haloMolecule);
+	std::variant<ParticleIterator, SingleCellIterator<ParticleCell>> previousIterator, Molecule& haloMolecule);
