749b108d3f34457f1847fe5ef80df0047c845a6e
FG-TUM <f.gratl@tum.de>
2021-02-11 20:08:14 +0100

    cleanup output and accept multiple input files

--- tools/decomp2vtkConverter.py
+++ tools/decomp2vtkConverter.py
@@ -32,7 +32,6 @@ class Cell:
         for elem in configString.split(' , ') :
             [key, value] = elem.split(': ')
             self.config[key] = value
-        print(self.config)
         
     def getPointCoordinates(self):
         output = io.StringIO()
@@ -62,99 +61,88 @@ class Cell:
 
 #### input parsing
 print('')
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print('ERROR: Not enough arguments given, need at least:')
     print('[1] input file name')
-    print('[2] output file name')
 
-inputFileName = str(sys.argv[1])
-outputFileName = str(sys.argv[2])
+for inputFileName in sys.argv[1:]:
+    outputFileName = inputFileName.replace('.decomp', '.vtk')
 
-print('Input file name:', inputFileName)
-print('Output file name:', outputFileName)
-
-if inputFileName.find('.decomp') == -1:
-    print('ERROR: wrong input file format, should be *.decomp')
-    sys.exit()
-if outputFileName.find('.vtk') == -1:
-    print('ERROR: wrong output file format, should be *.vtk')
-    sys.exit()
-
-f_in = open(inputFileName, 'r')
-f_out = open(outputFileName, 'w')
-
-### data validation
-currentLine = f_in.readline()
-while currentLine.find('decompData Regions') != -1:
-    if currentLine != '':
-        print('ERROR: reached end of file, but require "decompData Regions"')
+    if inputFileName.find('.decomp') == -1:
+        print('ERROR: wrong input file format, should be *.decomp')
         sys.exit()
-    print(currentLine)
+
+    f_in = open(inputFileName, 'r')
+    f_out = open(outputFileName, 'w')
+    ### data validation
     currentLine = f_in.readline()
-f_in.readline()  # skip decompData Regions
+    while currentLine.find('decompData Regions') != -1:
+        if currentLine != '':
+            print('ERROR: reached end of file, but require "decompData Regions"')
+            sys.exit()
+        currentLine = f_in.readline()
+    f_in.readline()  # skip decompData Regions
 
-
-### data parsing
-numcells = 0
-line = f_in.readline()
-celllist = []
-print('--------------------- START DEBUG OUTPUT ---------------------')
-while line.find('particleData') == -1 and line != "":
-    match=re.search(r'([0-9. ]+)(.*)', line)
-    minMaxString=match.group(1).rstrip()
-    configString=match.group(2)
-
-    celllist.append(Cell(minMaxString, numcells, configString))
-    numcells += 1
+    ### data parsing
+    numcells = 0
     line = f_in.readline()
-print('---------------------- END DEBUG OUTPUT ----------------------')
+    celllist = []
+    while line.find('particleData') == -1 and line != "":
+        match=re.search(r'([0-9. ]+)(.*)', line)
+        minMaxString=match.group(1).rstrip()
+        configString=match.group(2)
 
-print('Found', numcells, 'cells.')
-# Write VTK header and dataset information
-f_out.write('# vtk DataFile Version 2.0\n'
-        + 'MarDyn decomposition output\n'
-        + 'ASCII\n'
-        + 'DATASET UNSTRUCTURED_GRID\n'
-        )
+        celllist.append(Cell(minMaxString, numcells, configString))
+        numcells += 1
+        line = f_in.readline()
 
-# write cell corner points as point list
-f_out.write('POINTS ' + str(8 * numcells) + ' float\n')  # 8 points per cell
-for cell in celllist:
-    f_out.write(cell.getPointCoordinates())
+    print(inputFileName + ' -> ' + outputFileName + ' (' + str(numcells) + ' cells)')
+    # Write VTK header and dataset information
+    f_out.write('# vtk DataFile Version 2.0\n'
+            + 'MarDyn decomposition output\n'
+            + 'ASCII\n'
+            + 'DATASET UNSTRUCTURED_GRID\n'
+            )
 
-# write cells. Every line is a cell
-f_out.write('CELLS ' + str(numcells) + ' ' + str(numcells * 9) + '\n')
-# CELLS requires numcells and number of total list entries (=numcells + 8*numcells)
-for cell in celllist:
-    f_out.write('8 ' + cell.getCellPointIndices())
-
-# write vtk cell types
-f_out.write('CELL_TYPES ' + str(numcells) + '\n')
-for cell in celllist:
-    f_out.write('11\n')  # type 11 = VTK_VOXEL = cuboid
-
-# write ids for every cell. They are not necessarily the same as the MPI Rank ID
-f_out.write('CELL_DATA ' + str(numcells) + '\n'
-        + 'SCALARS cell_scalars int 1\n'
-        + 'LOOKUP_TABLE default\n'
-        )
-i=0
-for cell in celllist:
-    f_out.write(str(i) + '\n')
-    i = i + 1
-
-# write all configuration information as field data
-f_out.write('FIELD ConfigurationData '+ str(len(celllist[0].config.keys()) + 1) +'\n')
-# write one combined string
-f_out.write('FullConfiguration 1 ' + str(numcells) + ' string\n')
-for cell in celllist:
-    f_out.write('+'.join(cell.config.values()) + '\n')
-# write each config property individually
-for configKey in celllist[0].config.keys():
-    f_out.write('\n' + configKey.replace(' ', '') + ' 1 ' + str(numcells) + ' string\n')
+    # write cell corner points as point list
+    f_out.write('POINTS ' + str(8 * numcells) + ' float\n')  # 8 points per cell
     for cell in celllist:
-        f_out.write(cell.config[configKey] + '\n')
+        f_out.write(cell.getPointCoordinates())
 
-# finish
-f_in.close()
-f_out.close()
+    # write cells. Every line is a cell
+    f_out.write('CELLS ' + str(numcells) + ' ' + str(numcells * 9) + '\n')
+    # CELLS requires numcells and number of total list entries (=numcells + 8*numcells)
+    for cell in celllist:
+        f_out.write('8 ' + cell.getCellPointIndices())
+
+    # write vtk cell types
+    f_out.write('CELL_TYPES ' + str(numcells) + '\n')
+    for cell in celllist:
+        f_out.write('11\n')  # type 11 = VTK_VOXEL = cuboid
+
+    # write ids for every cell. They are not necessarily the same as the MPI Rank ID
+    f_out.write('CELL_DATA ' + str(numcells) + '\n'
+            + 'SCALARS cell_scalars int 1\n'
+            + 'LOOKUP_TABLE default\n'
+            )
+    i=0
+    for cell in celllist:
+        f_out.write(str(i) + '\n')
+        i = i + 1
+
+    # write all configuration information as field data
+    f_out.write('FIELD ConfigurationData '+ str(len(celllist[0].config.keys()) + 1) +'\n')
+    # write one combined string
+    f_out.write('FullConfiguration 1 ' + str(numcells) + ' string\n')
+    for cell in celllist:
+        f_out.write('+'.join(cell.config.values()) + '\n')
+    # write each config property individually
+    for configKey in celllist[0].config.keys():
+        f_out.write('\n' + configKey.replace(' ', '') + ' 1 ' + str(numcells) + ' string\n')
+        for cell in celllist:
+            f_out.write(cell.config[configKey] + '\n')
+
+    # finish
+    f_in.close()
+    f_out.close()
+
