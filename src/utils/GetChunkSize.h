21d182d9f37de09a60588e69360514129bf816d5
Steffen Seckler <38119442+SteffenSeckler@users.noreply.github.com>
2021-02-17 13:36:04 +0100

    Apply suggestions from code review
    Co-authored-by: FG-TUM <FG-TUM@users.noreply.github.com>

--- src/utils/GetChunkSize.h
+++ src/utils/GetChunkSize.h
@@ -3,12 +3,12 @@
 namespace chunk_size {
 
 /**
- * Get the chunk size based on the given magic numbers.
+ * Formula to calculate a chunk size for OpenMP loop parallelization based on the given magic numbers.
+ * The chunk size is the number of loop iterations scheduled together as one task.
  * @param loop_size The number of iterations.
- * @param max_num_chunks should be big enough to provide sufficient scheduling, but not too big, as too much overhead
- * might exist.
- * @param max_chunk_size should not be bigger than needed. When iterating the specified number of work items, the
- * overhead should be negligible. Bigger numbers should not produce any better results.
+ * @param max_num_chunks There should be enough chunks so that load can be balanced, but not too many since scheduling creates overhead.
+ * @param max_chunk_size When iterating the specified number of work items, the overhead should be negligible. 
+ *  Bigger numbers should not produce any better results.
  * @return The chunk size based on the given numbers.
  * @note max_chunk_size is the more dominant factor, i.e., if max_num_chunks would indicate bigger chunks,
  * max_chunk_size is restricting the chunk size.
