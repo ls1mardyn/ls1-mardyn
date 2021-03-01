aae6163a853394145cf1cb391397109a5be168d9
Steffen Seckler <seckler@in.tum.de>
2021-03-01 08:48:32 +0100

    Doc

--- src/io/PerCellGenerator.h
+++ src/io/PerCellGenerator.h
@@ -10,36 +10,48 @@ class Component;
  */
 class PerCellGenerator : public InputBase {
 public:
-	void readPhaseSpaceHeader(Domain *domain, double timestep) override{};
+	/**
+	 * The following xml object structure is handled by this method:
+	 * \code{.xml}
+	   <generator name="PerCellGenerator">
+		<!-- required field: number of molecules per cell -->
+		<numMoleculesPerCell>UNSIGNED INT<numMoleculesPerCell/>
+		<!-- required field: initial temperature -->
+		<initTemperature>DOUBLE<initTemperature/>
+		<!-- optional field (default=true): Iff true: Generates two particles per process if numMoleculesPerCell==0.
+			 If no particles were generated at all, the simulation crashes.-->
+		<generateAtLeastTwoParticles>BOOL</generateAtLeastTwoParticles>
+	   </generator>
+	   \endcode
+	 * @param xmlconfig
+	 */
+	void readXML(XMLfileUnits &xmlconfig) override;
+
+	void readPhaseSpaceHeader(Domain *domain, double timestep) override{
+		// Empty as no header file exists.
+	};
 
 	unsigned long readPhaseSpace(ParticleContainer *particleContainer, Domain *domain,
 								 DomainDecompBase *domainDecomp) override;
 
 	/**
-	 *
+	 * Fills the container with the specified number of molecules (with given component) per cell.
 	 * @param particleContainer
 	 * @param component
 	 * @param numMoleculesPerCell
-	 * @param fillHalo
+	 * @param fillHalo Indicates whether to fill the halo cells as well.
 	 */
 	static void fillContainer(ParticleContainer *particleContainer, Component *component,
 							  unsigned int numMoleculesPerCell, bool fillHalo);
 
 	/**
-	 *
+	 * Generates two particles/molecules at random positions within the boundaries of particleContainer.
 	 * @param particleContainer
-	 * @param component
+	 * @param component The component of the particles.
 	 */
 	static void generateTwoParticles(ParticleContainer *particleContainer, Component *component);
 
-	/**
-	 *
-	 * @param xmlconfig
-	 */
-	void readXML(XMLfileUnits &xmlconfig) override;
-
 private:
-
 	unsigned int _numMoleculesPerCell{std::numeric_limits<unsigned int>::max()};
 
 	bool _generateAtLeastTwoParticles{true};
