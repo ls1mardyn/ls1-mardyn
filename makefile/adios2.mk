b94da0e064a63ede4b1eee94fe4971fba8d3ea01
Christoph Niethammer <christoph.niethammer@gmail.com>
2021-04-19 19:51:19 +0200

    Add ADIOS2 option for Makefile builds
    Setting the variable ADIOS2=1 for Makefile builds in the src directory
    enables building with ADIOS2 support, i.e., the  AdiosWriter/Reader.
    ADIOS2 headers and libraries are detected via the adios2-config tool.
    If the tool is not in the PATH one can use the ADIOS2_INCDIR and
    ADIOS2_LIBDIR variables to provide the necessary paths for compiling
    and linking.
    
    Signed-off-by: Christoph Niethammer <niethammer@hlrs.de>

---
+++ makefile/adios2.mk
@@ -0,0 +1,20 @@
+$(info included adios2.mk!)
+
+ADIOS2_SOURCES = $(shell find ./ -name "*.cpp" | grep -v "/tests/" | grep "Adios")
+
+SOURCES += $(ADIOS2_SOURCES)
+CXXFLAGS += -DENABLE_ADIOS2
+
+ifneq ($(ADIOS2_INCDIR),)
+  INCLUDES+= -isystem $(ADIOS2_INCDIR)
+  $(info Appended ADIOS2_INCDIR)
+else
+  INCLUDES+= $(shell adios2-config --cxx-flags)
+endif
+
+ifneq ($(ADIOS2_LIBDIR),)
+  LDFLAGS += -L$(ADIOS2_LIBDIR)
+  $(info Appended ADIOS2_LIBDIR)
+else
+  LDFLAGS += $(shell adios2-config --cxx-libs)
+endif
