9016496c268901d9c34ced344c312e3a44069f6e
rauts <tobias.rau@visus.uni-stuttgart.de>
2021-04-19 13:21:08 +0200

    Implemented ADIOS2 Writer as output plugin. Not working yet.

---
+++ src/plugins/AdiosWriter.cpp
@@ -0,0 +1,277 @@
+/*
+ * AdiosWriter.cpp
+ *
+ *  Created on: April 2019
+ *      Author: Tobias Rau
+ */
+
+#include "AdiosWriter.h"
+#include "particleContainer/ParticleContainer.h"
+#include "Domain.h"
+#include "Simulation.h"
+#include "parallel/DomainDecompBase.h"
+#include "utils/mardyn_assert.h"
+
+using Log::global_log;
+
+void AdiosWriter::init(ParticleContainer* particleContainer,
+        DomainDecompBase* domainDecomp, Domain* domain) {
+
+        // set variable name to write
+		vars["molecule_id"] = std::vector<uint64_t>();
+		vars["component_id"] = std::vector<uint64_t>();
+        vars["rx"] = std::vector<double>();
+        vars["ry"] = std::vector<double>();
+        vars["rz"] = std::vector<double>();
+        vars["vx"] = std::vector<double>();
+        vars["vy"] = std::vector<double>();
+        vars["vz"] = std::vector<double>();
+        vars["qw"] = std::vector<double>();
+        vars["qx"] = std::vector<double>();
+        vars["qy"] = std::vector<double>();
+        vars["qz"] = std::vector<double>();
+        vars["Lx"] = std::vector<double>();
+        vars["Ly"] = std::vector<double>();
+        vars["Lz"] = std::vector<double>();
+
+};
+
+void AdiosWriter::readXML(XMLfileUnits& xmlconfig) {
+
+
+  fname = "test.bp";
+  _writefrequency = 50000;
+  xmlconfig.getNodeValue("outputfile", fname);
+  xmlconfig.getNodeValue("writefrequency", _writefrequency);
+  global_log->info() << "    AW: readXML." << std::endl;
+
+  if (!inst) initAdios();
+
+};
+
+void AdiosWriter::initAdios() {
+  auto& domainDecomp = _simulation.domainDecomposition();
+
+  try {
+    //get adios instance
+        inst = std::make_shared<adios2::ADIOS>();
+
+        // declare io as output
+        io = std::make_shared<adios2::IO>(inst->DeclareIO("Output"));
+
+        // use bp engine
+        io->SetEngine("BP4");
+
+        if (!engine) {
+            global_log->info() << "    AW: Opening File for writing." << fname.c_str() << std::endl;
+            engine = std::make_shared<adios2::Engine>(io->Open(fname, adios2::Mode::Write));
+        }
+  }
+    catch (std::invalid_argument& e) {
+        global_log->fatal() 
+                << "Invalid argument exception, STOPPING PROGRAM from rank" 
+                << domainDecomp.getRank() 
+                << ": " << e.what() << std::endl;
+	mardyn_exit(1);
+    } catch (std::ios_base::failure& e) {
+        global_log->fatal() 
+                << "IO System base failure exception, STOPPING PROGRAM from rank " 
+                << domainDecomp.getRank() 
+                << ": " << e.what() << std::endl;
+	mardyn_exit(1);
+    } catch (std::exception& e) {
+        global_log->fatal() 
+                << "Exception, STOPPING PROGRAM from rank" 
+                << domainDecomp.getRank() 
+                << ": " << e.what() << std::endl;
+	mardyn_exit(1);
+    }
+    global_log->info() << "    AW: Init complete." << std::endl;
+};
+
+void AdiosWriter::beforeEventNewTimestep(
+        ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+        unsigned long simstep) {
+    global_log->info() << "    AW: beforeEventNewTimestep." << std::endl;
+
+};
+
+void AdiosWriter::beforeForces(
+        ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+        unsigned long simstep
+) {
+    global_log->info() << "    AW: beforeForces." << std::endl;
+
+};
+
+void AdiosWriter::afterForces(
+        ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+        unsigned long simstep
+) {
+    global_log->info() << "    AW: afterForces." << std::endl;
+
+};
+
+void AdiosWriter::endStep(
+        ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+        Domain* domain, unsigned long simstep) {
+
+  if (simstep % _writefrequency != 0) return;
+  
+    // for all outputs
+    size_t localNumParticles = particleContainer->getNumberOfParticles();
+    size_t const globalNumParticles = domain->getglobalNumMolecules();
+    int const numProcs = domainDecomp->getNumProcs();
+	int const rank = domainDecomp->getRank();
+
+    std::vector<uint64_t> m_id;
+    m_id.reserve(localNumParticles);
+
+    std::vector<uint32_t> comp_id;
+    comp_id.reserve(localNumParticles);
+
+    for (auto m = particleContainer->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY); m.isValid(); ++m) {
+	    std::get<std::vector<uint64_t>>(vars["molecule_id"]).emplace_back(m->getID());
+		std::get<std::vector<uint64_t>>(vars["component_id"]).emplace_back(m->componentid());
+		std::get<std::vector<double>>(vars["rx"]).emplace_back(m->r(0));
+		std::get<std::vector<double>>(vars["ry"]).emplace_back(m->r(1));
+		std::get<std::vector<double>>(vars["rz"]).emplace_back(m->r(2));
+        std::get<std::vector<double>>(vars["vx"]).emplace_back(m->v(0));
+        std::get<std::vector<double>>(vars["vy"]).emplace_back(m->v(1));
+        std::get<std::vector<double>>(vars["vz"]).emplace_back(m->v(2));
+        auto q = m->q();
+        std::get<std::vector<double>>(vars["qw"]).emplace_back(q.qw());
+        std::get<std::vector<double>>(vars["qx"]).emplace_back(q.qx());
+        std::get<std::vector<double>>(vars["qy"]).emplace_back(q.qy());
+        std::get<std::vector<double>>(vars["qz"]).emplace_back(q.qz());
+        std::get<std::vector<double>>(vars["Lx"]).emplace_back(m->D(0));
+        std::get<std::vector<double>>(vars["Ly"]).emplace_back(m->D(1));
+        std::get<std::vector<double>>(vars["Lz"]).emplace_back(m->D(2));
+
+        m_id.emplace_back(m->getID());
+        comp_id.emplace_back(m->componentid());
+	}
+  
+    global_log->set_mpi_output_all();
+
+    // gather offsets
+    global_log->info() << "numProcs: " << numProcs 
+            << " localNumParticles: " << localNumParticles
+            << " domainDecomp->getRank(): " << domainDecomp->getRank() << std::endl;
+
+    uint64_t offset = 0;
+	MPI_Exscan(&localNumParticles, &offset, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+
+	global_log->info() << "    AW: localNumParticles " << localNumParticles << std::endl;
+	global_log->info() << "    AW: Offset " << offset << std::endl;
+
+    std::array<double,6> global_box{0, 0,0, domain->getGlobalLength(0), domain->getGlobalLength(1), domain->getGlobalLength(2)};
+    std::array<double,6> local_box;
+    domainDecomp->getBoundingBoxMinMax(domain, &local_box[0], &local_box[3]);
+    
+    global_log->info() << "Local Box: " << local_box[0] << " "<< local_box[1]<< " " << local_box[2] << " "<< local_box[3] << " "<< local_box[4]<< " " << local_box[5] <<  std::endl;
+    try {
+	    engine->BeginStep();
+	    io->RemoveAllVariables();
+	  //  for (auto& [variableName,variableContainer] : vars) {
+			//global_log->info() << "    AW: Defining Variables " << variableName << std::endl;
+
+   //   		if (std::holds_alternative<std::vector<double>>(variableContainer)) {
+		 //       adios2::Variable<double> adiosVar = io->DefineVariable<double>(variableName, {globalNumParticles},
+		 //                   {offset},
+		 //                   {localNumParticles}, adios2::ConstantDims);
+
+			//	// ready for write; transfer data to adios
+			//	engine->Put<double>(adiosVar, std::get<std::vector<double>>(variableContainer).data());
+			//} else {
+			//	adios2::Variable<uint64_t> adiosVar =
+			//		io->DefineVariable<uint64_t>(variableName, {globalNumParticles}, {offset},
+			//																   {localNumParticles}, adios2::ConstantDims);
+			//	// ready for write; transfer data to adios
+			//	engine->Put<uint64_t>(adiosVar, std::get<std::vector<uint64_t>>(variableContainer).data());
+			//}
+	  //  }
+
+	    // global box
+	    if(domainDecomp->getRank() == 0) {
+	    adios2::Variable<double> adios_global_box =
+	        io->DefineVariable<double>("global_box", {6}, {0}, {6}, adios2::ConstantDims);
+
+	    global_log->info() << "    AW: Putting Variables" << std::endl;
+	    if (!adios_global_box) {
+	        global_log->error() << "    AW: Could not create variable: global_box" << std::endl;
+	        return;
+	    }
+	    engine->Put<double>(adios_global_box, global_box.data());
+	    }
+
+	 //   //local box
+	 //   adios2::Variable<double> adios_local_box =
+		//io->DefineVariable<double>("local_box", {},
+		//            {},
+		//            {6});
+
+		//if (!adios_local_box) {
+		//    global_log->error() << "    AW: Could not create variable: local_box" << std::endl;
+		//    return;
+		//}
+		//engine->Put<double>(adios_local_box, local_box.data());
+
+        // offsets
+		adios2::Variable<uint64_t> adios_offset = io->DefineVariable<uint64_t>(
+			"offsets", {static_cast<size_t>(numProcs)}, {static_cast<size_t>(rank)}, {1}, false);
+		engine->Put<uint64_t>(adios_offset, offset);
+
+    	
+	    //simulation time
+	    current_time = _simulation.getSimulationTime();
+	    if(domainDecomp->getRank() == 0) {
+	        adios2::Variable<double> adios_simulationtime =
+	            io->DefineVariable<double>("simulationtime");
+	        if (!adios_simulationtime) {
+	            global_log->error() << "    AW: Could not create variable: simulationtime" << std::endl;
+	            return;
+	        }             
+	        engine->Put<double>(adios_simulationtime, current_time);
+
+            // write number of procs
+			adios2::Variable<int> adios_numprocs = io->DefineVariable<int>("numProcs");
+			engine->Put<int>(adios_numprocs, numProcs);
+	    }
+
+
+	    // wait for completion of write
+	    engine->EndStep();
+	}
+	catch (std::invalid_argument& e) {
+		global_log->error() <<
+			"[ADIOS2] Invalid argument exception, STOPPING PROGRAM";
+		global_log->error() <<e.what();
+	}
+	catch (std::ios_base::failure& e) {
+		global_log->error() <<
+			"[ADIOS2] IO System base failure exception, STOPPING PROGRAM";
+		global_log->error() <<e.what();
+	}
+	catch (std::exception& e) {
+		global_log->error() <<"[ADIOS2] Exception, STOPPING PROGRAM";
+		global_log->error() <<e.what();
+	}
+    global_log->info() << "    AW: endStep." << std::endl;
+};
+
+int AdiosWriter::getOffset(std::vector<int>& offsets, int const rank) {
+    int sum = 0;
+    for (auto i=0; i<rank; ++i) {
+        sum += offsets[i];
+    }
+    return sum;
+}
+
+void AdiosWriter::finish(ParticleContainer* particleContainer,
+        DomainDecompBase* domainDecomp, Domain* domain
+) {
+    engine->Close();
+    global_log->info() << "    AW: finish." << std::endl;
+}
+
---
+++ src/plugins/AdiosWriter.h
@@ -0,0 +1,91 @@
+/*
+ * AdiosWriter.h
+ *
+ *  Created on: 11 Jul 2018
+ *      Author: Oliver Fernandes
+ */
+
+///
+/// \file AdiosWriter.h
+/// Insitu Megamol Plugin Header. See the AdiosWriter class description for a manual on how to use the plugin
+///
+
+#pragma once
+
+#include "PluginBase.h"
+#include "molecules/MoleculeForwardDeclaration.h"
+
+#include <chrono>
+#include <set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <memory>
+#include <map>
+#include <errno.h>
+
+#include <variant>
+
+#ifdef ENABLE_ADIOS2
+
+#include <adios2.h>
+#include <mpi.h>
+
+class AdiosWriter : public PluginBase {
+public:
+    AdiosWriter() {}; 
+    virtual ~AdiosWriter() {};
+
+    void init(ParticleContainer* particleContainer,
+            DomainDecompBase* domainDecomp, Domain* domain
+    );
+
+    void readXML(XMLfileUnits& xmlconfig);
+
+    void beforeEventNewTimestep(
+            ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+            unsigned long simstep
+    );
+
+    void beforeForces(
+            ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+            unsigned long simstep
+    );
+
+    void afterForces(
+            ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+            unsigned long simstep
+    );
+
+    void endStep(
+            ParticleContainer* particleContainer, DomainDecompBase* domainDecomp,
+            Domain* domain, unsigned long simstep
+    );
+    
+    void finish(ParticleContainer* particleContainer,
+            DomainDecompBase* domainDecomp, Domain* domain);
+
+    std::string getPluginName() {
+        return std::string("AdiosWriter");
+    }
+
+    static PluginBase* createInstance() { return new AdiosWriter(); }
+
+protected:
+    // 
+private:
+  void initAdios();
+    int getOffset(std::vector<int>&, int const);
+    // output filename, from XML
+    std::string fname;
+    uint32_t _writefrequency;
+    double current_time;
+    // variables to write, see documentation
+    std::map<std::string, std::variant<std::vector<double>, std::vector<uint64_t>>> vars;
+    // main instance
+    std::shared_ptr<adios2::ADIOS> inst;
+    std::shared_ptr<adios2::Engine> engine;  
+    std::shared_ptr<adios2::IO> io;
+};
+#endif // ENABLE_ADIOS2
