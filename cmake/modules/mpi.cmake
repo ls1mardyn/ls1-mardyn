6a566123e887d7b360d76df1e3109b32765ae402
rauts <tobias.rau@visus.uni-stuttgart.de>
2021-06-28 13:27:41 +0200

    enabled mpi by default in cmake

--- cmake/modules/mpi.cmake
+++ cmake/modules/mpi.cmake
@@ -1,5 +1,5 @@
 # mpi
-option(ENABLE_MPI "Enable MPI" OFF)
+option(ENABLE_MPI "Enable MPI" ON)
 if(ENABLE_MPI)
     message(STATUS "MPI Enabled")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DENABLE_MPI")
