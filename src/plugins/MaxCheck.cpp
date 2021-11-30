3f896cd1eb5094ed95e1fc63159fa02f8fee6edc
HomesGH <homes@tu-berlin.de>
2021-11-30 13:34:13 +0100

    Add and adjust output for MaxCheck plugin

--- src/plugins/MaxCheck.cpp
+++ src/plugins/MaxCheck.cpp
@@ -36,7 +36,7 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 	xmlconfig.getNodeValue("control/start", _control.start);
 	xmlconfig.getNodeValue("control/frequency", _control.freq);
 	xmlconfig.getNodeValue("control/stop", _control.stop);
-	global_log->info() << "MaxCheck is acting start:freq:stop = "
+	global_log->info() << "[MaxCheck] Active period: start:freq:stop = "
 			<< _control.start << ":" << _control.freq << ":" << _control.stop
 			<< endl;
 
@@ -57,15 +57,26 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 	xmlconfig.getNodeValue("range/zmin", _range.zmin);
 	xmlconfig.getNodeValue("range/zmax", _range.zmax);
 
+	// Warn if old config style is used as it has big influence on the simulation
+	double range;
+	if (xmlconfig.getNodeValue("yrange/min", range)) {
+		global_log->warning() << "[MaxCheck] <yrange> is deprecated! Use <range> and <ymin>/<ymax> instead." << endl;
+	}
+
+	global_log->info() << "[MaxCheck] Apply " << ((_range.inclusive) ? "inside" : "outside") << " range:"
+					   << " x = " << _range.xmin << " - " << _range.xmax << " ;"
+					   << " y = " << _range.ymin << " - " << _range.ymax << " ;"
+					   << " z = " << _range.zmin << " - " << _range.zmax << endl;
+
 	// targets
 	uint32_t numTargets = 0;
 	XMLfile::Query query = xmlconfig.query("targets/target");
 	numTargets = query.card();
-	global_log->info() << "MaxCheck: Number of component targets: "
+	global_log->info() << "[MaxCheck] Number of component targets: "
 			<< numTargets << endl;
 	if (numTargets < 1) {
 		global_log->warning()
-				<< "MaxCheck: No target parameters specified. Program exit ..."
+				<< "[MaxCheck] No target parameters specified. Program exit ..."
 				<< endl;
 		Simulation::exit(-1);
 	}
@@ -85,26 +96,26 @@ void MaxCheck::readXML(XMLfileUnits& xmlconfig) {
 		mv.method = MCM_UNKNOWN;
 
 		xmlconfig.getNodeValue("@method", mv.method);
-		global_log->info() << "MaxCheck: Method(cid=" << cid_ub << "): "
+		global_log->info() << "[MaxCheck] Method(cid=" << cid_ub << "): "
 				<< mv.method << endl;
 
 		xmlconfig.getNodeValue("Fmax", mv.F);
-		global_log->info() << "MaxCheck: Fmax(cid=" << cid_ub << "): " << mv.F
+		global_log->info() << "[MaxCheck] Fmax(cid=" << cid_ub << "): " << mv.F
 				<< endl;
 		mv.F2 = mv.F * mv.F;
 
 		xmlconfig.getNodeValue("vmax", mv.v);
-		global_log->info() << "MaxCheck: vmax(cid=" << cid_ub << "): " << mv.v
+		global_log->info() << "[MaxCheck] vmax(cid=" << cid_ub << "): " << mv.v
 				<< endl;
 		mv.v2 = mv.v * mv.v;
 		
 		xmlconfig.getNodeValue("Mmax", mv.M);
-		global_log->info() << "MaxCheck: Mmax(cid=" << cid_ub << "): " << mv.M
+		global_log->info() << "[MaxCheck] Mmax(cid=" << cid_ub << "): " << mv.M
 				<< endl;
 		mv.M2 = mv.M * mv.M;
 		
 		xmlconfig.getNodeValue("Lmax", mv.L);
-		global_log->info() << "MaxCheck: Lmax(cid=" << cid_ub << "): " << mv.L
+		global_log->info() << "[MaxCheck] Lmax(cid=" << cid_ub << "): " << mv.L
 				<< endl;
 		mv.L2 = mv.L * mv.L;
 
