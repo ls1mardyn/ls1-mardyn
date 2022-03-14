350fb4aadf43251b073d5c96a5990ded5fbd3e03
HomesGH <homes@tu-berlin.de>
2022-03-14 12:04:50 +0100

    Add function to convert gibbs free energy values

--- tools/ppls1/ppls1/fluids/ljts/ljts.py
+++ tools/ppls1/ppls1/fluids/ljts/ljts.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May 31 12:32:59 2020
 
-@author: mheinen/homes
-"""
+import numpy as np
+import pandas as pd
+
 
 #%% Get saturated densities by Vrabec et al., Molecular Physics 104 (2006).
 def rho_vrabec2006(T):
@@ -21,6 +20,54 @@ def rho_vrabec2006(T):
     a,b,c=0.5649,0.2128,0.0702
     rhov=rc-a*dt**(1/3.)+b*dt+c*dt**(3/2.)
     return rhol,rhov
+
+#%% Convert Gibbs free energy from PeTS to ms2 value
+def g_PeTS2ms2(g_pets, T, rho):
+    '''
+    Convert Gibbs free energy from PeTS to ms2 value
+    
+    :param float g_pets: Gibbs free energy from PeTS
+    :param float T: Temperature
+    :param float rho: Density
+    :return: float g_ms2: Gibbs free energy from ms2
+    '''
+    
+    rhocrit = 0.319
+    Tcrit   = 1.086
+    delta0 = (0.001/0.8)/rhocrit
+    tau0   = Tcrit/0.8
+    ig1 = -2.5/tau0
+    ig2 = 1.5 - np.log(delta0) - 1.5*np.log(tau0)
+    tau   = Tcrit/T
+    delta = rho/rhocrit
+    alphaId = np.log(delta) + 1.5*np.log(tau) + ig1*tau + ig2
+    g_res = g_pets/T - 1.0 - alphaId
+    g_ms2 = g_res + np.log(rho)
+    return g_ms2
+
+#%% Convert Gibbs free energy from ms2 to PeTS value
+def g_ms22PeTS(g_ms2, T, rho):
+    '''
+    Convert Gibbs free energy from ms2 to PeTS value
+    
+    :param float g_ms2: Gibbs free energy from ms2
+    :param float T: Temperature
+    :param float rho: Density
+    :return: float g_pets: Gibbs free energy from PeTS
+    '''
+    
+    rhocrit = 0.319
+    Tcrit   = 1.086
+    delta0 = (0.001/0.8)/rhocrit
+    tau0   = Tcrit/0.8
+    ig1 = -2.5/tau0
+    ig2 = 1.5 - np.log(delta0) - 1.5*np.log(tau0)
+    tau   = Tcrit/T
+    delta = rho/rhocrit
+    alphaId = np.log(delta) + 1.5*np.log(tau) + ig1*tau + ig2
+    g_res = g_ms2 - np.log(rho)
+    g_pets = T*(1.0 + alphaId + g_res)
+    return g_pets
 
 if __name__ == "__main__":
     print('Running test with T = 0.8 ...')
