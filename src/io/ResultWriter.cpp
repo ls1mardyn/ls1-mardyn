a27030037831c987716b10a37f0bd532c423eaa9
HomesGH <homes@tu-berlin.de>
2023-05-05 11:37:47 +0200

    Major update of ResultWriter to improve parsing of output, doc, error-fixes, cleanup, etc.

--- src/io/ResultWriter.cpp
+++ src/io/ResultWriter.cpp
@@ -1,29 +1,21 @@
 #include "io/ResultWriter.h"
 
 #include "Domain.h"
-#include "parallel/DomainDecompBase.h"
 #include "Simulation.h"
+#include "parallel/DomainDecompBase.h"
 #include "utils/Logger.h"
-#include <chrono>
+
+#include <fstream>
 
 using Log::global_log;
 
 void ResultWriter::readXML(XMLfileUnits& xmlconfig) {
-	_writeFrequency = 1;
 	xmlconfig.getNodeValue("writefrequency", _writeFrequency);
 	global_log->info() << "[ResultWriter] Write frequency: " << _writeFrequency << endl;
 
-	_outputPrefix = "mardyn";
 	xmlconfig.getNodeValue("outputprefix", _outputPrefix);
 	global_log->info() << "[ResultWriter] Output prefix: " << _outputPrefix << endl;
 
-	size_t acc_steps = 1000;
-	xmlconfig.getNodeValue("accumulation_steps", acc_steps);
-	_U_pot_acc = new Accumulator<double>(acc_steps);
-	_p_acc = new Accumulator<double>(acc_steps);
-	global_log->info() << "[ResultWriter] Accumulation steps: " << acc_steps << endl;
-
-	_writePrecision = 5;
 	xmlconfig.getNodeValue("writeprecision", _writePrecision);
 	global_log->info() << "[ResultWriter] Write precision: " << _writePrecision << endl;
 }
@@ -31,59 +23,89 @@ void ResultWriter::readXML(XMLfileUnits& xmlconfig) {
 void ResultWriter::init(ParticleContainer * /*particleContainer*/,
                         DomainDecompBase *domainDecomp, Domain * /*domain*/) {
 
+	// Only main rank writes data to file
 	if(domainDecomp->getRank() == 0) {
-		const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		tm unused{};
-		const auto nowStr = std::put_time(localtime_r(&now, &unused), "%c");
-		string resultfile(_outputPrefix+".res");
-		_resultStream.open(resultfile.c_str(), std::ios::out);
-		_resultStream << "# ls1 MarDyn simulation started at " << nowStr << endl;
-		_resultStream << "# Averages are the accumulated values over " << _U_pot_acc->getWindowLength()  << " time steps."<< endl;
-		_resultStream << std::setw(10) << "# step" << std::setw(_writePrecision+15) << "time" 
-			<< std::setw(_writePrecision+15) << "U_pot"
+		const string resultfile(_outputPrefix+".res");
+		std::ofstream resultStream;
+		resultStream.open(resultfile.c_str(), std::ios::out);
+		resultStream << std::setw(10) << "#step"
+			<< std::setw(_writePrecision+15) << "time"
 			<< std::setw(_writePrecision+15) << "U_pot_avg"
-			<< std::setw(_writePrecision+15) << "p"
+			<< std::setw(_writePrecision+15) << "U_kin_avg"
+			<< std::setw(_writePrecision+15) << "U_kinTrans_avg"
+			<< std::setw(_writePrecision+15) << "U_kinRot_avg"
 			<< std::setw(_writePrecision+15) << "p_avg"
-			<< std::setw(_writePrecision+15) << "beta_trans"
-			<< std::setw(_writePrecision+15) << "beta_rot"
 			<< std::setw(_writePrecision+15) << "c_v"
 			<< std::setw(_writePrecision+15) << "N"
 			<< endl;
+		resultStream.close();
 	}
 }
 
 void ResultWriter::endStep(ParticleContainer *particleContainer, DomainDecompBase *domainDecomp, Domain *domain,
                            unsigned long simstep) {
 
-	// Writing of cavities now handled by CavityWriter
+	const uint64_t globalNumMolecules = domain->getglobalNumMolecules(true, particleContainer, domainDecomp);
 
-	unsigned long globalNumMolecules = domain->getglobalNumMolecules(true, particleContainer, domainDecomp);
-	double cv = domain->cv();
+	_uPot_acc += domain->getGlobalUpot();
+	_p_acc += domain->getGlobalPressure();
 
-	_U_pot_acc->addEntry(domain->getGlobalUpot());
-	_p_acc->addEntry(domain->getGlobalPressure());
-	if((domainDecomp->getRank() == 0) && (simstep % _writeFrequency == 0)){
-		_resultStream << std::setw(10) << simstep << std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _simulation.getSimulationTime()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << domain->getGlobalUpot()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _U_pot_acc->getAverage()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << domain->getGlobalPressure()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _p_acc->getAverage()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << domain->getGlobalBetaTrans()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << domain->getGlobalBetaRot()
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << cv
-			<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << globalNumMolecules
-			<< endl;
+	double uKinLocal = 0.0F;
+	double uKinGlobal = 0.0F;
+	double uKinTransLocal = 0.0F;
+	double uKinTransGlobal = 0.0F;
+	double uKinRotLocal = 0.0F;
+	double uKinRotGlobal = 0.0F;
+	#if defined(_OPENMP)
+	#pragma omp parallel reduction(+:uKinLocal,uKinTransLocal,uKinRotLocal)
+	#endif
+	{
+		for (auto moleculeIter = particleContainer->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY);
+			 moleculeIter.isValid(); ++moleculeIter) {
+			uKinLocal += moleculeIter->U_kin();
+			uKinTransLocal += moleculeIter->U_trans();
+			uKinRotLocal += moleculeIter->U_rot();
+		}
 	}
-}
+#ifdef ENABLE_MPI
+    MPI_Allreduce(&uKinLocal, &uKinGlobal, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Allreduce(&uKinTransLocal, &uKinTransGlobal, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Allreduce(&uKinRotLocal, &uKinRotGlobal, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+#else
+    uKinGlobal = uKinLocal;
+	uKinTransGlobal = uKinTransLocal;
+	uKinRotGlobal = uKinRotLocal;
+#endif
+	_uKin_acc += uKinGlobal;
+	_uKinTrans_acc += uKinTransGlobal;
+	_uKinRot_acc += uKinRotGlobal;
 
-void ResultWriter::finish(ParticleContainer * /*particleContainer*/,
-						  DomainDecompBase *domainDecomp, Domain * /*domain*/){
+	_numSamples++;
 
-	if(domainDecomp->getRank() == 0) {
-		const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		tm unused{};
-		const auto nowStr = std::put_time(localtime_r(&now, &unused), "%c");
-		_resultStream << "# ls1 mardyn simulation finished at " << nowStr << endl;
-		_resultStream.close();
+	if ((simstep % _writeFrequency == 0) and (simstep > 0UL)) {
+		// Only main rank writes data to file
+		if (domainDecomp->getRank() == 0) {
+			const string resultfile(_outputPrefix+".res");
+			std::ofstream resultStream;
+			resultStream.open(resultfile.c_str(), std::ios::app);
+			resultStream << std::setw(10) << simstep << std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _simulation.getSimulationTime()
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _uPot_acc/_numSamples
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _uKin_acc/_numSamples
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _uKinTrans_acc/_numSamples
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _uKinRot_acc/_numSamples
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << _p_acc/_numSamples
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << domain->cv()
+				<< std::setw(_writePrecision+15) << std::scientific << std::setprecision(_writePrecision) << globalNumMolecules
+				<< endl;
+			resultStream.close();
+		}
+
+		// Reset values
+		_numSamples = 0UL;
+		_uPot_acc = 0.0F;
+		_uKin_acc = 0.0F;
+		_uKinTrans_acc = 0.0F;
+		_uKinRot_acc = 0.0F;
+		_p_acc = 0.0F;
 	}
 }
--- src/io/ResultWriter.h
+++ src/io/ResultWriter.h
@@ -1,11 +1,10 @@
 #ifndef SRC_IO_RESULTWRITER_H_
 #define SRC_IO_RESULTWRITER_H_
 
-#include <fstream>
-#include <string>
-
 #include "plugins/PluginBase.h"
 #include "utils/Accumulator.h"
+
+#include <string>
 
 
 /** @brief Writes thermodynamic properties to a file.
@@ -15,27 +14,27 @@
  * The following values are written to the file:
  * - Simulation time step
  * - time since the simulation started (dimensionless)
- * - Average potential Energy
- * - Pressure
- * - BetaTrans
- * - BetaRot
+ * - Average potential energy
+ * - Average kinetic energy
+ * - Average kinetic translational energy
+ * - Average kinetic rotational energy
+ * - Average Pressure
+ * - Isochoric heat capacity
+ * - Global number of particles
  */
 class ResultWriter : public PluginBase {
 public:
-	ResultWriter() : _U_pot_acc(nullptr), _p_acc(nullptr) {}
-	~ResultWriter() {
-		delete _U_pot_acc;
-		delete _p_acc;
-	}
+	ResultWriter() = default;
+	~ResultWriter() override = default;
+
 
 	/** @brief Read in XML configuration for ResultWriter and all its included objects.
 	 *
 	 * The following xml object structure is handled by this method:
 	 * \code{.xml}
 		<outputplugin name="ResultWriter">
-			<writefrequency>INTEGER</writefrequency>				<!-- Frequency in which the output is written; Default: 1 -->
-			<outputprefix>STRING</outputprefix>						<!-- Prefix of the output file; Default: "results" -->
-			<accumulation_steps>INTEGER</accumulation_steps>		<!-- Result is accumulated over the specified steps; Default: 1000 -->
+			<writefrequency>UINTEGER</writefrequency>				<!-- Frequency in which the output is written; Default: 1000 -->
+			<outputprefix>STRING</outputprefix>						<!-- Prefix of the output file; Default: "mardyn" -->
 			<writeprecision>UINTEGER</writeprecision>				<!-- Precision of output can be set here; Default: 5 -->
 		</outputplugin>
 	   \endcode
@@ -51,8 +50,8 @@ public:
             unsigned long simstep
     );
 
-	void finish(ParticleContainer *particleContainer,
-				DomainDecompBase *domainDecomp, Domain *domain);
+	void finish(ParticleContainer* /* particleContainer */,
+				DomainDecompBase* /* domainDecomp */, Domain* /* domain */) override {}
 	
 	std::string getPluginName() {
 		return std::string("ResultWriter");
@@ -60,12 +59,15 @@ public:
 	static PluginBase* createInstance() { return new ResultWriter(); }
 
 private:
-	std::ofstream _resultStream;
-	long _writeFrequency;
-	int _writePrecision;
-	std::string _outputPrefix;
-	Accumulator<double> *_U_pot_acc;
-	Accumulator<double> *_p_acc;
+	uint64_t _writeFrequency{1000UL};
+	unsigned int _writePrecision{5};
+	std::string _outputPrefix{"mardyn"};
+	uint64_t _numSamples{0UL};
+	double _uPot_acc{0.0F};
+	double _uKin_acc{0.0F};
+	double _uKinTrans_acc{0.0F};
+	double _uKinRot_acc{0.0F};
+	double _p_acc{0.0F};
 };
 
 #endif  // SRC_IO_RESULTWRITER_H_
