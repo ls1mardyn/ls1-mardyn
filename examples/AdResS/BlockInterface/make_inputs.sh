b9780ea403523fa2ec9756501f6d0732c8cb7da2
AlexHocks <alex.hocks@tum.de>
2023-06-22 20:10:31 +0200

    auto input generation

---
+++ examples/AdResS/BlockInterface/make_inputs.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/zsh
+
+CLEAN=1
+TYPES=('C6H12' 'CH4')
+
+if [[ $CLEAN -eq 1 ]]
+then
+  for n in 1 4 16 64 256 1024
+  do
+    for type in "${TYPES[@]}"
+    do
+      rm -f ./${n}k/config_"${type}"_0.xml
+      rm -f ./${n}k/config_"${type}"_1.xml
+      rm -f CI_INIT_${n}k_"${type}"_0.cmd
+      rm -f CI_INIT_${n}k_"${type}"_1.cmd
+    done
+  done
+  exit 0
+fi
+
+for n in 1 4 16 64 256 1024
+do
+  for type in "${TYPES[@]}"
+  do
+    rm -f ./${n}k/config_"${type}"_0.xml
+    rm -f ./${n}k/config_"${type}"_1.xml
+    cp ./${n}k/config_"${type}".xml ./${n}k/config_"${type}"_0.xml
+    cp ./${n}k/config_"${type}".xml ./${n}k/config_"${type}"_1.xml
+    SIZE=$(sed '27q;d' ./${n}k/config_"${type}".xml | sed -r 's#.*<.*>([0-9]*).*</lx>#\1#g' -)
+    sed -i -r "s#<lx unit=\"reduced\">.*</lx>#<lx unit=\"reduced\">$((SIZE/2))\.</lx>#g" ./${n}k/config_"${type}"_0.xml
+    sed -i -r "s#<lx unit=\"reduced\">.*</lx>#<lx unit=\"reduced\">$((SIZE/2))\.</lx>#g" ./${n}k/config_"${type}"_1.xml
+    sed -i -n '60,81!p' ./${n}k/config_"${type}"_0.xml
+    sed -i -n '38,59!p' ./${n}k/config_"${type}"_1.xml
+    sed -i -n '79,96!p' ./${n}k/config_"${type}"_0.xml
+    sed -i -n '79,96!p' ./${n}k/config_"${type}"_1.xml
+    sed -i -r "s#<lower> <x>.*</x>#<lower> <x>0</x>#g" ./${n}k/config_"${type}"_1.xml
+    sed -i -r "s#<upper> <x>.*</x>#<upper> <x>$((SIZE/2))</x>#g" ./${n}k/config_"${type}"_1.xml
+
+    rm -f CI_INIT_${n}k_"${type}"_0.cmd
+    rm -f CI_INIT_${n}k_"${type}"_1.cmd
+
+    touch CI_INIT_${n}k_"${type}"_0.cmd
+    touch CI_INIT_${n}k_"${type}"_1.cmd
+    echo "#!/bin/bash
+#SBATCH -J CI_INIT_${n}k_${type}_0
+#SBATCH -o ../../../../log/%x.out
+#SBATCH -D ../../../../ls1-mardyn
+#SBATCH --partition=medium
+#SBATCH --get-user-env
+#SBATCH --mail-type=all
+#SBATCH --mem=800mb
+#SBATCH --mail-user=hocksa@hsu-hh.de
+#SBATCH --export=NONE
+#SBATCH --time=00:10:00
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=72
+#SBATCH --error=../../../../log/%x.err
+
+export OMP_NUM_THREADS=72
+ulimit -s 1000000
+
+module load slurm_setup
+module load mpi/2021.6.0
+
+mpiexec -n 16 ./build/src/MarDyn ./examples/AdResS/BlockInterface/${n}k/config_${type}_0.xml --loop-abort-time=570
+" > CI_INIT_${n}k_"${type}"_0.cmd
+  echo "#!/bin/bash
+#SBATCH -J CI_INIT_${n}k_${type}_1
+#SBATCH -o ../../../../log/%x.out
+#SBATCH -D ../../../../ls1-mardyn
+#SBATCH --partition=medium
+#SBATCH --get-user-env
+#SBATCH --mail-type=all
+#SBATCH --mem=800mb
+#SBATCH --mail-user=hocksa@hsu-hh.de
+#SBATCH --export=NONE
+#SBATCH --time=00:10:00
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=72
+#SBATCH --error=../../../../log/%x.err
+
+export OMP_NUM_THREADS=72
+ulimit -s 1000000
+
+module load slurm_setup
+module load mpi/2021.6.0
+
+mpiexec -n 16 ./build/src/MarDyn ./examples/AdResS/BlockInterface/${n}k/config_${type}_1.xml --loop-abort-time=570
+" > CI_INIT_${n}k_"${type}"_1.cmd
+  done
+done
+
---
+++ examples/AdResS/BlockInterface/sbatch_q.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/zsh
+
+E_1k=0
+E_4k=0
+E_16k=0
+E_64k=0
+E_256k=0
+E_1024k=0
+
+if [[ $E_1k -gt 0 ]]
+then
+  sbatch ./CI_INIT_1k_C6H12_0.cmd
+  sbatch ./CI_INIT_1k_C6H12_1.cmd
+  sbatch ./CI_INIT_1k_CH4_0.cmd
+  sbatch ./CI_INIT_1k_CH4_1.cmd
+fi
+
+if [[ $E_4k -gt 0 ]]
+then
+  sbatch ./CI_INIT_4k_C6H12_0.cmd
+  sbatch ./CI_INIT_4k_C6H12_1.cmd
+  sbatch ./CI_INIT_4k_CH4_0.cmd
+  sbatch ./CI_INIT_4k_CH4_1.cmd
+fi
+
+if [[ $E_16k -gt 0 ]]
+then
+  sbatch ./CI_INIT_16k_C6H12_0.cmd
+  sbatch ./CI_INIT_16k_C6H12_1.cmd
+  sbatch ./CI_INIT_16k_CH4_0.cmd
+  sbatch ./CI_INIT_16k_CH4_1.cmd
+fi
+
+if [[ $E_64k -gt 0 ]]
+then
+  sbatch ./CI_INIT_64k_C6H12_0.cmd
+  sbatch ./CI_INIT_64k_C6H12_1.cmd
+  sbatch ./CI_INIT_64k_CH4_0.cmd
+  sbatch ./CI_INIT_64k_CH4_1.cmd
+fi
+
+if [[ $E_256k -gt 0 ]]
+then
+  sbatch ./CI_INIT_256k_C6H12_0.cmd
+  sbatch ./CI_INIT_256k_C6H12_1.cmd
+  sbatch ./CI_INIT_256k_CH4_0.cmd
+  sbatch ./CI_INIT_256k_CH4_1.cmd
+fi
+
+if [[ $E_1024k -gt 0 ]]
+then
+  sbatch ./CI_INIT_1024k_C6H12_0.cmd
+  sbatch ./CI_INIT_1024k_C6H12_1.cmd
+  sbatch ./CI_INIT_1024k_CH4_0.cmd
+  sbatch ./CI_INIT_1024k_CH4_1.cmd
+fi
