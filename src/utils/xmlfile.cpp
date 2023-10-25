abd8b61751d4069ea797c3bddfcf373130089b2a
HomesGH <homes@tu-berlin.de>
2023-10-25 17:27:43 +0200

    Parse boolean and check input type

--- src/utils/xmlfile.cpp
+++ src/utils/xmlfile.cpp
@@ -589,21 +589,19 @@ template<> bool XMLfile::Node::getValue<bool>(bool& value) const
 {
 	std::string v;
 	bool found=getValue(v);
-	if(found)
-	{
-		int i=atoi(v.c_str());
-		if(i!=0)
-		{
-			/* found integer value unequal 0 */
-			value=true;
-		}
-		else
-		{
-			/* remove white spaces */
-			v.erase( std::remove_if( v.begin(), v.end(), ::isspace ), v.end() );
-			/* convert to upper case letters */
-			std::transform(v.begin(), v.end(), v.begin(), ::toupper);
-			value=(v == "TRUE" || v == "YES" || v == "ON");
+	if (found) {
+		// Remove white spaces
+		v.erase(std::remove_if(v.begin(), v.end(), ::isspace), v.end());
+		// Convert to upper case letters
+		std::transform(v.begin(), v.end(), v.begin(), ::toupper);
+
+		if (v == "TRUE" || v == "YES" || v == "ON" || v == "1") {
+			value = true;
+		} else if (v == "FALSE" || v == "NO" || v == "OFF" || v == "0") {
+			value = false;
+		} else {
+			std::cerr << "ERROR parsing \"" << v << "\" to boolean from tag \"<" << name() << ">\" in xml file" << std::endl;
+			Simulation::exit(1);
 		}
 	}
 	return found;
