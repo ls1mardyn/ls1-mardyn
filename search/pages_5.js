2cfb92d64d9ee4953e2571ac5c1cb4fefb8d3e40
rubenhorn <rubenhorn@users.noreply.github.com>
2023-11-10 12:21:41 +0000

    deploy: c28808b3d6d6889995b14c3afa2977a33729fe87

--- search/pages_5.js
+++ search/pages_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['plugin_20summary_0',['Plugin Summary',['../PluginsSummary.html',1,'']]]
+  ['plugin_20summary_0',['Plugin Summary',['../PluginsSummary.html',1,'']]],
+  ['profiling_1',['Profiling',['../Profiling.html',1,'']]]
 ];
