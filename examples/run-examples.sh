788158f31984d2f52a65e53909c44fcaa0dc0916
Christoph Niethammer <niethammer@hlrs.de>
2024-02-08 12:27:12 +0100

    Fix script to run examples locally
    Ignore comment lines and empty lines in the example lists file.
    
    Signed-off-by: Christoph Niethammer <niethammer@hlrs.de>

--- examples/run-examples.sh
+++ examples/run-examples.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # Testscript running all examples from example-list.txt
 #
-# Copyright (c) 2017      Christoph Niethammer <niethammer@hlrs.de>
+# Copyright (c) 2017-2024 Christoph Niethammer <niethammer@hlrs.de>
 #
 
-MARDYN_EXE="mpirun -np 1 $PWD/../src/MarDyn"
+MARDYN_EXE="mpirun -np 2 $PWD/../src/MarDyn"
 MARDYN_OPTIONS="--steps 10 -v"
 EXAMPLE_LIST_FILE=${EXAMPLE_LIST_FILE:=example-list.txt}
 LOGFILE=${LOGFILE:=$PWD/run-examples.log}
 
-#mapfile -t all_examples < $EXAMPLE_LIST_FILE # requires bash4 ...
-declare -a all_examples
-while IFS= read -r; do all_examples+=("$REPLY"); done < $EXAMPLE_LIST_FILE
-
+all_examples=()
 failed_examples=()
 
-# definitions for esear color output to display
+while IFS= read -r line ; do
+  [[ "$line" =~ ^#.*$ ]] && continue
+  [[ "$line" =~ ^$ ]] && continue
+
+  example="$line"
+  echo "Going to run example file $example"
+  all_examples+=("$example")
+done < "$EXAMPLE_LIST_FILE"
+
+
+# definitions for easy color output
 Color_Off='\e[0m'       # Text Reset
 IGreen='\e[0;92m'       # Intense Green
 IRed='\e[0;91m'         # Intense Red
@@ -23,11 +30,10 @@ IMagenta='\e[0;95m'     # magenta
 
 logfile=$LOGFILE
 date > $logfile
-for example in ${all_examples[@]}
-do
+for example in ${all_examples[@]} ; do
   example_dir=$(dirname $example)
   example_inp_file=$(basename $example)
-  echo -e -n "Running example ${IMagenta}$example_inp_file${Color_Off} in $example_dir ... "
+  echo -e -n "Running example ${IMagenta}$example_inp_file${Color_Off} in ${IMagenta}$example_dir${Color_Off} ... "
   echo "Running example $example_inp_file in $example_dir" >> $logfile
   pushd "$example_dir" >/dev/null
   cmd="$MARDYN_EXE $MARDYN_OPTIONS $example_inp_file"
