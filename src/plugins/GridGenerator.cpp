c60d256c524038553495a9e702f9c727700a2f57
Jose P. Escobar <jose.escobar@hsu-hh.de>
2024-02-20 15:34:34 +0100

    added sampler class

--- src/plugins/GridGenerator.cpp
+++ src/plugins/GridGenerator.cpp
@@ -8,7 +8,7 @@
 
 #include "GridGenerator.h"
 
-GridGenerator::GridGenerator():total_elements{1}{
+GridGenerator::GridGenerator(){
 
 }
 
@@ -25,19 +25,59 @@ void GridGenerator::readXML(XMLfileUnits& xmlconfig){
 void GridGenerator::init(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, Domain* domain){
     
     MeshEntireDomain();
+    Log::global_log->info()<<"["<<this->getPluginName()<<"]\n";
+    Log::global_log->info()<<"Meshed region is: ("<<lower_corner[0]<<","<<lower_corner[1]<<","<<lower_corner[2]<<")x("<<upper_corner[0]<<","<<upper_corner[1]<<","<<upper_corner[2]<<")\n";
     SetTotalElements();
     SetElementInfo();
     element_information.index=this->total_elements-1;
+    this->element_information.MeshTraversal();
     
+    this->Output("not-in-use",1);
 }
 
 void GridGenerator::beforeEventNewTimestep(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, unsigned long simstep){
     Log::global_log->info()<<this->getPluginName()<<" information\n";
     Log::global_log->info()<<"Total elements: "<<this->total_elements<<"\n"
-                           <<"Elemens per direction: ["<<this->elements_per_dimension[0]<<","<<this->elements_per_dimension[1]<<","
+                           <<"Length of domain: "<<this->element_information.length_x<<","
+                           <<this->element_information.length_y<<","
+                           <<this->element_information.length_z<<"\n"
+                           <<"Elements per direction: ["<<this->elements_per_dimension[0]<<","<<this->elements_per_dimension[1]<<","
                            <<this->elements_per_dimension[2]<<"]\n"
-                           <<"Volume per element: "<<element_information.volume<<std::endl;
+                           <<"Volume per element: "<<element_information.volume
+                           <<"Largest Index: "<<this->element_information.index<<std::endl;
+    
                           
+}
+
+void GridGenerator::Output(std::string prefix, long unsigned accumulated){
+    Log::global_log->info()<<"[GridGenerator] output"<<std::endl;
+
+    std::ofstream outfile("mesh-information.txt");
+    outfile.precision(6);
+
+    std::ofstream centerfile("centers.txt");
+    centerfile.precision(6);
+
+    outfile <<"//GridGenerator plugin mesh information \n"
+            <<"//Total number of elements: "<<this->total_elements<<"\n";
+    
+    //Table header
+    outfile <<"idx \t lx \t ly \t lz \t ux \t uy \t uz \n";
+    centerfile <<"idx \t cx \t cy \t cz \n";
+    std::vector<double> lc(3),uc(3),ec(3);
+    for(int i=0; i<= element_information.index;i++){
+        lc = this->element_information.GetElementLowerCorner(i);
+        uc = this->element_information.GetElementUpperCorner(i);
+        ec = this->element_information.GetElementCenter(i);
+        outfile << i <<"\t"<<lc[0]<<"\t"<<lc[1]<<"\t"<<lc[2]<<"\t"
+                <<uc[0]<<"\t"<<uc[1]<<"\t"<<uc[2]<<"\n";
+        centerfile << i <<"\t"<<ec[0]<<"\t"<<ec[1]<<"\t"<<ec[2]<<"\n";
+    }
+
+    outfile.close();
+
+    centerfile.close();
+
 }
 
 void GridGenerator::MeshEntireDomain(){
@@ -52,13 +92,71 @@ void GridGenerator::SetElementInfo(){
     for(int d=0;d<3;d++){
         element_width_per_dimension[d]= (upper_corner[d]-lower_corner[d])/elements_per_dimension[d];
     }
+    this->element_information.x_width=element_width_per_dimension[0];
+    this->element_information.y_width=element_width_per_dimension[1];
+    this->element_information.z_width=element_width_per_dimension[2];
 
     this->element_information.volume=element_width_per_dimension[0]*element_width_per_dimension[1]*element_width_per_dimension[2];
+
+    this->element_information.length_x=(upper_corner[0]-lower_corner[0]);
+    this->element_information.length_y=(upper_corner[1]-lower_corner[1]);
+    this->element_information.length_z=(upper_corner[2]-lower_corner[2]);
+
+    this->element_information.elements_in_x=elements_per_dimension[0];
+    this->element_information.elements_in_y=elements_per_dimension[1];
+    this->element_information.elements_in_z=elements_per_dimension[2];
 
 }
 
 void GridGenerator::SetTotalElements(){
+    this->total_elements=1;
     for(int d=0;d<3;d++){
         this->total_elements *= elements_per_dimension[d];
     }
+}
+
+PropertySampler::PropertySampler(ElementInfo& info):info(info){
+    this->init();
+}
+
+void PropertySampler::init(){
+
+    particles_per_cell.resize(this->info.index);
+    std::fill(particles_per_cell.begin(), particles_per_cell.end(),0);
+
+}
+
+void PropertySampler::ParticlePerCellCount(ElementInfo& info,  ParticleContainer* particle_container){
+    //Iterate all non-halo cells
+    ParticleIterator it = particle_container->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY);
+
+    for(it;it.isValid();++it){
+
+        std::tuple<int, int, int> indeces= GetParticleLocalCellIndices(it);
+        int particle_global_index = this->info.LocalToGlobalIndex(indeces);
+        particles_per_cell[particle_global_index] += 1;
+    }
+} 
+
+std::tuple<int, int, int> PropertySampler::GetParticleLocalCellIndices(ParticleIterator it)
+{
+    
+    std::array<double, 3> position = it->r_arr();
+    std::tuple<int, int, int> local_inds;
+
+    int x, y, z;
+
+    x = std::floor(position[0]/info.x_width);
+    y = std::floor(position[1]/info.y_width);
+    z = std::floor(position[0]/info.z_width);
+
+    local_inds= std::make_tuple(x,y,z);
+
+    return local_inds;
+
+
+}
+
+std::vector<int>& PropertySampler::GetParticlesPerCell(){
+    return this->particles_per_cell;
 }
--- src/plugins/GridGenerator.h
+++ src/plugins/GridGenerator.h
@@ -15,12 +15,127 @@
 #include "plugins/PluginBase.h"
 
 struct ElementInfo{
+
+    double length_x, length_y, length_z;
+    int elements_in_x, elements_in_y, elements_in_z;
     int index;
-    //std::vector<double> 
     double x_width;
     double y_width;
     double z_width;
     double volume;
+
+    std::tuple<int, int, int> local_indeces;
+
+    std::tuple<int, int, int> GlobalToLocalIndex(int idx){
+
+        int x, y, z;
+
+        std::tuple<int, int, int> local;
+
+        x= std::fmod(idx,elements_in_x);
+        y= std::fmod((idx/elements_in_x),elements_in_y);
+        z=idx/(elements_in_x*elements_in_y);
+        local = std::make_tuple(x,y,z);
+
+        return local;
+    }
+
+    int LocalToGlobalIndex(std::tuple<int, int, int> index_vector){
+        return LocalToGlobalIndex(std::get<0>(index_vector),std::get<1>(index_vector),std::get<2>(index_vector));
+    }
+
+    int LocalToGlobalIndex(int i, int j, int k){
+
+        int global_index = i + j*elements_in_x + k*elements_in_x*elements_in_y;
+
+        return global_index;
+    }
+
+    std::vector<double> GetElementLowerCorner(int localx, int localy, int localz){
+
+        std::vector<double> lower_corner(3);
+        lower_corner[0]=localx*x_width;
+        lower_corner[1]=localy*y_width;
+        lower_corner[2]=localz*z_width;
+        
+        return lower_corner;
+    }
+
+    std::vector<double> GetElementUpperCorner(int localx, int localy, int localz){
+
+        std::vector<double> upper_corner(3);
+        upper_corner[0]=(localx+1)*x_width;
+        upper_corner[1]=(localy+1)*y_width;
+        upper_corner[2]=(localz+1)*z_width;
+        
+        return upper_corner;
+    }
+
+    std::vector<double> GetElementLowerCorner(int global_index){
+        std::tuple<int,int,int> local_index = GlobalToLocalIndex(global_index);
+        return GetElementLowerCorner(std::get<0>(local_index),std::get<1>(local_index),std::get<2>(local_index));
+    }
+
+    std::vector<double> GetElementUpperCorner(int global_index){
+        std::tuple<int,int,int> local_index = GlobalToLocalIndex(global_index);
+        return GetElementUpperCorner(std::get<0>(local_index),std::get<1>(local_index),std::get<2>(local_index));
+    }
+
+    std::vector<double> GetElementCenter(int global_index){
+        std::vector<double> center(3);
+        std::vector<double> lc = GetElementLowerCorner(global_index);
+        std::vector<double> uc = GetElementUpperCorner(global_index);
+
+        center[0]= 0.5*(lc[0]+uc[0]);
+        center[1]= 0.5*(lc[1]+uc[1]);
+        center[2]= 0.5*(lc[2]+uc[2]);
+
+        return center;
+    }
+
+
+    void MeshTraversal(){
+        for(int idx=0;idx<=index;idx++){
+            local_indeces = GlobalToLocalIndex(idx);
+            int indx_x, indx_y, indx_z;
+            indx_x=std::get<0>(local_indeces);
+            indx_y=std::get<1>(local_indeces);
+            indx_z=std::get<2>(local_indeces);
+
+            std::vector<double> ucorner, lcorner;
+
+            ucorner = GetElementUpperCorner(indx_x, indx_y, indx_z);
+            lcorner = GetElementLowerCorner(indx_x, indx_y, indx_z);
+
+            std::cout<<"\tGlobal element: "<<idx<<" has local indeces: \n";
+            std::cout<<"\t("<<indx_x<<","<<indx_y<<","<<indx_z<<")\n";
+            std::cout<<"\tThe bounding box of global element: "<<LocalToGlobalIndex(indx_x, indx_y, indx_z)<<" is: \n"
+            <<"\t("<<lcorner[0]<<","<<lcorner[1]<<","<<lcorner[2]<<")x("<<ucorner[0]<<","<<ucorner[1]<<","<<ucorner[2]<<")\n";
+        }
+    }
+
+
+
+
+};
+
+class PropertySampler{
+    
+    private: 
+        std::vector<int> particles_per_cell;
+        ElementInfo& info;
+    public:
+
+    PropertySampler(ElementInfo& info);
+
+    void ParticlePerCellCount(ElementInfo& info,  ParticleContainer* pc);
+    std::vector<int>& GetParticlesPerCell();
+    void init();
+
+    private:
+
+
+    std::tuple<int, int, int> GetParticleLocalCellIndices(ParticleIterator it);
 
 
 };
@@ -34,6 +149,8 @@ class GridGenerator: public PluginBase{
     std::vector<double> lower_corner{3};
     std::vector<double> upper_corner{3};
     int total_elements;
+    //PropertySampler sampler;
+    ParticleContainer* particle_container;
 
 
     public:
@@ -47,6 +164,8 @@ class GridGenerator: public PluginBase{
         void endStep(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, Domain* domain, unsigned long simstep) override{}
         void finish(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, Domain* domain) override{}
 
+        void Output(std::string prefix, long unsigned accumulated);
+
         std::string getPluginName()  {return "GridGenerator";}
     static PluginBase* createInstance() {return new GridGenerator(); }
     private:
@@ -56,3 +175,5 @@ class GridGenerator: public PluginBase{
     void SetTotalElements();
 
 };
+
+
