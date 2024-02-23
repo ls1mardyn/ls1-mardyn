79c478597e8d6e3e25ee96ebfa84c2d8bc34876b
amartyads <amartyads@gmail.com>
2024-02-23 13:10:48 +0100

    copied part of explanation from alloptions to readxml doc

--- src/parallel/StaticIrregDomainDecomposition.h
+++ src/parallel/StaticIrregDomainDecomposition.h
@@ -61,9 +61,14 @@ public:
    * \code{.xml}
      <parallelisation type="StaticIrregDomainDecomposition">
        <!-- structure handled by DomainDecompMPIBase -->
-       <subdomainWeightsCSV> STRING.csv</subdomainWeightsCSV>
+       <subdomainWeightsCSV> STRING.csv </subdomainWeightsCSV>
      </parallelisation>
      \endcode
+    *
+    * A file with its first line being 1,2,1 defines an x axis with subdomain
+   lengths in the 1:2:1 ratio
+    * If file not given, default behaviour is an equally spaced grid, same as
+   DomainDecomposition
     *
     * @param &xmlconfig The xml node from which to read the CSV filename with
    weights
