e40ba22b795c98ace4a97ce2acd546a97ac51681
JakNiem <j.niemann@tu-berlin.de>
2024-05-22 17:22:08 +0200

    Sampling of cylinder in central axis for coalescence measurements

---
+++ src/plugins/CoalescenceSampling.cpp
@@ -0,0 +1,196 @@
+/*
+ * CoalescenceSampling.cpp
+ *
+ *  Created on: May 2024
+ *      Author: homes
+ */
+
+#include "CoalescenceSampling.h"
+
+#include <math.h>
+#include <limits>
+#include <algorithm>
+#include <map>
+
+#include "particleContainer/ParticleContainer.h"
+#include "particleContainer/adapter/ParticlePairs2PotForceAdapter.h"
+#include "utils/Random.h"
+#include "utils/FileUtils.h"
+#include "Simulation.h"
+#include "longRange/LongRangeCorrection.h"
+
+
+
+CoalescenceSampling::CoalescenceSampling() {}
+
+void CoalescenceSampling::init(ParticleContainer* /* particleContainer */, DomainDecompBase* domainDecomp, Domain* domain) {
+    Log::global_log->info() << "["<< getPluginName()<<"] ----------------INIT CALLED: "  << std::endl;
+    _rhoOutputFilename = _outputPrefix +  "rho_r"+ std::to_string(_radius)+".csv";
+
+    _globalBoxLength[0] = domain->getGlobalLength(0);
+    _globalBoxLength[1] = domain->getGlobalLength(1);
+    _globalBoxLength[2] = domain->getGlobalLength(2);
+
+    _yLower = std::max(_yLower, 0.f);
+    _yUpper = std::min(_yUpper, (float) _globalBoxLength[1]);
+    _radius = std::min(_radius, 0.5f *  (float) std::min(_globalBoxLength[0],_globalBoxLength[2]));
+
+    _numBinsGlobal = static_cast<unsigned int>((_yUpper - _yLower)/_binwidth);
+
+    if ((_yUpper - _yLower)/_binwidth != static_cast<float>(_numBinsGlobal)) {
+        Log::global_log->info() << "["<< getPluginName()<<"] Can not divide domain without remainder in y-direction! " << std::endl;
+        
+        float yCenter = (_yUpper + _yLower)/2;
+        float adjustedWidth = ((float) _numBinsGlobal) * _binwidth;
+        _yLower = yCenter - adjustedWidth/2.f;
+        _yUpper = yCenter + adjustedWidth/2.f;
+                
+        Log::global_log->info() << "["<< getPluginName()<<"] Changing (_yLower, _yUpper) to ("<<_yLower<<", "<<_yUpper<<")." << std::endl;
+    }
+
+
+    // prepare rho Outputfile 
+    if (domainDecomp->getRank() == 0) {
+		std::ofstream ofs(_rhoOutputFilename, std::ios::out);
+		ofs << std::setw(24) << "simstep";
+        for (unsigned long i = 0; i < _numBinsGlobal; i++) {
+            ofs << std::setw(24) << _yLower+(i+0.5)*_binwidth;   
+        }
+		ofs << std::endl;
+		ofs.close();
+	}
+
+    resizeVectors();
+    resetVectors();
+}
+
+
+
+void CoalescenceSampling::readXML(XMLfileUnits& xmlconfig) {
+
+    Log::global_log->info() << "["<< getPluginName()<<"] ----------------READXML CALLED: "  << std::endl;
+    xmlconfig.getNodeValue("binwidth", _binwidth);
+    xmlconfig.getNodeValue("start", _startSampling);
+    xmlconfig.getNodeValue("writefrequency", _writeFrequency);
+    xmlconfig.getNodeValue("stop", _stopSampling);
+    xmlconfig.getNodeValue("yLower", _yLower);
+    xmlconfig.getNodeValue("yUpper", _yUpper);
+    xmlconfig.getNodeValue("radius", _radius);
+    xmlconfig.getNodeValue("outputPrefix", _outputPrefix);
+
+
+    Log::global_log->info() << "["<< getPluginName()<<"] Start:WriteFreq:Stop: " << _startSampling << " : " << _writeFrequency << " : " << _stopSampling << std::endl;
+    Log::global_log->info() << "["<< getPluginName()<<"] Binwidth: " << _binwidth << std::endl;
+    Log::global_log->info() << "["<< getPluginName()<<"] All components treated as single one" << std::endl;
+}        
+
+
+
+void CoalescenceSampling::afterForces(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, unsigned long simstep) {
+
+    // Sampling starts after _startSampling and is conducted up to _stopSampling
+    if ((simstep < _startSampling) or (simstep > _stopSampling)) {
+        return;
+    }
+
+    // Do not write or sample data directly after (re)start in the first step
+    // if ( simstep == _simulation.getNumInitTimesteps() ) {
+    //     return;
+    // }
+
+    CommVar<std::vector<unsigned long>> numMolecules_step;
+    CommVar<std::vector<double>> mass_step;
+    CommVar<std::vector<double>> ekin_step;                        // Including drift energy
+    std::array<CommVar<std::vector<double>>, 3> ekinVect_step;
+    std::array<CommVar<std::vector<double>>, 3> velocityVect_step;
+    std::array<CommVar<std::vector<double>>, 3> virialVect_step;
+
+    numMolecules_step.local.resize(_numBinsGlobal);
+    mass_step.local.resize(_numBinsGlobal);
+
+    numMolecules_step.global.resize(_numBinsGlobal);
+    mass_step.global.resize(_numBinsGlobal);
+
+    std::fill(numMolecules_step.local.begin(), numMolecules_step.local.end(), 0ul);
+
+    std::fill(numMolecules_step.global.begin(), numMolecules_step.global.end(), 0ul);
+
+    for (auto pit = particleContainer->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY); pit.isValid(); ++pit) {
+        // Do not consider particles outside of [_yLower,_yUpper]
+        const double ry = pit->r(1);
+        if (ry<_yLower or ry>_yUpper) { continue; }
+        // Do not consider particles outside of most outer radius
+        const double distCenter_x = pit->r(0)-0.5*_globalBoxLength[0];
+        const double distCenter_z = pit->r(2)-0.5*_globalBoxLength[2];
+        const double distCenter = std::sqrt(std::pow(distCenter_x,2) + std::pow(distCenter_z,2));
+        if (distCenter >= _radius) { continue; }
+        const unsigned int index = std::min(_numBinsGlobal, static_cast<unsigned int>((ry-_yLower)/_binwidth));  // Index of bin of height
+
+        numMolecules_step.local[index] ++;
+    }
+
+// Gather quantities. Note: MPI_Reduce instead of MPI_Allreduce! Therefore, only root has correct values
+#ifdef ENABLE_MPI
+    MPI_Reduce(numMolecules_step.local.data(), numMolecules_step.global.data(), _numBinsGlobaly, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+#else
+    for (unsigned long i = 0; i < _numBinsGlobal; i++) {
+        numMolecules_step.global[i] = numMolecules_step.local[i];
+    }
+#endif
+
+    // Only root knows real quantities (MPI_Reduce instead of MPI_Allreduce)
+    // Accumulate data
+    if (domainDecomp->getRank() == 0) {
+        for (unsigned long i = 0; i < _numBinsGlobal; i++) {
+            const unsigned long numMols = numMolecules_step.global[i];
+            _numMolecules_accum[i] += numMols;
+            _countSamples[i]++;
+        }
+    }
+
+    // Write out data every _writeFrequency step
+    if ( (simstep - _startSampling) % _writeFrequency == 0 ) {
+        Log::global_log->info() << "------------------------------------------A_-------------------";
+        // write rho Output 
+        if (domainDecomp->getRank() == 0) {
+            std::ofstream ofs(_rhoOutputFilename, std::ios::app);
+            ofs << std::setw(24) << simstep;
+
+            for (unsigned long i = 0; i < _numBinsGlobal; i++) {
+                unsigned long numSamples {0ul};
+                double numMolsPerStep {std::nan("0")}; // Not an int as particles change bin during simulation
+                double rho {0.0};
+                if (_countSamples[i] > 0ul){
+                    const double numMols_accum = static_cast<double>(_numMolecules_accum[i]);
+                    const double cellVolume = _binwidth*3.1415926536*std::pow(_radius,2);
+                    numSamples = _countSamples[i];
+
+                    numMolsPerStep = numMols_accum/numSamples;
+                    rho         = numMolsPerStep  / cellVolume;
+                }
+                ofs << std::setw(24) << rho;   
+            }
+            ofs << std::endl;
+            ofs.close();
+        }
+
+        // Reset vectors to zero
+        resetVectors();
+    }
+}
+
+
+
+
+// Resize vectors
+void CoalescenceSampling::resizeVectors() {
+
+    _numMolecules_accum.resize(_numBinsGlobal);
+    _countSamples.resize(_numBinsGlobal);
+}
+
+// Fill vectors with zeros
+void CoalescenceSampling::resetVectors() {
+    std::fill(_numMolecules_accum.begin(), _numMolecules_accum.end(), 0ul);
+    std::fill(_countSamples.begin(), _countSamples.end(), 0ul);
+}
---
+++ src/plugins/CoalescenceSampling.h
@@ -0,0 +1,103 @@
+/*
+ * CoalescenceSampling.h
+ *
+ *  Created on: May 2024
+ *      Author: niemann
+ */
+
+
+#ifndef MARDYN_TRUNK_COALESCENCESAMPLING_H
+#define MARDYN_TRUNK_COALESCENCESAMPLING_H
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <limits>
+
+#include "PluginBase.h"
+#include "particleContainer/ParticleContainer.h"
+#include "Domain.h"
+#include "utils/CommVar.h"
+#include "parallel/DomainDecompBase.h"
+
+
+/** @brief
+* Plugin: can be enabled via config.xml <br>
+*
+* Samples values in cylinder binwise in y-direction
+* There is NO differentiation between components; it is assumed that all (pseudo) components have the same mass (for correct calculation of temperature)
+* \code{.xml}
+* <plugin name="CoalescenceSampling">
+*           <binwidth>FLOAT</binwidth>                  <!-- Width of sampling bins; default 1.0 -->
+            <start>INT</start>                          <!-- Simstep to start sampling; default 0 -->
+            <stop>INT</stop>                            <!-- Simstep to stop sampling; default MAX -->
+            <writefrequency>INT</writefrequency>        <!-- Simstep to write out result file; default 10000 -->
+            <yLower>FLOAT</yLower>                      <!-- start of Cylinder -->
+            <yUpper>FLOAT</yUpper>                      <!-- end of Cylinder -->
+            <radius>FLOAT</radius>                      <!-- radius of Cylinder -->
+            <outputPrefix>STRING</outputPrefix>                      <!-- outputPrefix -->
+* </plugin>
+* \endcode
+*/
+
+class CoalescenceSampling : public PluginBase{
+
+ private: 
+    // Control: general
+    float _binwidth {.5f};
+    unsigned long _startSampling {0ul};
+    unsigned long _writeFrequency {100ul};
+    unsigned long _stopSampling {std::numeric_limits<unsigned long>::max()};
+    float _yLower {0.f};
+    float _yUpper {std::numeric_limits<double>::max()};
+    float _radius {4.f};
+    std::string _outputPrefix = "coalSampling_";
+    std::string _rhoOutputFilename = _outputPrefix +  "_coalSampling_r"+std::to_string(_radius)+".csv";
+
+    // Auxiliary variables
+    unsigned int _numBinsGlobal;
+    std::array<double, 3> _globalBoxLength;
+    
+    // Accumulated quantities over _writeFrequency per bin
+    // NOTE: Only the root process knows correct values (except number of molecules and temperature)
+    // With only the root process writing data, MPI_Reduce instead of MPI_Allreduce could be used -> only root has correct data
+    std::vector<unsigned long> _numMolecules_accum;             // Number of molecules in bin
+    // std::vector<unsigned long> _doftotal_accum;                 // DOF in bin
+    // std::vector<double> _mass_accum;                            // Mass
+    // std::vector<double> _ekin_accum;                            // Kinetic energy including drift
+    // std::vector<double> _virial_accum;                          // Virial
+    // std::array<std::vector<double>, 3> _ekinVect_accum;         // Kinetic energy in each direction (drift corrected); radial, height (y), tangantial
+    // std::array<std::vector<double>, 3> _velocityVect_accum;     // Drift velocity in each direction; radial, height (y), tangantial
+    // std::array<std::vector<double>, 3> _virialVect_accum;       // Virial in each direction; radial, height (y), tangantial
+
+    std::vector<unsigned long> _countSamples;                   // Number of samples; can vary from bin to bin as some bins could be empty
+
+    void resizeVectors();  // Change size of accumulation vectors
+    void resetVectors();   // Set accumulation vectors to zero
+
+ public:
+    CoalescenceSampling();
+    ~CoalescenceSampling() override = default;
+
+    void init(ParticleContainer* /* particleContainer */, DomainDecompBase* domainDecomp, Domain* domain) override;
+
+    void readXML(XMLfileUnits& xmlconfig) override;
+
+    void beforeForces(ParticleContainer* /* particleContainer */, DomainDecompBase* /* domainDecomp */, unsigned long /* simstep */) override {}
+
+    // Needs to be called when halo cells are still existing (for sampling of chemical potential)
+    void afterForces(ParticleContainer* particleContainer, DomainDecompBase* domainDecomp, unsigned long simstep) override;
+    
+    void endStep(ParticleContainer* /* particleContainer */, DomainDecompBase* /* domainDecomp */, Domain* /* domain */, unsigned long /* simstep */) override {}
+    
+    void finish(ParticleContainer* /* particleContainer */, DomainDecompBase* /* domainDecomp */, Domain* /* domain */) override {}
+
+    std::string getPluginName() override { return "CoalescenceSampling"; }
+
+    static PluginBase* createInstance() { return new CoalescenceSampling(); }
+};
+
+
+
+#endif //MARDYN_TRUNK_COALESCENCESAMPLING_H
+
