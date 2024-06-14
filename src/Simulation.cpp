40dd2b4b5566fbe1b9bd8a4aa76e88cb905e5137
HomesGH <55833544+HomesGH@users.noreply.github.com>
2024-06-14 19:41:50 +0200

    Add molecule-wise virial correction

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -565,7 +565,7 @@ void Simulation::readXML(XMLfileUnits& xmlconfig) {
 			{
 				delete _longRangeCorrection;
 				Log::global_log->info() << "Initializing homogeneous LRC." << std::endl;
-				_longRangeCorrection = new Homogeneous(_cutoffRadius, _LJCutoffRadius, _domain, global_simulation);
+				_longRangeCorrection = new Homogeneous(_cutoffRadius, _LJCutoffRadius, _domain, _moleculeContainer, global_simulation);
 			}
 			else if("none" == type)
 			{
@@ -581,7 +581,7 @@ void Simulation::readXML(XMLfileUnits& xmlconfig) {
 		} else {
 			delete _longRangeCorrection;
 			Log::global_log->info() << "Initializing default homogeneous LRC, as no LRC was defined." << std::endl;
-			_longRangeCorrection = new Homogeneous(_cutoffRadius, _LJCutoffRadius, _domain, global_simulation);
+			_longRangeCorrection = new Homogeneous(_cutoffRadius, _LJCutoffRadius, _domain, _moleculeContainer, global_simulation);
 		}
 
 		xmlconfig.changecurrentnode(".."); /* algorithm section */
