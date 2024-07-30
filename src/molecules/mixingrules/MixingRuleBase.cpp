cc6c1dacd9ec2e82e26db95a666bcfbb76107d04
HomesGH <55833544+HomesGH@users.noreply.github.com>
2024-07-30 12:54:19 +0200

    Break up blocks that have different intent
    Co-authored-by: FG-TUM <FG-TUM@users.noreply.github.com>

--- src/molecules/mixingrules/MixingRuleBase.cpp
+++ src/molecules/mixingrules/MixingRuleBase.cpp
@@ -12,19 +12,19 @@ void MixingRuleBase::readXML(const XMLfileUnits& xmlconfig) {
 	xmlconfig.getNodeValue("@cid1", cid1);
 	xmlconfig.getNodeValue("@cid2", cid2);
 
+    // catch invalid inputs
 	if (cid1 == cid2) {
 		Log::global_log->error() << "Mixing rules: cid1 and cid2 must not be the same but are both " << cid1 << std::endl;
 		mardyn_exit(1);
 	} else if ((cid1 <= 0) or (cid2 <= 0)) {
 		Log::global_log->error() << "Mixing rules: cid1 and cid2 must be greater than zero" << std::endl;
 		mardyn_exit(1);
-	} else if (cid1 > cid2) {
+	} 
+	
 	// Symmetry for mixing rules is assumed
-	// cid1 must be less than cid2
-		_cid1 = cid2 - 1;  // component id - 1 to convert to internal format starting with 0
-		_cid2 = cid1 - 1;
-	} else {
-		_cid1 = cid1 - 1;
-		_cid2 = cid2 - 1;
-	}
+	// _cid1 must be less than _cid2
+	std::tie(_cid1, _cid2) = std::minmax(cid1, cid2);
+	// component id - 1 to convert to internal format starting with 0
+	--_cid1;
+	--_cid2;
 }
