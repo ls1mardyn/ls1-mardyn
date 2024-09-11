2b1d448d58fe6ec9b6180f687a1d59e3c6fb6895
amartyads <amartyads@gmail.com>
2024-09-11 17:12:04 +0200

    more documentation

--- src/parallel/boundaries/BoundaryUtils.h
+++ src/parallel/boundaries/BoundaryUtils.h
@@ -56,14 +56,44 @@ bool isDimensionStringPermissible(std::string dimension);
 /* Check if a dimension is allowed. */
 bool isDimensionNumericPermissible(int dim);
 
+/* Convert a string from permissibleDimensionsString into DimensionType */
 DimensionType convertStringToDimension(std::string dimension);
+
+/* Convert a dimension from number to DimensionType, where x = +-1, y = +-2 and
+ * z = +-3 */
 DimensionType convertNumericToDimension(int dim);
+
+/**
+ * Convert an LS1 internal dimension representation int to DimensionType, where
+ * x = 0, y = 1 and z = 2 Since this does not contain direction information, the
+ * positive direction is returned.
+ */
 DimensionType convertLS1DimsToDimensionPos(int dim);
 
+/* Convert a DimensionType into a string from permissibleDimensionsString */
 std::string convertDimensionToString(DimensionType dimension);
+
+/**
+ * Convert a DimensionType into a string from permissibleDimensionsString, and
+ * remove directions Ex: POSX and NEGX both return "x"
+ */
 std::string convertDimensionToStringAbs(DimensionType dimension);
+
+/* Convert a dimension from DimensionType to number, where x = +-1, y = +-2 and
+ * z = +-3 */
 int convertDimensionToNumeric(DimensionType dimension);
+
+/**
+ * Convert a DimensionType into an int, and remove directions
+ * +x/-x returns 1, +y/-y returns 2, +z/-z returns 3
+ */
 int convertDimensionToNumericAbs(DimensionType dimension);
+
+/**
+ * Convert a DimensionType to LS1 internal dimension representation int, where x
+ * = 0, y = 1 and z = 2 Since this does not contain direction information, both
+ * POSX and NEGX return 0, for example.
+ */
 int convertDimensionToLS1Dims(DimensionType dimension);
 
 /* Used to convert string read from the XML input file. */
