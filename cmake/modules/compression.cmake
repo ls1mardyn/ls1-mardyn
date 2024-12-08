75bdefc7558569daa9a39a965f76c029426b6855
Christoph Niethammer <niethammer@hlrs.de>
2024-12-08 17:23:26 +0100

    Remove unused compression code
    This code implementing some compression wrapper was never used elsewhere in
    the codebase and has been unmaintained for around 6 years.
    
    Signed-off-by: Christoph Niethammer <niethammer@hlrs.de>

--- cmake/modules/compression.cmake
+++
@@ -1,9 +0,0 @@
-# compression wrapper
-option(ENABLE_COMPRESSION "Use compression wrapper" ${ENABLE_COMPRESSION_WRAPPER})
-if(ENABLE_COMPRESSION)
-    message(STATUS "Using Compression wrapper.")
-    option(ENABLE_LZ4 "Use LZ4 in compression wrapper" ON)
-    include(lz4)
-else()
-    message(STATUS "Compression wrapper disabled.")
-endif()
